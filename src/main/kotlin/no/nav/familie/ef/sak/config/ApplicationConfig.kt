76fd5ea6971523942ef44190605e4de74ad7e64e
ma10s <mattis.janitz@nav.no>
2020-03-03 16:26:34 +0100

    Legg til acceptedClients

--- src/main/kotlin/no/nav/familie/ef/sak/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/config/ApplicationConfig.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ef.sak.config
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
@@ -13,6 +14,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootConfiguration
 @ConfigurationPropertiesScan
+@EntityScan("no.nav.familie")
+@ComponentScan("no.nav.familie")
 @EnableSwagger2
 @EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
 //@EnableOAuth2Client(cacheEnabled = true)
