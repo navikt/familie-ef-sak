830ae6cd7aa49d74cd182812174296e5dddb87d7
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-03-13 08:10:53 +0100

    Bedre testdata.

--- src/test/kotlin/no/nav/familie/ef/sak/config/FamilieIntegrasjonerMock.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/config/FamilieIntegrasjonerMock.kt
@@ -3,12 +3,21 @@ package no.nav.familie.ef.sak.no.nav.familie.ef.sak.config
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import no.nav.familie.ef.sak.config.IntegrasjonerConfig
+import no.nav.familie.ef.sak.integration.dto.personopplysning.Periode
 import no.nav.familie.ef.sak.integration.dto.personopplysning.PersonIdent
 import no.nav.familie.ef.sak.integration.dto.personopplysning.PersonhistorikkInfo
 import no.nav.familie.ef.sak.integration.dto.personopplysning.Personinfo
+import no.nav.familie.ef.sak.integration.dto.personopplysning.adresse.Adresse
+import no.nav.familie.ef.sak.integration.dto.personopplysning.adresse.AdressePeriode
+import no.nav.familie.ef.sak.integration.dto.personopplysning.adresse.AdresseType
+import no.nav.familie.ef.sak.integration.dto.personopplysning.adresse.Adresseinfo
+import no.nav.familie.ef.sak.integration.dto.personopplysning.relasjon.Familierelasjon
+import no.nav.familie.ef.sak.integration.dto.personopplysning.relasjon.RelasjonsRolleType
 import no.nav.familie.ef.sak.integration.dto.personopplysning.relasjon.SivilstandType
+import no.nav.familie.ef.sak.integration.dto.personopplysning.status.PersonstatusPeriode
 import no.nav.familie.ef.sak.integration.dto.personopplysning.status.PersonstatusType
 import no.nav.familie.ef.sak.integration.dto.personopplysning.tilhørighet.Landkode
+import no.nav.familie.ef.sak.integration.dto.personopplysning.tilhørighet.StatsborgerskapPeriode
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.objectMapper
 import org.springframework.context.annotation.Bean
@@ -45,25 +54,85 @@ class FamilieIntegrasjonerMock(integrasjonerConfig: IntegrasjonerConfig) {
     }
 
     companion object {
-        private val person = Ressurs.success(Personinfo(PersonIdent("12345678901"),
+        private val person = Ressurs.success(Personinfo(PersonIdent("12137578901"),
                                                         "Bob",
-                                                        null,
-                                                        null,
-                                                        LocalDate.now(),
+                                                        Adresseinfo(AdresseType.BOSTEDSADRESSE,
+                                                                    "Bob",
+                                                                    "Adresselinje 1",
+                                                                    "Adresselinje 2",
+                                                                    "Adresselinje 3",
+                                                                    "Adresselinje 4",
+                                                                    "0754",
+                                                                    "Oslo",
+                                                                    "Norge",
+                                                                    PersonstatusType.BOSA),
+
+                                                        "Kjønn",
+                                                        LocalDate.of(1975, 12, 13),
                                                         null,
                                                         PersonstatusType.FØDR,
                                                         SivilstandType.ENKE,
-                                                        emptySet(),
+                                                        setOf(Familierelasjon(PersonIdent("24120165487"),
+                                                                              RelasjonsRolleType.BARN,
+                                                                              LocalDate.of(2001, 12, 24),
+                                                                              true),
+                                                              Familierelasjon(PersonIdent("11127565498"),
+                                                                              RelasjonsRolleType.EKTE,
+                                                                              LocalDate.of(1975, 12, 11),
+                                                                              false)),
                                                         Landkode.NORGE,
-                                                        null,
-                                                        null,
-                                                        null,
-                                                        null,
-                                                        emptyList(),
-                                                        15))
-        private val personhistorikkInfo = Ressurs.success(PersonhistorikkInfo(PersonIdent("12345678901"),
-                                                                              emptyList(),
-                                                                              emptyList(),
-                                                                              emptyList()))
+                                                        "Utenlandsadresse",
+                                                        "Geografisk tilknytning",
+                                                        "Diskresjonskode",
+                                                        "adresseLandkode",
+                                                        listOf(Adresseinfo(AdresseType.BOSTEDSADRESSE,
+                                                                           "Bob",
+                                                                           "Adresselinje 1",
+                                                                           "Adresselinje 2",
+                                                                           "Adresselinje 3",
+                                                                           "Adresselinje 4",
+                                                                           "0754",
+                                                                           "Oslo",
+                                                                           "Norge",
+                                                                           PersonstatusType.BOSA),
+                                                               Adresseinfo(AdresseType.POSTADRESSE,
+                                                                           "Bob",
+                                                                           "Adresselinje 1",
+                                                                           "Adresselinje 2",
+                                                                           "Adresselinje 3",
+                                                                           "Adresselinje 4",
+                                                                           "0754",
+                                                                           "Oslo",
+                                                                           "Norge",
+                                                                           PersonstatusType.BOSA)),
+                                                        45))
+        private val personhistorikkInfo =
+                Ressurs.success(PersonhistorikkInfo(PersonIdent("12137578901"),
+                                                    listOf(PersonstatusPeriode(Periode(LocalDate.of(1975, 12, 13),
+                                                                                       LocalDate.of(2004, 5, 21)),
+                                                                               PersonstatusType.FØDR)),
+                                                    listOf(StatsborgerskapPeriode(Periode(LocalDate.of(1975, 12, 13),
+                                                                                          LocalDate.of(2004, 5, 21)),
+                                                                                  Landkode.UDEFINERT)),
+                                                    listOf(AdressePeriode(Periode(LocalDate.of(1975, 12, 13),
+                                                                                  LocalDate.of(2004, 5, 21)),
+                                                                          Adresse(AdresseType.BOSTEDSADRESSE,
+                                                                                  "Adresselinje 1",
+                                                                                  "Adresselinje 2",
+                                                                                  "Adresselinje 3",
+                                                                                  "Adresselinje 4",
+                                                                                  "zipcode",
+                                                                                  "Boston",
+                                                                                  "Uniten")),
+                                                           AdressePeriode(Periode(LocalDate.of(1975, 12, 13),
+                                                                                  LocalDate.of(9999, 12, 31)),
+                                                                          Adresse(AdresseType.BOSTEDSADRESSE,
+                                                                                  "Adresselinje 1",
+                                                                                  "Adresselinje 2",
+                                                                                  "Adresselinje 3",
+                                                                                  "Adresselinje 4",
+                                                                                  "0754",
+                                                                                  "Oslo",
+                                                                                  "Norge")))))
     }
 }
