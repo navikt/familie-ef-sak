82eac77b787697fe4ddc43f1f8e86c15ccdd5d49
Bent Lorentzen <bent.lorentzen@nav.no>
2020-03-25 15:26:23 +0100

    variableFix

--- src/main/kotlin/no/nav/familie/ef/sak/integration/dto/pdl/PdlPersonRequestVariables.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/integration/dto/pdl/PdlPersonRequestVariables.kt
@@ -1,4 +1,4 @@
 package no.nav.familie.ef.sak.integration.dto.pdl
 
 data class PdlPersonRequestVariables(var ident: String,
-                                     val historikk: Boolean)
+                                     val navnHistorikk: Boolean)
