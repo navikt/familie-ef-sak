6b9d1e1f2222f49c3b77debe6b861ddec7601050
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2020-05-19 12:54:40 +0200

    Medlemskap (#65)
    Lagt til vurdering av medlemskapsvilkår.

--- src/main/kotlin/no/nav/familie/ef/sak/api/gui/PersonInfoController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/gui/PersonInfoController.kt
@@ -1,10 +1,9 @@
-package no.nav.familie.ef.sak.api
+package no.nav.familie.ef.sak.api.gui
 
 import no.nav.familie.ba.sak.validering.PersontilgangConstraint
-import no.nav.familie.ef.sak.api.dto.Person
+import no.nav.familie.ef.sak.api.gui.dto.Person
 import no.nav.familie.ef.sak.integration.dto.pdl.PdlAnnenForelder
 import no.nav.familie.ef.sak.integration.dto.pdl.PdlBarn
-import no.nav.familie.ef.sak.integration.dto.pdl.PdlResponse
 import no.nav.familie.ef.sak.integration.dto.pdl.PdlSøker
 import no.nav.familie.ef.sak.service.PersonService
 import no.nav.familie.kontrakter.felles.Ressurs
@@ -30,7 +29,7 @@ class PersonInfoController(private val personService: PersonService) {
 
     @ExceptionHandler(ConstraintViolationException::class)
     fun handleRestClientResponseException(e: ConstraintViolationException): Ressurs<ConstraintViolationException> {
-        return Ressurs.failure<ConstraintViolationException>(e.message, null, e)
+        return Ressurs.failure(e.message, null, e)
     }
 
     @GetMapping
@@ -40,18 +39,17 @@ class PersonInfoController(private val personService: PersonService) {
 
 
     @GetMapping("/soker") // TODO Testendepunkt. Fjernes etter hvert
-    fun søkerinfo(@RequestHeader(name = "Nav-Personident") @PersontilgangConstraint ident: String): PdlResponse<PdlSøker> {
+    fun søkerinfo(@RequestHeader(name = "Nav-Personident") @PersontilgangConstraint ident: String): PdlSøker {
         return personService.hentPdlPerson(ident)
     }
 
     @GetMapping("/barn") // TODO Testendepunkt. Fjernes etter hvert
-    fun barninfoPdl(@RequestHeader(name = "Nav-Personident") @PersontilgangConstraint ident: String): PdlResponse<PdlBarn> {
+    fun barninfoPdl(@RequestHeader(name = "Nav-Personident") @PersontilgangConstraint ident: String): PdlBarn {
         return personService.hentPdlBarn(ident)
     }
 
     @GetMapping("/forelder2") // TODO Testendepunkt. Fjernes etter hvert
-    fun annenForelderinfoPdl(@RequestHeader(name = "Nav-Personident") @PersontilgangConstraint ident: String)
-            : PdlResponse<PdlAnnenForelder> {
+    fun annenForelderinfoPdl(@RequestHeader(name = "Nav-Personident") @PersontilgangConstraint ident: String): PdlAnnenForelder {
         return personService.hentPdlAnnenForelder(ident)
     }
 
---
+++ src/test/kotlin/no/nav/familie/ef/sak/vurdering/medlemskap/MedlemskapRegelsettTest.kt
@@ -0,0 +1,122 @@
+package no.nav.familie.ef.sak.vurdering.medlemskap
+
+import io.mockk.mockk
+import no.nav.familie.ef.sak.integration.dto.pdl.*
+import no.nav.familie.ef.sak.nare.evaluations.Resultat
+import no.nav.familie.ef.sak.no.nav.familie.ef.sak.vurdering.medlemskap.pdlSøker
+import no.nav.familie.ef.sak.no.nav.familie.ef.sak.vurdering.medlemskap.søknad
+import no.nav.familie.kontrakter.ef.søknad.Medlemskapsdetaljer
+import no.nav.familie.kontrakter.ef.søknad.Stønadsstart
+import no.nav.familie.kontrakter.ef.søknad.Søknadsfelt
+import no.nav.familie.kontrakter.felles.medlemskap.Medlemskapsinfo
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Month
+
+internal class MedlemskapRegelsettTest {
+
+    @Test
+    fun `vurderingMedlemskapSøker returnerer Nei hvis søker er registrert som ikke medlem`() {
+        val medlemskapsinfoUtenUnntak = Medlemskapsinfo("3213213", emptyList(), emptyList(), emptyList())
+        val pdlSøker = pdlSøker(fødsel = listOf(Fødsel(null, LocalDate.of(1999, 4, 5), null, null, null)),
+                                bostedsadresse = emptyList(),
+                                folkeregisterpersonstatus = listOf(Folkeregisterpersonstatus("bosatt", "bo")),
+                                statsborgerskap = listOf(Statsborgerskap("SE", LocalDate.of(2002, 2, 2), null)))
+        val søknad = søknad(medlemskapsdetaljer = Søknadsfelt("", Medlemskapsdetaljer(Søknadsfelt("", true), mockk(), mockk())),
+                            stønadsstart = Søknadsfelt("", Stønadsstart(Søknadsfelt("", Month.AUGUST), Søknadsfelt("", 2014))))
+        val medlemskapshistorikk = Medlemskapshistorikk(pdlSøker, medlemskapsinfoUtenUnntak)
+        val medlemskapsgrunnlag = Medlemskapsgrunnlag(pdlSøker, medlemskapshistorikk, søknad)
+
+        val evaluering = MedlemskapRegelsett().vurderingMedlemskapSøker.evaluer(medlemskapsgrunnlag)
+
+        assertThat(evaluering.resultat).isEqualTo(Resultat.NEI)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er søker registrert medlem?" }?.resultat).isEqualTo(Resultat.NEI)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er søker registrert bosatt i norge" }?.resultat)
+                .isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find {
+            it.beskrivelse == "Oppholder søker seg i Norge? ELLER Skyldes utenlandsoppholdet arbeid for norsk arbeidsgiver?"
+        }?.resultat).isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er vilkår for forutgående medlemskap oppfylt?" }?.resultat)
+                .isEqualTo(Resultat.NEI)
+        assertThat(evaluering.children.find {
+            it.beskrivelse == "Har søker lovlig opphold i Norge eller er søker EØS-borger med oppholdsrett"
+        }?.resultat).isEqualTo(Resultat.JA)
+    }
+
+    @Test
+    fun `vurderingMedlemskapSøker returnerer Ja hvis søker er medlem og oppfyller kravet til forutgående medlemskap`() {
+        val medlemskapsinfoUtenUnntak = Medlemskapsinfo("3213213", emptyList(), emptyList(), emptyList())
+        val pdlSøker = pdlSøker(fødsel = listOf(Fødsel(null, LocalDate.of(1999, 4, 5), null, null, null)),
+                                folkeregisterpersonstatus = listOf(Folkeregisterpersonstatus("bosatt", "bo")),
+                                bostedsadresse = listOf(Bostedsadresse(null,
+                                                                       null,
+                                                                       Folkeregistermetadata(LocalDateTime.of(1999, 4, 5, 0, 0),
+                                                                                             null),
+                                                                       null,
+                                                                       null)),
+                                statsborgerskap = listOf(Statsborgerskap("SE", LocalDate.of(2002, 2, 2), null)))
+        val søknad = søknad(medlemskapsdetaljer = Søknadsfelt("",
+                                                              Medlemskapsdetaljer(Søknadsfelt("oppholdINorge", true),
+                                                                                  mockk(),
+                                                                                  mockk())),
+                            stønadsstart = Søknadsfelt("", Stønadsstart(Søknadsfelt("", Month.AUGUST), Søknadsfelt("", 2014))))
+        val medlemskapshistorikk = Medlemskapshistorikk(pdlSøker, medlemskapsinfoUtenUnntak)
+        val medlemskapsgrunnlag = Medlemskapsgrunnlag(pdlSøker, medlemskapshistorikk, søknad)
+
+        val evaluering = MedlemskapRegelsett().vurderingMedlemskapSøker.evaluer(medlemskapsgrunnlag)
+
+        assertThat(evaluering.resultat).isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er søker registrert medlem?" }?.resultat).isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er søker registrert bosatt i norge" }?.resultat)
+                .isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find {
+            it.beskrivelse == "Oppholder søker seg i Norge? ELLER Skyldes utenlandsoppholdet arbeid for norsk arbeidsgiver?"
+        }?.resultat).isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er vilkår for forutgående medlemskap oppfylt?" }?.resultat)
+                .isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find {
+            it.beskrivelse == "Har søker lovlig opphold i Norge eller er søker EØS-borger med oppholdsrett"
+        }?.resultat).isEqualTo(Resultat.JA)
+    }
+
+    @Test
+    fun `vurderingMedlemskapSøker returnerer Kanskje hvis søker ikke oppholder seg i Norge`() {
+        val medlemskapsinfoUtenUnntak = Medlemskapsinfo("3213213", emptyList(), emptyList(), emptyList())
+        val pdlSøker = pdlSøker(fødsel = listOf(Fødsel(null, LocalDate.of(1999, 4, 5), null, null, null)),
+                                folkeregisterpersonstatus = listOf(Folkeregisterpersonstatus("bosatt", "bo")),
+                                bostedsadresse = listOf(Bostedsadresse(null,
+                                                                       null,
+                                                                       Folkeregistermetadata(LocalDateTime.of(1999, 4, 5, 0, 0),
+                                                                                             null),
+                                                                       null,
+                                                                       null)),
+                                statsborgerskap = listOf(Statsborgerskap("SE", LocalDate.of(2002, 2, 2), null)))
+        val søknad = søknad(medlemskapsdetaljer = Søknadsfelt("",
+                                                              Medlemskapsdetaljer(Søknadsfelt("oppholdINorge", false),
+                                                                                  mockk(),
+                                                                                  mockk())),
+                            stønadsstart = Søknadsfelt("", Stønadsstart(Søknadsfelt("", Month.AUGUST), Søknadsfelt("", 2014))))
+        val medlemskapshistorikk = Medlemskapshistorikk(pdlSøker, medlemskapsinfoUtenUnntak)
+        val medlemskapsgrunnlag = Medlemskapsgrunnlag(pdlSøker, medlemskapshistorikk, søknad)
+
+        val evaluering = MedlemskapRegelsett().vurderingMedlemskapSøker.evaluer(medlemskapsgrunnlag)
+
+        assertThat(evaluering.resultat).isEqualTo(Resultat.KANSKJE)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er søker registrert medlem?" }?.resultat).isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er søker registrert bosatt i norge" }?.resultat)
+                .isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find { it.beskrivelse == "Oppholder søker seg i Norge?" }?.resultat)
+                .isEqualTo(Resultat.NEI)
+        assertThat(evaluering.children.find {
+            it.beskrivelse == "Skyldes utenlandsoppholdet arbeid for norsk arbeidsgiver?"
+        }?.resultat).isEqualTo(Resultat.KANSKJE)
+        assertThat(evaluering.children.find { it.beskrivelse == "Er vilkår for forutgående medlemskap oppfylt?" }?.resultat)
+                .isEqualTo(Resultat.JA)
+        assertThat(evaluering.children.find {
+            it.beskrivelse == "Har søker lovlig opphold i Norge eller er søker EØS-borger med oppholdsrett"
+        }?.resultat).isEqualTo(Resultat.JA)
+    }
+
+}
