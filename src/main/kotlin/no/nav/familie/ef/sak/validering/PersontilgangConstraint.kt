0093c44581e38a63f1d23bd8ff2930a03f246a2c
Johan Blomgren <johan.blomgren@nav.no>
2020-05-20 16:13:21 +0200

    Bruk body istedet for header når man skal sende inn personident

--- src/main/kotlin/no/nav/familie/ef/sak/validering/PersontilgangConstraint.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/validering/PersontilgangConstraint.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.sak.validering
 
 import no.nav.familie.ef.sak.validering.Persontilgang
+import no.nav.familie.ef.sak.validering.Persontilgang2
 import no.nav.familie.ef.sak.validering.Sakstilgang
 import javax.validation.Constraint
 import javax.validation.Payload
@@ -8,8 +9,8 @@ import kotlin.reflect.KClass
 
 @Suppress("unused")
 @MustBeDocumented
-@Constraint(validatedBy = [Persontilgang::class])
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
+@Constraint(validatedBy = [Persontilgang::class, Persontilgang2::class])
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class PersontilgangConstraint(val message: String = "Ikke tilgang til person",
                                          val groups: Array<KClass<*>> = [],
