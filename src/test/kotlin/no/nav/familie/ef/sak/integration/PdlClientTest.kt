52aceaa22f5e3d895c5e129c3d77b4a4d9c6845e
Johan Blomgren <blommish@users.noreply.github.com>
2020-05-29 17:43:53 +0200

    Pdl json parsing feil (#106)
    * Fikset pdlclienten
    
    * Fikset at folkeregisterpersonstatus kan være en tom liste
    
    * Fikset brukne spørringer mot pdl og lagt til testdata.
    
    * PdlClient kjører grønt.
    
    * Fikset manglende felter i graphql-spørringer.
    
    Co-authored-by: Bent Lorentzen <bent.lorentzen@nav.no>

---
+++ src/test/kotlin/no/nav/familie/ef/sak/integration/PdlClientTest.kt
@@ -0,0 +1,81 @@
+package no.nav.familie.ef.sak.integration
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ef.sak.config.PdlConfig
+import no.nav.familie.ef.sak.integration.dto.pdl.AdressebeskyttelseGradering
+import no.nav.familie.http.sts.StsRestClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.web.client.RestOperations
+import java.net.URI
+import java.time.LocalDate
+
+class PdlClientTest {
+
+    private val wireMockServer = WireMockServer(wireMockConfig().dynamicPort())
+    private val restOperations: RestOperations = RestTemplateBuilder().build()
+    private lateinit var pdlClient: PdlClient
+
+    @BeforeEach
+    fun setUp() {
+        wireMockServer.start()
+        val stsRestClient = mockk<StsRestClient>()
+        every { stsRestClient.systemOIDCToken } returns "token"
+        pdlClient = PdlClient(PdlConfig(URI.create(wireMockServer.baseUrl())), restOperations, stsRestClient)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        wireMockServer.resetAll()
+        wireMockServer.stop()
+    }
+
+    @Test
+    fun `pdlClienten håndterer response for søker-query mot pdltjenesten riktig`() {
+        wireMockServer.stubFor(post(urlEqualTo("/${PdlConfig.PATH_GRAPHQL}"))
+                                       .willReturn(okJson(readfile("søker.json"))))
+        val response = pdlClient.hentSøker("")
+        assertThat(response.bostedsadresse[0].vegadresse?.adressenavn).isEqualTo("INNGJERDSVEGEN")
+    }
+
+    @Test
+    fun `pdlClienten håndterer response for annenForelder-query mot pdltjenesten riktig`() {
+        wireMockServer.stubFor(post(urlEqualTo("/${PdlConfig.PATH_GRAPHQL}"))
+                                       .willReturn(okJson(readfile("annenForelder.json"))))
+        val response = pdlClient.hentForelder2("")
+        assertThat(response.bostedsadresse[0].angittFlyttedato).isEqualTo(LocalDate.of(1966,11,18))
+    }
+
+    @Test
+    fun `pdlClienten håndterer response for barn-query mot pdltjenesten riktig`() {
+        wireMockServer.stubFor(post(urlEqualTo("/${PdlConfig.PATH_GRAPHQL}"))
+                                       .willReturn(okJson(readfile("barn.json"))))
+        val response = pdlClient.hentBarn("")
+        assertThat(response.fødsel[0].fødselsdato).isEqualTo(LocalDate.of(1966,11,18))
+    }
+
+    @Test
+    fun `pdlClienten håndterer response for søkerKort-query mot pdltjenesten riktig`() {
+        wireMockServer.stubFor(post(urlEqualTo("/${PdlConfig.PATH_GRAPHQL}"))
+                                       .willReturn(okJson(readfile("søker_kort.json"))))
+        val response = pdlClient.hentSøkerKort("")
+        assertThat(response.adressebeskyttelse[0].gradering).isEqualTo(AdressebeskyttelseGradering.FORTROLIG)
+    }
+
+
+
+
+    private fun readfile(filnavn: String): String {
+        return this::class.java.getResource("/json/$filnavn").readText()
+    }
+
+
+
+}
