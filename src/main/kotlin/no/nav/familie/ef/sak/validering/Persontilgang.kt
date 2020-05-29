65b577f49dd0a652dd233cdfe65218db568280cd
Bent Lorentzen <bent.lorentzen@nav.no>
2020-05-29 12:15:52 +0200

    Fikset brukne spørringer mot pdl og lagt til testdata.

--- src/main/kotlin/no/nav/familie/ef/sak/validering/Persontilgang.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/validering/Persontilgang.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.sak.validering
 
 import no.nav.familie.ef.sak.api.dto.PersonIdentDto
 import no.nav.familie.ef.sak.integration.FamilieIntegrasjonerClient
+import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import javax.validation.ConstraintValidator
@@ -14,6 +15,8 @@ class Persontilgang(private val integrasjonerClient: FamilieIntegrasjonerClient)
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun isValid(personIdent: PersonIdentDto, ctx: ConstraintValidatorContext): Boolean {
+
+        val jwtToken = SpringTokenValidationContextHolder().tokenValidationContext.getJwtToken("azure")
         integrasjonerClient.sjekkTilgangTilPersoner(listOf(personIdent.personIdent))
                 .filterNot { it.harTilgang }
                 .forEach {
