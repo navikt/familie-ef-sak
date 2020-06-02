e1cc420b79d404370ee600ac7f407b5cd08f3c2f
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2020-06-02 10:36:08 +0200

    Skrur litt på config for forhåpentligvis å få integrasjon mot oppdrag i preprod til å funke

--- src/main/kotlin/no/nav/familie/ef/sak/økonomi/ØkonomiKlient.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/økonomi/ØkonomiKlient.kt
@@ -5,6 +5,7 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.oppdrag.OppdragId
 import no.nav.familie.kontrakter.felles.oppdrag.OppdragStatus
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
@@ -18,19 +19,19 @@ const val FAGSYSTEM = "EF"
 
 @Service
 class ØkonomiKlient(@Value("\${FAMILIE_OPPDRAG_API_URL}")
-                    private val familieOppdragUri: URI,
-                    azure: RestOperations) : AbstractRestClient(azure, "familie.oppdrag") {
+                    private val familieOppdragUri: URI, @Qualifier("azure") restOperations: RestOperations) :
+        AbstractRestClient(restOperations, "familie.oppdrag") {
 
     fun iverksettOppdrag(utbetalingsoppdrag: Utbetalingsoppdrag): Ressurs<String> {
-        return postForEntity(URI.create("$familieOppdragUri/oppdrag"), utbetalingsoppdrag)
+        return postForEntity(URI.create("$familieOppdragUri/api/oppdrag"), utbetalingsoppdrag)
     }
 
     fun hentStatus(oppdragId: OppdragId): Ressurs<OppdragStatus> {
-        return postForEntity(URI.create("$familieOppdragUri/status"), oppdragId)
+        return postForEntity(URI.create("$familieOppdragUri/api/status"), oppdragId)
     }
 
     fun grensesnittavstemOppdrag(fraDato: LocalDateTime, tilDato: LocalDateTime): ResponseEntity<Ressurs<String>> {
-        return operations.exchange(URI.create("$familieOppdragUri/grensesnittavstemming/$FAGSYSTEM/?fom=$fraDato&tom=$tilDato"),
+        return operations.exchange(URI.create("$familieOppdragUri/api/grensesnittavstemming/$FAGSYSTEM/?fom=$fraDato&tom=$tilDato"),
                                    HttpMethod.POST)
     }
 }
