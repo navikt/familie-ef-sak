a5c769b7b2b954ce449f68ec194b0b15a66e51ba
Johan Blomgren <johan.blomgren@nav.no>
2020-06-17 12:49:54 +0200

    Oppdatert versjon av kontrakter og fikset Vedlegg med sporbar

--- src/main/kotlin/no/nav/familie/ef/sak/repository/domain/Vedlegg.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/repository/domain/Vedlegg.kt
@@ -2,21 +2,45 @@ package no.nav.familie.ef.sak.repository.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Embedded
 import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 import java.util.*
 
-@Suppress("ArrayInDataClass")
 @Table("vedlegg")
 data class Vedlegg(@Id
                    val id: UUID,
                    @Column("sak_id")
                    val sakId: UUID,
-                   @Column("opprettet_tid")
-                   val opprettetTid: LocalDateTime = LocalDateTime.now(),
-        //TODO spårbar
+                   @Embedded(onEmpty = Embedded.OnEmpty.USE_EMPTY)
+                   val sporbar: Sporbar = Sporbar(),
                    val data: ByteArray,
-                   val navn: String)
+                   val navn: String) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Vedlegg
+
+        if (id != other.id) return false
+        if (sakId != other.sakId) return false
+        if (sporbar != other.sporbar) return false
+        if (!data.contentEquals(other.data)) return false
+        if (navn != other.navn) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + sakId.hashCode()
+        result = 31 * result + sporbar.hashCode()
+        result = 31 * result + data.contentHashCode()
+        result = 31 * result + navn.hashCode()
+        return result
+    }
+}
 
 object VedleggMapper {
     fun toDomain(sakId: UUID, vedlegg: no.nav.familie.kontrakter.ef.søknad.Vedlegg): Vedlegg =
--- src/main/resources/db/migration/V6__vedlegg.sql
+++ src/main/resources/db/migration/V6__vedlegg.sql
@@ -1,7 +1,10 @@
 CREATE TABLE vedlegg (
     id              UUID PRIMARY KEY,
     sak_id          UUID REFERENCES sak(id),
-    opprettet_tid   TIMESTAMP(3)                NOT NULL    DEFAULT localtimestamp,
+    opprettet_av    VARCHAR                     NOT NULL DEFAULT 'VL',
+    opprettet_tid   TIMESTAMP(3)                NOT NULL DEFAULT localtimestamp,
+    endret_av       VARCHAR                     NOT NULL,
+    endret_tid      TIMESTAMP(3)                NOT NULL DEFAULT localtimestamp,
     data            BYTEA                       NOT NULL,
     navn            VARCHAR                     NOT NULL
 );
