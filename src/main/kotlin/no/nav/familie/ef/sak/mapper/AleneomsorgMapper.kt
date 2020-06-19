6be23b019ab1f61ecffe4f0090b093eb20a6e2a5
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-06-19 16:45:18 +0200

    wip

--- src/main/kotlin/no/nav/familie/ef/sak/mapper/AleneomsorgMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/mapper/AleneomsorgMapper.kt
@@ -3,9 +3,8 @@ package no.nav.familie.ef.sak.mapper
 import no.nav.familie.ef.sak.api.gui.dto.Aleneomsorg
 import no.nav.familie.ef.sak.api.gui.dto.AnnenForelder
 import no.nav.familie.ef.sak.api.gui.dto.Barn
-import no.nav.familie.ef.sak.integration.dto.pdl.PdlAnnenForelder
-import no.nav.familie.ef.sak.integration.dto.pdl.PdlBarn
-import no.nav.familie.ef.sak.integration.dto.pdl.PdlSøker
+import no.nav.familie.ef.sak.api.gui.dto.DeltBosted
+import no.nav.familie.ef.sak.integration.dto.pdl.*
 import no.nav.familie.kontrakter.ef.søknad.Søknad
 import no.nav.familie.kontrakter.ef.søknad.Barn as Søknadsbarn
 
@@ -17,59 +16,64 @@ object AleneomsorgMapper {
                søknad: Søknad): Aleneomsorg {
 
 
-        val alleBarn: List<Pair<Søknadsbarn?, PdlBarn?>> = lagUnionAvSøknadsbarnOgRegisterBarn(søknad.barn.verdi, pdlBarn)
+        val alleBarn = lagUnionAvSøknadsbarnOgRegisterBarn(søknad.barn.verdi, pdlBarn)
 
         Aleneomsorg(
-                tilBarn(pdlBarn,
-                        pdlSøker,
-                        barneforeldre,
-                        søknad.barn.verdi)
+                alleBarn.map {
+                    tilBarn(it, barneforeldre)
+                }
         )
 
 
     }
 
-    private fun lagUnionAvSøknadsbarnOgRegisterBarn(søknadsbarn: List<Søknadsbarn>,
-                                                    pdlBarnMap: Map<String, PdlBarn>): List<Pair<Søknadsbarn?, PdlBarn?>> {
+    private fun lagUnionAvSøknadsbarnOgRegisterBarn(søknadsbarn: List<Søknadsbarn>, pdlBarnMap: Map<String, PdlBarn>)
+            : List<Pair<Søknadsbarn?, Map.Entry<String, PdlBarn>?>> {
 
         val søknadsbarnMatchetTilPdlBarn =
                 søknadsbarn.map {
-                    it to pdlBarnMap[it.fødselsnummer?.verdi?.verdi]?.let { pdlBarn ->
-                        it.fødselsnummer?.verdi?.verdi to pdlBarn
-                    }
+                    it to pdlBarnMap.entries.firstOrNull { entry -> it.fødselsnummer?.verdi?.verdi == entry.key }
                 }
 
-        val pdlBarnMatchetTilSøknadsbarn =
-                pdlBarnMap.map { entry ->
+        val pdlBarnIkkeISøknad =
+                pdlBarnMap.filter { entry ->
+                    søknadsbarn.firstOrNull { it.fødselsnummer?.verdi?.verdi == entry.key } == null
+                }.map { entry ->
                     søknadsbarn.firstOrNull { it.fødselsnummer?.verdi?.verdi == entry.key } to entry
                 }
 
-        val registerbarnIkkeISøknad = pdlBarnMatchetTilSøknadsbarn.filter { it.first == null }
-        val søknadsbarnIkkeIRegister = søknadsbarnMatchetTilPdlBarn.filter { it.second == null }
-
+        return søknadsbarnMatchetTilPdlBarn + pdlBarnIkkeISøknad
     }
 
-    private fun tilBarn(pdlBarnMap: Map<String, PdlBarn>,
-                        pdlSøker: PdlSøker,
-                        barneforeldre: Map<String, PdlAnnenForelder>,
-                        søknadsbarn: List<Søknadsbarn>): List<Barn> {
+    private fun tilBarn(it: Pair<no.nav.familie.kontrakter.ef.søknad.Barn?, Map.Entry<String, PdlBarn>?>,
+                        barneforeldre: Map<String, PdlAnnenForelder>): Barn {
+
+        val navn = it.second?.value?.navn?.firstOrNull()?.visningsnavn() ?: it.first?.navn?.verdi ?: "Ubestemt"
+        val fødselsnummer = it.second?.key ?: it.first?.fødselsnummer?.verdi?.verdi
+        val termindatoFødselsdato = it.second?.value?.fødsel?.firstOrNull()?.fødselsdato ?: it.first?.fødselTermindato?.verdi
+        val begrunnelseIkkeOppgittAnnenForelder = it.first?.annenForelder?.verdi?.ikkeOppgittAnnenForelderBegrunnelse?.verdi
+        val annenForelder = tilAnnenForelder(it, barneforeldre)
+        val aleneomsorg = it.first?.
+        val søkersRelasjonTilBarnet =
+        val skalBoBorHosSøker = it.first?.harSkalHaSammeAdresse?.verdi
+        val deltBosted = tilDeltBosted(it.second?.value?.deltBosted?.firstOrNull())
+        val fraRegister = it.second != null
+
+        return Barn(navn,
+                    fødselsnummer,
+                    termindatoFødselsdato,
+                    begrunnelseIkkeOppgittAnnenForelder,
+                    annenForelder,
+                    aleneomsorg,
+                    søkersRelasjonTilBarnet,
+                    skalBoBorHosSøker,
+                    deltBosted,
+                    fraRegister)
+    }
+
+    private fun tilDeltBosted(firstOrNull: no.nav.familie.ef.sak.integration.dto.pdl.DeltBosted?): DeltBosted {
 
 
-        søknadsbarn.map { tilBarn(it, pdlBarnMap[it.fødselsnummer?.verdi?.verdi], barneforeldre) }
-
-        val pdlBarn = pdlBarnMap.value
-
-        val søknadBarn =
-
-
-                søknadBarn.annenForelder?.verdi?.kanIkkeOppgiAnnenForelderFar
-
-        Barn(pdlBarn.navn.gjeldende().visningsnavn(),
-             pdlBarnMap.key,
-             pdlBarn.fødsel.first().fødselsdato,
-             søknadsbarn.folkeregisterbarn.verdi.tilAnnenForelder(pdlBarn,
-                                                                  barneforeldre,
-                                                                  søknadsbarn)
     }
 
     private fun finnMatchendePdlBarn(søknadsbarn: Søknadsbarn, pdlBarnMap: Map<String, PdlBarn>): PdlBarn? {
@@ -86,7 +90,7 @@ object AleneomsorgMapper {
         TODO("Not yet implemented")
     }
 
-    private fun tilAnnenForelder(pdlBarn: PdlBarn, barneforeldre: Map<String, PdlAnnenForelder>): AnnenForelder? {
+    private fun tilAnnenForelder(pdlBarn: Pair<no.nav.familie.kontrakter.ef.søknad.Barn?, Map.Entry<String, PdlBarn>?>, barneforeldre: Map<String, PdlAnnenForelder>): AnnenForelder? {
 
         if (pdlBarn.familierelasjoner.isEmpty()) {
             return null
