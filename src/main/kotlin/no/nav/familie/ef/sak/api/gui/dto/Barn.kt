3758780c8d941e502d8be07955487a42f9ad1b4d
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-06-24 14:46:56 +0200

    wip

--- src/main/kotlin/no/nav/familie/ef/sak/api/gui/dto/Barn.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/gui/dto/Barn.kt
@@ -3,9 +3,9 @@ package no.nav.familie.ef.sak.api.gui.dto
 import java.time.LocalDate
 
 data class Barn(val navn: String,
-                val fødselsnummer: String,
+                val fødselsnummer: String?,
                 val termindatoFødselsdato: LocalDate?,
-                val begrunnelseIkkeOppgittAnnenForelder: String,
+                val begrunnelseIkkeOppgittAnnenForelder: String?,
                 val annenForelder: AnnenForelder?,
                 val aleneomsorg: Boolean,
                 val søkersRelasjonTilBarnet: String,
