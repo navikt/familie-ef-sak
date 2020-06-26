8c221657d5723ba6aaacd6bd1507096188e51b98
Johan Blomgren <johan.blomgren@nav.no>
2020-06-26 16:35:05 +0200

    CachedKodeverkService blir en egen klasse

--- src/main/kotlin/no/nav/familie/ef/sak/service/KodeverkService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/service/KodeverkService.kt
@@ -1,31 +1,11 @@
 package no.nav.familie.ef.sak.service
 
-import no.nav.familie.ef.sak.integration.FamilieIntegrasjonerClient
-import no.nav.familie.kontrakter.felles.kodeverk.KodeverkDto
 import no.nav.familie.kontrakter.felles.kodeverk.hentGjelende
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
 @Service
 class KodeverkService(private val cachedKodeverkService: CachedKodeverkService) {
-
-    /**
-     *  En metode som ikke er @Cacheable i en bean som kaller en annen metode som har @Cacheable bruker aldri cached
-     */
-    @Service
-    class CachedKodeverkService(private val familieIntegrasjonerClient: FamilieIntegrasjonerClient) {
-
-        @Cacheable("kodeverk_landkoder", sync = true)
-        fun hentLandkoder(): KodeverkDto {
-            return familieIntegrasjonerClient.hentKodeverkLandkoder()
-        }
-
-        @Cacheable("kodeverk_poststed", sync = true)
-        fun hentPoststed(): KodeverkDto {
-            return familieIntegrasjonerClient.hentKodeverkPoststed()
-        }
-    }
 
     fun hentLand(landkode: String, gjeldendeDato: LocalDate): String? {
         return cachedKodeverkService.hentLandkoder().hentGjelende(landkode, gjeldendeDato)
