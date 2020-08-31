09ef9fde66640322069c3b20b8c30380ad8cfab8
Johan Blomgren <blommish@users.noreply.github.com>
2020-08-31 15:06:43 +0200

    Fikset config for swagger 3.0.0 (#220)
    * Fikset config for swagger 3.0.0

--- src/main/kotlin/no/nav/familie/ef/sak/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/config/ApplicationConfig.kt
@@ -11,13 +11,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootConfiguration
 @ConfigurationPropertiesScan
 @ComponentScan("no.nav.familie.ef.sak", "no.nav.familie.sikkerhet")
-@EnableSwagger2
-@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
+@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger"])
 @Import(RestTemplateAzure::class, RestTemplateSts::class, StsRestClient::class)
 @EnableOAuth2Client(cacheEnabled = true)
 class ApplicationConfig {
