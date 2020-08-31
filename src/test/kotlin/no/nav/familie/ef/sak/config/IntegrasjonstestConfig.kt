f612f857f709ddc1a6a221251965aca2c3ffab9e
Martine Enger <marenger@student.matnat.uio.no>
2020-08-31 09:05:45 +0200

    [kaizen] Rename config-fil for integrasjonstest

--- src/test/kotlin/no/nav/familie/ef/sak/config/IntegrasjonstestConfig.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/config/IntegrasjonstestConfig.kt
@@ -8,4 +8,4 @@ import org.springframework.context.annotation.Profile
 @Profile("integrasjonstest")
 @Import(TokenGeneratorConfiguration::class)
 @Configuration
-class ControllerTestConfig
+class IntegrasjonstestConfig
