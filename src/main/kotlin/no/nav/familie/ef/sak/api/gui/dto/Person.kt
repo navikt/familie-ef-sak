b74a1a169133b6fb1f52a86ae0113c0972ad07d0
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-09-14 12:26:43 +0200

    Fjern kode som ikke er i bruk - skal bruke pdl for Ãbrukerdata

--- src/main/kotlin/no/nav/familie/ef/sak/api/gui/dto/Person.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.familie.ef.sak.api.gui.dto
-
-import no.nav.familie.ef.sak.integration.dto.personopplysning.PersonhistorikkInfo
-import no.nav.familie.ef.sak.integration.dto.personopplysning.Personinfo
-
-class Person(val personinfo: Personinfo,
-             val personhistorikkInfo: PersonhistorikkInfo,
-             val pdlData: Map<String, Any>)
