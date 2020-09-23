2d90f537ba59b52b21f233b46fec5fbc12bff16d
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-09-23 13:21:20 +0200

    Fjern id fra fagsakPerson da dette ikke er nødvendig

--- src/test/kotlin/no/nav/familie/ef/sak/OppslagSpringRunnerTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/OppslagSpringRunnerTest.kt
@@ -43,7 +43,6 @@ abstract class OppslagSpringRunnerTest {
                TilkjentYtelse::class,
                Oppgave::class,
                Behandling::class,
-               FagsakPerson::class,
                Fagsak::class
         ).forEach { jdbcAggregateOperations.deleteAll(it.java) }
     }
