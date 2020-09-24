b72851818de0e0fce75dbfeaea3d977e40b66134
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-09-24 10:53:27 +0200

    Forenkle FinnOppgaveRequestDto for første versjon

--- src/main/kotlin/no/nav/familie/ef/sak/api/oppgave/FinnOppgaveRequestDto.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/oppgave/FinnOppgaveRequestDto.kt
@@ -5,40 +5,37 @@ import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveRequest
 import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import no.nav.familie.kontrakter.felles.oppgave.Tema
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 data class FinnOppgaveRequestDto(
-        val behandlingstema: Behandlingstema? = null,
-        val oppgavetype: Oppgavetype? = null,
+        val behandlingstema: String? = null,
+        val oppgavetype: String? = null,
         val enhet: String? = null,
         val saksbehandler: String? = null,
         val journalpostId: String? = null,
         val tilordnetRessurs: String? = null,
         val tildeltRessurs: Boolean? = null,
-        val opprettetFomTidspunkt: LocalDateTime? = null,
-        val opprettetTomTidspunkt: LocalDateTime? = null,
-        val fristFomDato: LocalDate? = null,
-        val fristTomDato: LocalDate? = null,
-        val aktivFomDato: LocalDate? = null,
-        val aktivTomDato: LocalDate? = null,
+        val opprettet: LocalDate? = null,
+        val frist: LocalDate? = null,
         val limit: Long? = null,
         val offset: Long? = null) {
 
     fun tilFinnOppgaveRequest(): FinnOppgaveRequest = FinnOppgaveRequest(
             tema = Tema.ENF,
-            behandlingstema = this.behandlingstema,
-            oppgavetype = this.oppgavetype,
+            behandlingstema = if (this.behandlingstema != null) Behandlingstema.values()
+                    .find { it.value == this.behandlingstema } else null,
+            oppgavetype = if (this.oppgavetype != null) Oppgavetype.values()
+                    .find { it.value == this.oppgavetype } else null,
             enhet = this.enhet,
             saksbehandler = this.saksbehandler,
             journalpostId = this.journalpostId,
             tildeltRessurs = this.tildeltRessurs,
             tilordnetRessurs = this.tilordnetRessurs,
-            opprettetFomTidspunkt = this.opprettetFomTidspunkt,
-            opprettetTomTidspunkt = this.opprettetTomTidspunkt,
-            fristFomDato = this.fristFomDato,
-            fristTomDato = this.fristTomDato,
-            aktivFomDato = this.aktivFomDato,
-            aktivTomDato = this.aktivTomDato,
-            limit = this.limit,
-            offset = this.offset)
+            opprettetFomTidspunkt = this.opprettet?.atStartOfDay(),
+            opprettetTomTidspunkt = this.opprettet?.plusDays(1)?.atStartOfDay(),
+            fristFomDato = this.frist,
+            fristTomDato = this.frist,
+            aktivFomDato = null,
+            aktivTomDato = null,
+            limit = 150,
+            offset = 0)
 }
