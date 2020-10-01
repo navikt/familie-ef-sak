8c25d793012d03b773b603606a9273d4be75634f
Johan Blomgren <johan.blomgren@nav.no>
2020-10-01 17:14:03 +0200

    Må slette brukerContext etter test

--- src/test/kotlin/no/nav/familie/ef/sak/repository/RepositoryTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/repository/RepositoryTest.kt
@@ -1,22 +1,17 @@
 package no.nav.familie.ef.sak.no.nav.familie.ef.sak.repository
 
-import io.mockk.every
-import io.mockk.mockk
 import no.nav.familie.ef.sak.OppslagSpringRunnerTest
+import no.nav.familie.ef.sak.no.nav.familie.ef.sak.util.BrukerContextUtil
 import no.nav.familie.ef.sak.repository.CustomRepository
 import no.nav.familie.ef.sak.repository.FagsakRepository
 import no.nav.familie.ef.sak.repository.domain.Endret
 import no.nav.familie.ef.sak.repository.domain.Sporbar
 import no.nav.familie.ef.sak.repository.findByIdOrThrow
-import no.nav.security.token.support.core.context.TokenValidationContext
-import no.nav.security.token.support.core.jwt.JwtTokenClaims
-import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.web.context.request.RequestAttributes
-import org.springframework.web.context.request.RequestContextHolder
 import java.time.LocalDateTime
 
 @ActiveProfiles("integrasjonstest")
@@ -24,6 +19,11 @@ internal class RepositoryTest : OppslagSpringRunnerTest() {
 
     @Autowired lateinit var fagsakRepository: FagsakRepository
     @Autowired lateinit var customRepository: CustomRepository
+
+    @AfterEach
+    internal fun tearDown() {
+        BrukerContextUtil.clearBrukerContext()
+    }
 
     @Test
     internal fun `skal oppdatere sporbar automatisk når en entitet oppdateres`() {
@@ -44,7 +44,7 @@ internal class RepositoryTest : OppslagSpringRunnerTest() {
         assertThat(opprettetSporbar.endret.endretTid).isEqualTo(fagsak.sporbar.endret.endretTid)
 
         val nyEndretAv = "En annen brukere"
-        mockBrukerContext(nyEndretAv)
+        BrukerContextUtil.mockBrukerContext(nyEndretAv)
         val oppdatertFagsak = fagsakRepository.save(fagsak)
         val oppdatertSporbar = fagsakRepository.findByIdOrThrow(fagsak.id).sporbar
 
@@ -53,18 +53,5 @@ internal class RepositoryTest : OppslagSpringRunnerTest() {
         assertThat(oppdatertSporbar.opprettetTid).isEqualTo(opprettetTid)
         assertThat(oppdatertSporbar.endret.endretAv).isEqualTo(nyEndretAv)
         assertThat(oppdatertSporbar.endret.endretTid).isEqualTo(oppdatertFagsak.sporbar.endret.endretTid)
-    }
-
-    private fun mockBrukerContext(preferredUsername: String) {
-        val tokenValidationContext = mockk<TokenValidationContext>()
-        val jwtTokenClaims = mockk<JwtTokenClaims>()
-        val requestAttributes = mockk<RequestAttributes>()
-        RequestContextHolder.setRequestAttributes(requestAttributes)
-        every {
-            requestAttributes.getAttribute(SpringTokenValidationContextHolder::class.java.name,
-                                           RequestAttributes.SCOPE_REQUEST)
-        } returns tokenValidationContext
-        every { tokenValidationContext.getClaims("azuread") } returns jwtTokenClaims
-        every { jwtTokenClaims.get("preferred_username") } returns preferredUsername
     }
 }
