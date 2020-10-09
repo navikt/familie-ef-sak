f1eb7e41cc4a8c7e30cc3a653dd9ba0a54c073f7
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-10-09 15:32:19 +0200

    Stønadstype inneholder Æ og kan ikke ligge i urlen.

--- src/main/kotlin/no/nav/familie/ef/sak/api/fagsak/FagsakController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/fagsak/FagsakController.kt
@@ -1,11 +1,13 @@
 package no.nav.familie.ef.sak.api.fagsak
 
-import no.nav.familie.ef.sak.repository.domain.Stønadstype
 import no.nav.familie.ef.sak.service.FagsakService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/api/fagsak"])
@@ -13,12 +15,11 @@ import org.springframework.web.bind.annotation.*
 @Validated
 class FagsakController(private val fagsakService: FagsakService) {
 
-    @PostMapping("/{stønadstype}")
-    fun hentFagsakForPerson(@PathVariable("stønadstype") stønadstype: Stønadstype,
-                            @RequestBody personIdent: String): Ressurs<FagsakDto> {
+    @PostMapping()
+    fun hentFagsakForPerson(@RequestBody fagsakRequest: FagsakRequest): Ressurs<FagsakDto> {
         // TODO: Sjekk at personen eksisterer
         // TODO: Sjekk "tilgangskontroll" (kode6, 7, egenAnsatt) for personen
-        return Ressurs.success(fagsakService.hentEllerOpprettFagsak(personIdent, stønadstype))
+        return Ressurs.success(fagsakService.hentEllerOpprettFagsak(fagsakRequest.personIdent, fagsakRequest.stønadstype))
     }
 
 }
