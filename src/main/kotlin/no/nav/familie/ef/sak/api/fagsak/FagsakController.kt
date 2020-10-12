3cbed9173a58d4aac2bc70e2e89f2fd3d4cb10b2
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-10-12 10:02:22 +0200

    Sjekk tilgang til bruker mfor uthenting av fagsak

--- src/main/kotlin/no/nav/familie/ef/sak/api/fagsak/FagsakController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/fagsak/FagsakController.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ef.sak.api.fagsak
 
 import no.nav.familie.ef.sak.service.FagsakService
+import no.nav.familie.ef.sak.validering.FagsakPersontilgangConstraint
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
@@ -16,9 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 class FagsakController(private val fagsakService: FagsakService) {
 
     @PostMapping()
-    fun hentFagsakForPerson(@RequestBody fagsakRequest: FagsakRequest): Ressurs<FagsakDto> {
-        // TODO: Sjekk at personen eksisterer
-        // TODO: Sjekk "tilgangskontroll" (kode6, 7, egenAnsatt) for personen
+    fun hentFagsakForPerson(@FagsakPersontilgangConstraint @RequestBody fagsakRequest: FagsakRequest): Ressurs<FagsakDto> {
         return Ressurs.success(fagsakService.hentEllerOpprettFagsak(fagsakRequest.personIdent, fagsakRequest.stønadstype))
     }
 
