68d9016bec628341f574658c579e63bdb169e251
Martine Enger <marenger@student.matnat.uio.no>
2020-11-09 14:42:54 +0100

    [kaizen]

--- src/main/kotlin/no/nav/familie/ef/sak/repository/domain/TilkjentYtelseMedMetaData.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/repository/domain/TilkjentYtelseMedMetaData.kt
@@ -1,9 +1,5 @@
 package no.nav.familie.ef.sak.repository.domain
 
-import org.springframework.jdbc.core.RowMapper
-import java.sql.ResultSet
-import java.util.*
-
 data class TilkjentYtelseMedMetaData(val tilkjentYtelse: TilkjentYtelse, val eksternBehandlingId: Long, val eksternFagsakId: Long)
 
 
