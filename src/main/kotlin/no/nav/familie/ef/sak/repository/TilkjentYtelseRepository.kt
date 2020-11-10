b8a778fb7c8ca7e71c9bb029ec3a72091396f9ec
alexandraback <alexandra1.back@gmail.com>
2020-11-10 07:44:11 +0100

    Fjern obrukt metode

--- src/main/kotlin/no/nav/familie/ef/sak/repository/TilkjentYtelseRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/repository/TilkjentYtelseRepository.kt
@@ -8,12 +8,4 @@ import java.util.*
 interface TilkjentYtelseRepository : RepositoryInterface<TilkjentYtelse, UUID>, InsertUpdateRepository<TilkjentYtelse> {
 
     fun findByPersonident(personident: String): TilkjentYtelse?
-
-    @Query(value = "SELECT t.*, be.id as eksternBehandlingId " +
-            "FROM tilkjent_ytelse t " +
-            "JOIN behandling_ekstern be on t.behandling_id = be.behandling_id " +
-            "WHERE t.id = :tilkjentYtelseId"
-            )
-    fun finn(tilkjentYtelseId: UUID): TilkjentYtelseMedMetaData?
-
 }
