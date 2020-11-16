77f9e543c359ff9396be6bbc2d021726bdb415ec
Martine Enger <marenger@student.matnat.uio.no>
2020-11-16 14:52:14 +0100

    Tar bort endringer på andel tilkjent ytelse fra sql-skript da jeg tror det er bedre å ta i separat pr

--- src/main/resources/db/migration/V22__tilkjent_ytelse_med_sporbar.sql
+++ src/main/resources/db/migration/V22__tilkjent_ytelse_med_sporbar.sql
@@ -1,7 +1,3 @@
-ALTER TABLE andel_tilkjent_ytelse ALTER COLUMN stonad_fom DROP NOT NULL;
-ALTER TABLE andel_tilkjent_ytelse ALTER COLUMN stonad_tom DROP NOT NULL;
-ALTER TABLE andel_tilkjent_ytelse ALTER COLUMN belop DROP NOT NULL;
-
 ALTER TABLE tilkjent_ytelse ADD COLUMN IF NOT EXISTS opprettet_av VARCHAR NOT NULL DEFAULT 'VL';
 ALTER TABLE tilkjent_ytelse ADD COLUMN IF NOT EXISTS endret_av VARCHAR NOT NULL DEFAULT 'VL';
 ALTER TABLE tilkjent_ytelse ADD COLUMN IF NOT EXISTS opprettet_tid TIMESTAMP(3) NOT NULL DEFAULT localtimestamp;
