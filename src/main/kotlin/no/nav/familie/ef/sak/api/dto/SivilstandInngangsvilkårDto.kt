c6f6f388e8eb1b20e0c0842ddd27d4866336ee6e
My Thao Nguyen <my.thao.nguyen@nav.no>
2020-11-16 14:29:27 +0100

    Legg til Sivilstand inngangsvilkår mapper og Dto

---
+++ src/main/kotlin/no/nav/familie/ef/sak/api/dto/SivilstandInngangsvilkårDto.kt
@@ -0,0 +1,18 @@
+package no.nav.familie.ef.sak.api.dto
+
+import java.time.LocalDate
+
+data class SivilstandInngangsvilkårDto(val søknadsgrunnlag: SivilstandSøknadsgrunnlagDto,
+                                       val registergrunnlag: SivilstandRegistergrunnlagDto)
+
+data class SivilstandSøknadsgrunnlagDto(val samlivsbruddsdato: LocalDate?,
+                                        val endringSamværsordningDato: LocalDate?,
+                                        val fraflytningsdato: LocalDate?,
+                                        val erUformeltGift: Boolean?,
+                                        val erUformeltSeparertEllerSkilt: Boolean?,
+                                        val datoSøktSeparasjon: LocalDate?,
+                                        val søktOmSkilsmisseSeparasjon: Boolean?,
+                                        val årsakEnslig: String?)
+
+data class SivilstandRegistergrunnlagDto(val type: Sivilstandstype,
+                                         val gyldigFraOgMed: LocalDate?)
