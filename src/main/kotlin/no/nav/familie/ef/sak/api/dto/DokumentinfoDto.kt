4987f62d79a814a1298e1b30089a2596a888ed49
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-11-16 14:19:51 +0100

    Riktig formatering

--- src/main/kotlin/no/nav/familie/ef/sak/api/dto/DokumentinfoDto.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/dto/DokumentinfoDto.kt
@@ -3,10 +3,9 @@ package no.nav.familie.ef.sak.api.dto
 import no.nav.familie.kontrakter.felles.journalpost.Journalposttype
 import java.time.LocalDate
 
-data class DokumentinfoDto(
-        val dokumentinfoId: String,
-        val filnavn: String?,
-        val tittel: String,
-        val journalpostId: String,
-        val dato: LocalDate?,
-        val journalposttype: Journalposttype)
+data class DokumentinfoDto(val dokumentinfoId: String,
+                           val filnavn: String?,
+                           val tittel: String,
+                           val journalpostId: String,
+                           val dato: LocalDate?,
+                           val journalposttype: Journalposttype)
