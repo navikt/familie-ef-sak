a8988401b97e8ec6258e464b5bf19c060faf4948
eirarset <eirik.arseth@nav.no>
2020-11-19 13:28:12 +0100

    Legger til test på medsending av aktoerId i finnOppgaver

--- src/test/kotlin/no/nav/familie/ef/sak/api/oppgave/OppgaveControllerTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/api/oppgave/OppgaveControllerTest.kt
@@ -1,15 +1,17 @@
 package no.nav.familie.ef.sak.api.oppgave
 
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
+import io.mockk.*
 import no.nav.familie.ef.sak.api.ManglerTilgang
 import no.nav.familie.ef.sak.integration.PdlClient
+import no.nav.familie.ef.sak.integration.dto.pdl.PdlAktørId
+import no.nav.familie.ef.sak.integration.dto.pdl.PdlHentIdenter
+import no.nav.familie.ef.sak.integration.dto.pdl.PdlIdent
 import no.nav.familie.ef.sak.repository.domain.Oppgave
 import no.nav.familie.ef.sak.service.OppgaveService
 import no.nav.familie.ef.sak.service.TilgangService
 import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveRequest
+import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveResponseDto
 import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -24,6 +26,26 @@ internal class OppgaveControllerTest {
 
 
     private val oppgaveController: OppgaveController = OppgaveController(oppgaveService, tilgangService, pdlClient)
+
+    @Test
+    internal fun `skal sende med aktoerId i request `() {
+        val finnOppgaveRequestSlot = slot<FinnOppgaveRequest>()
+
+        every {
+            tilgangService.validerTilgangTilPersonMedBarn(any())
+        } just Runs
+
+        every {
+            tilgangService.validerHarSaksbehandlerrolle()
+        } just Runs
+
+        every { pdlClient.hentAktørId("4321") } returns PdlHentIdenter(PdlAktørId(listOf(PdlIdent("1234"))))
+
+        every { oppgaveService.hentOppgaver(capture(finnOppgaveRequestSlot)) } returns FinnOppgaveResponseDto(0, listOf())
+        oppgaveController.hentOppgaver(FinnOppgaveRequestDto(ident = "4321"))
+
+        assertThat(finnOppgaveRequestSlot.captured.aktoerId).isEqualTo("1234")
+    }
 
     @Test
     internal fun `skal feile hvis bruker er veileder`() {
