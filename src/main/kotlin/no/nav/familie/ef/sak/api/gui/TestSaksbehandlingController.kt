aeecfb74fbe5d527c006bfe138a59368dfe8f83e
Martine Enger <martine.enger@nav.no>
2020-11-24 08:33:32 +0100

    Testdata for testing lokalt og i preprod (#381)
    * Endepunkt som kan lage behandling og fagsak med testsøknad for dolly-bruker
    * Legg til familierelasjoner i mockdata for søker
    * Fjerner gammelt dummy-endepunkt som blir erstattet av nytt
    * Mock for familie-oppdrag lokalt

---
+++ src/main/kotlin/no/nav/familie/ef/sak/api/gui/TestSaksbehandlingController.kt
@@ -0,0 +1,50 @@
+package no.nav.familie.ef.sak.api.gui
+
+import no.nav.familie.ef.sak.integration.dto.pdl.gjeldende
+import no.nav.familie.ef.sak.integration.dto.pdl.visningsnavn
+import no.nav.familie.ef.sak.repository.domain.BehandlingType
+import no.nav.familie.ef.sak.repository.domain.Stønadstype
+import no.nav.familie.ef.sak.service.BehandlingService
+import no.nav.familie.kontrakter.ef.søknad.Testsøknad
+import no.nav.familie.ef.sak.service.FagsakService
+import no.nav.familie.ef.sak.service.PersonService
+import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.context.annotation.Profile
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/api/test")
+@ProtectedWithClaims(issuer = "azuread")
+@Profile("!prod")
+class TestSaksbehandlingController(private val fagsakService: FagsakService,
+                                   private val behandlingService: BehandlingService,
+                                   private val personService: PersonService) {
+
+    @PostMapping(path = ["fagsak"], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun opprettFagsakForTestperson(@RequestBody testFagsakRequest: TestFagsakRequest): Ressurs<UUID> {
+        val fagsakDto =
+                fagsakService.hentEllerOpprettFagsak(testFagsakRequest.personIdent, Stønadstype.OVERGANGSSTØNAD)
+        val fagsak = fagsakService.hentFagsak(fagsakDto.id)
+        val behandling = behandlingService.opprettBehandling(BehandlingType.FØRSTEGANGSBEHANDLING, fagsak.id)
+        val søkerMedBarn = personService.hentPersonMedRelasjoner(testFagsakRequest.personIdent)
+
+        val barnNavnOgFnr = søkerMedBarn.barn.map { Testsøknad.NavnOgFnr(it.value.navn.gjeldende().visningsnavn(), it.key) }
+        val søknad = Testsøknad.søknadOvergangsstønadMedSøker(søker = Testsøknad.NavnOgFnr(søkerMedBarn.søker.navn.gjeldende()
+                                                                                                   .visningsnavn(),
+                                                                                           søkerMedBarn.søkerIdent),
+                                                              barneliste = barnNavnOgFnr)
+        behandlingService.lagreSøknadForOvergangsstønad(søknad,
+                                                        behandling.id,
+                                                        fagsak.id,
+                                                        "TESTJPID")
+        return Ressurs.success(behandling.id)
+    }
+}
+
+data class TestFagsakRequest(val personIdent: String)
