06ad0b159d1e71ec2fad2f8cf2f7bed5659040c3
Alexandra <alexandra1.back@gmail.com>
2020-11-26 10:39:32 +0100

    Lagt til konsistensavstemmingtask for å trigge regelmessig konsistens… (#385)

--- src/test/kotlin/no/nav/familie/ef/sak/task/GrensesnittavstemmingTaskTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/task/GrensesnittavstemmingTaskTest.kt
@@ -4,32 +4,31 @@ import io.mockk.every
 import io.mockk.justRun
 import io.mockk.mockk
 import io.mockk.slot
+import no.nav.familie.ef.sak.api.avstemming.GrensesnittavstemmingDto
 import no.nav.familie.ef.sak.repository.domain.Stønadstype
 import no.nav.familie.ef.sak.service.AvstemmingService
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.prosessering.domene.Task
-import no.nav.familie.prosessering.domene.TaskRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.any
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 internal class GrensesnittavstemmingTaskTest {
 
-    private val taskRepository: TaskRepository = mockk()
-
     private val avstemmingService: AvstemmingService = mockk()
 
     private val grensesnittavstemmingTask: GrensesnittavstemmingTask =
-            GrensesnittavstemmingTask(avstemmingService, taskRepository)
+            GrensesnittavstemmingTask(avstemmingService)
 
     @Test
-    fun `doTask skal kalle avstemmingService med fradato fra payload og dato for triggerTid som parametere`() {
+    fun `doTask skal kalle oppdragClient med fradato fra payload og dato for triggerTid som parametere`() {
         val fradatoSlot = slot<LocalDateTime>()
         val tildatoSlot = slot<LocalDateTime>()
-        val stønadstype = slot<Stønadstype>()
+        val stønadstypeSlot = slot<Stønadstype>()
         justRun {
-            avstemmingService.grensesnittavstemOppdrag(capture(fradatoSlot), capture(tildatoSlot), capture(stønadstype))
+            avstemmingService.grensesnittavstemOppdrag(capture(fradatoSlot), capture(tildatoSlot), capture(stønadstypeSlot))
         }
 
         grensesnittavstemmingTask.doTask(Task(type = "",
@@ -38,28 +37,25 @@ internal class GrensesnittavstemmingTaskTest {
 
         assertThat(fradatoSlot.captured).isEqualTo(LocalDate.of(2018, 4, 18).atStartOfDay())
         assertThat(tildatoSlot.captured).isEqualTo(LocalDate.of(2018, 4, 19).atStartOfDay())
-        assertThat(stønadstype.captured).isEqualTo(Stønadstype.OVERGANGSSTØNAD)
+        assertThat(stønadstypeSlot.captured).isEqualTo(Stønadstype.OVERGANGSSTØNAD)
     }
 
     @Test
     fun `onCompletion skal opprette ny grensesnittavstemmingTask med dato for forrige triggerTid som payload`() {
-        val slot = slot<Task>()
+        val triggeTid = LocalDateTime.of(2018, 4, 18, 8, 0)
+        val slot = slot<GrensesnittavstemmingDto>()
         every {
-            taskRepository.save(capture(slot))
+            avstemmingService.opprettGrensesnittavstemmingTask(capture(slot))
         } answers {
-            slot.captured
+            any()
         }
 
         grensesnittavstemmingTask.onCompletion(Task(type = GrensesnittavstemmingTask.TYPE,
                                                     payload = payload,
-                                                    triggerTid = LocalDateTime.of(2018, 4, 18, 8, 0)))
+                                                    triggerTid = triggeTid))
 
-        assertThat(slot.captured).isEqualToComparingOnlyGivenFields(Task(type = GrensesnittavstemmingTask.TYPE,
-                                                                         payload = payload,
-                                                                         triggerTid = LocalDateTime.of(2018, 4, 19, 8, 0)),
-                                                                    "type",
-                                                                    "payload",
-                                                                    "triggerTid")
+        assertThat(slot.captured).isEqualTo(GrensesnittavstemmingDto(stønadstype = Stønadstype.OVERGANGSSTØNAD,
+                                                                     fraDato = triggeTid.toLocalDate()))
     }
 
     companion object {
