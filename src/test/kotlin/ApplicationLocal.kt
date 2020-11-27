0cec289a536d5801690b5569d4e76110f77594a2
Johan Blomgren <johan.blomgren@nav.no>
2020-11-27 13:48:08 +0100

    Prøver med postgres for tester

--- src/test/kotlin/ApplicationLocal.kt
+++ src/test/kotlin/ApplicationLocal.kt
@@ -1,10 +1,13 @@
 package no.nav.familie.ef.sak
 
 import no.nav.familie.ef.sak.config.ApplicationConfig
+import no.nav.familie.ef.sak.no.nav.familie.ef.sak.database.DbContainerInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.test.context.ContextConfiguration
 
+@ContextConfiguration(initializers = [DbContainerInitializer::class])
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
 class ApplicationLocal
 
