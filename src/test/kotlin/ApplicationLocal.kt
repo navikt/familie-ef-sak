3d73ea60504bd69c740bfc3113645c2f0a0b7234
Johan Blomgren <johan.blomgren@nav.no>
2020-11-30 09:17:10 +0100

    Fikset så att ApplicationLocal og ApplicationLocalPostgres virker

--- src/test/kotlin/ApplicationLocal.kt
+++ src/test/kotlin/ApplicationLocal.kt
@@ -5,15 +5,14 @@ import no.nav.familie.ef.sak.no.nav.familie.ef.sak.database.DbContainerInitializ
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.test.context.ContextConfiguration
 
-@ContextConfiguration(initializers = [DbContainerInitializer::class])
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
 class ApplicationLocal
 
 fun main(args: Array<String>) {
 
     SpringApplicationBuilder(ApplicationConfig::class.java)
+            .initializers(DbContainerInitializer())
             .profiles("local",
                       "mock-integrasjoner",
                       "mock-pdl",
