9567a66986f7b752eee606092fc59602b67fe984
trondil <56258085+throndi@users.noreply.github.com>
2020-12-10 13:08:46 +0100

    Rettelser ift ny testsøknad i ef-kontrakter

--- src/test/kotlin/no/nav/familie/ef/sak/mapper/SøknadsskjemaMapperTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/mapper/SøknadsskjemaMapperTest.kt
@@ -1,10 +1,9 @@
 package no.nav.familie.ef.sak.mapper
 
-import no.nav.familie.kontrakter.ef.søknad.Stønadsstart
-import no.nav.familie.kontrakter.ef.søknad.Søknadsfelt
-import no.nav.familie.kontrakter.ef.søknad.Testsøknad
+import no.nav.familie.kontrakter.ef.søknad.*
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 internal class SøknadsskjemaMapperTest {
 
@@ -19,4 +18,28 @@ internal class SøknadsskjemaMapperTest {
         Assertions.assertThat(søknadTilLagring.søkerFraBestemtMåned).isEqualTo(false)
         Assertions.assertThat(søknadTilLagring.søkerFra).isNull()
     }
+    @Test
+    internal fun `skal mappe samboer fødselsdato`() {
+
+        //val kontraktsøknad = Testsøknad.søknadOvergangsstønad
+
+
+        val nyPerson = TestsøknadBuilder.Builder().defaultPersonMinimum(fødselsdato = LocalDate.now())
+        val søknad = TestsøknadBuilder.Builder().setBosituasjon(samboerdetaljer = nyPerson).build().søknadOvergangsstønad
+
+        /**val nyFødselsdato : Søknadsfelt<LocalDate> = kontraktsøknad.bosituasjon.verdi.samboerdetaljer!!.verdi.fødselsdato!!.copy(verdi = LocalDate.now())
+        val nyPersonMinimum : PersonMinimum = kontraktsøknad.bosituasjon.verdi.samboerdetaljer!!.verdi.copy(fødselsdato = nyFødselsdato)
+        val nyBosituasjon : Bosituasjon = kontraktsøknad.bosituasjon.verdi.copy(samboerdetaljer = Søknadsfelt("", nyPersonMinimum))
+        val nySøknad : SøknadOvergangsstønad = kontraktsøknad.copy(bosituasjon = Søknadsfelt(kontraktsøknad.bosituasjon.label, nyBosituasjon))
+             **/
+        val søknadTilLagring = SøknadsskjemaMapper.tilDomene(søknad)
+        Assertions.assertThat(søknadTilLagring.bosituasjon.samboer!!.fødselsdato!!).isEqualTo(LocalDate.now())
+
+    }
+    @Test
+    internal fun `skal mappe sivilstand`(){
+        val kontraktsøknad = Testsøknad.søknadOvergangsstønad
+        val sivilstandsdetaljer : Sivilstandsdetaljer = kontraktsøknad.sivilstandsdetaljer.verdi
+    }
+
 }
