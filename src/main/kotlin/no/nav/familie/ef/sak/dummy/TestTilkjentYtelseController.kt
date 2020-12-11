f678ad58e840ccd319a45d5e38b9f3efed80b0d0
Johan Blomgren <johan.blomgren@nav.no>
2020-12-11 10:56:21 +0100

    Lagt til endepunkter for å konsistensavstemme i testtilkjentytelse

--- src/main/kotlin/no/nav/familie/ef/sak/dummy/TestTilkjentYtelseController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/dummy/TestTilkjentYtelseController.kt
@@ -2,26 +2,38 @@ package no.nav.familie.ef.sak.dummy
 
 import no.nav.familie.ef.sak.api.dto.AndelTilkjentYtelseDTO
 import no.nav.familie.ef.sak.api.dto.TilkjentYtelseDTO
+import no.nav.familie.ef.sak.repository.domain.Stønadstype
 import no.nav.familie.ef.sak.repository.domain.TilkjentYtelse
+import no.nav.familie.ef.sak.service.AvstemmingService
 import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.oppdrag.KonsistensavstemmingRequest
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.context.annotation.Profile
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping(path = ["/api/test/"])
 @ProtectedWithClaims(issuer = "azuread")
 @Profile("!prod")
-class TestTilkjentYtelseController(private val testTilkjentYtelseService: TestTilkjentYtelseService) {
+class TestTilkjentYtelseController(private val testTilkjentYtelseService: TestTilkjentYtelseService,
+                                   private val avstemmingService: AvstemmingService) {
 
     @PostMapping("/send-til-oppdrag")
     fun testMotOppdrag(@RequestBody tilkjentYtelseTestDTO: TilkjentYtelseTestDTO): Ressurs<TilkjentYtelse> {
 
         return Ressurs.success(testTilkjentYtelseService.lagreTilkjentYtelseOgIverksettUtbetaling(tilkjentYtelseTestDTO))
+    }
+
+    @GetMapping("/konsistensavstemming/{stønadstype}")
+    fun getKonsistensavstemming(@PathVariable stønadstype: Stønadstype): Ressurs<KonsistensavstemmingRequest> {
+        return Ressurs.success(testTilkjentYtelseService.konsistensavstemOppdrag(stønadstype))
+    }
+
+    @PostMapping("/konsistensavstemming/{stønadstype}")
+    fun konsistensavstemming(@PathVariable stønadstype: Stønadstype): Ressurs<String> {
+        avstemmingService.konsistensavstemOppdrag(stønadstype)
+        return Ressurs.success("ok")
     }
 
     @PostMapping("/dummy")
--- src/main/kotlin/no/nav/familie/ef/sak/dummy/TestTilkjentYtelseService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/dummy/TestTilkjentYtelseService.kt
@@ -3,12 +3,17 @@ package no.nav.familie.ef.sak.dummy
 import no.nav.familie.ef.sak.api.fagsak.FagsakDto
 import no.nav.familie.ef.sak.repository.domain.BehandlingStatus
 import no.nav.familie.ef.sak.repository.domain.BehandlingType
+import no.nav.familie.ef.sak.repository.domain.Stønadstype
 import no.nav.familie.ef.sak.repository.domain.TilkjentYtelse
 import no.nav.familie.ef.sak.service.BehandlingService
 import no.nav.familie.ef.sak.service.FagsakService
 import no.nav.familie.ef.sak.service.TilkjentYtelseService
+import no.nav.familie.ef.sak.økonomi.tilKlassifisering
+import no.nav.familie.kontrakter.felles.oppdrag.KonsistensavstemmingRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 
 @Service
@@ -30,6 +35,14 @@ class TestTilkjentYtelseService(private val behandlingService: BehandlingService
         behandlingService.oppdaterStatusPåBehandling(behandlingId = behandling.id,
                                                      status = BehandlingStatus.FERDIGSTILT)
         return opprettTilkjentYtelse
+    }
+
+    fun konsistensavstemOppdrag(stønadstype: Stønadstype): KonsistensavstemmingRequest {
+        val oppdragIdListe = tilkjentYtelseService.finnLøpendeUtbetalninger(datoForAvstemming = LocalDate.now(),
+                                                                            stønadstype = stønadstype)
+        return KonsistensavstemmingRequest(fagsystem = stønadstype.tilKlassifisering(),
+                                           oppdragIdListe = oppdragIdListe,
+                                           avstemmingstidspunkt = LocalDateTime.now())
     }
 
     private fun behandlingType(fagsak: FagsakDto): BehandlingType =
