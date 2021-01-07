32fde59a63ef8ca21e003b6f850542e01fa1867e
Johan Blomgren <blommish@users.noreply.github.com>
2021-01-07 10:59:27 +0100

    InfotrygdFeed bruker ikke ressurs (#434)

--- src/main/kotlin/no/nav/familie/ef/sak/integration/InfotrygdFeedClient.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/integration/InfotrygdFeedClient.kt
@@ -4,8 +4,6 @@ import no.nav.familie.http.client.AbstractPingableRestClient
 import no.nav.familie.kontrakter.ef.infotrygd.OpprettPeriodeHendelseDto
 import no.nav.familie.kontrakter.ef.infotrygd.OpprettStartBehandlingHendelseDto
 import no.nav.familie.kontrakter.ef.infotrygd.OpprettVedtakHendelseDto
-import no.nav.familie.kontrakter.felles.Ressurs
-import no.nav.familie.kontrakter.felles.getDataOrThrow
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -18,27 +16,27 @@ import java.net.URI
 class InfotrygdFeedClient(@Value("\${INFOTRYGD_FEED_API_URL}")
                           private val infotrygdFeedUri: URI,
                           @Qualifier("azure")
-                          restOperations: RestOperations
-) : AbstractPingableRestClient(restOperations, "infotrygd.feed") {
+                          restOperations: RestOperations)
+    : AbstractPingableRestClient(restOperations, "infotrygd.feed") {
 
 
-    private val opprettVedtakUri: URI =
+    val opprettVedtakUri: URI =
             UriComponentsBuilder.fromUri(infotrygdFeedUri).pathSegment("api/entry/vedtak").build().toUri()
-    private val opprettStartBehandlingUri: URI =
+    val opprettStartBehandlingUri: URI =
             UriComponentsBuilder.fromUri(infotrygdFeedUri).pathSegment("api/entry/start-behandling").build().toUri()
-    private val opprettPeriodeUri: URI =
+    val opprettPeriodeUri: URI =
             UriComponentsBuilder.fromUri(infotrygdFeedUri).pathSegment("api/entry/periode").build().toUri()
 
-    fun opprettVedtakHendelse(hendelseDto: OpprettVedtakHendelseDto): String {
-        return postForEntity<Ressurs<String>>(opprettVedtakUri, hendelseDto).getDataOrThrow()
+    fun opprettVedtakHendelse(hendelseDto: OpprettVedtakHendelseDto) {
+        postForEntity<Any>(opprettVedtakUri, hendelseDto)
     }
 
-    fun opprettStartBehandlingHendelse(hendelseDto: OpprettStartBehandlingHendelseDto): String {
-        return postForEntity<Ressurs<String>>(opprettStartBehandlingUri, hendelseDto).getDataOrThrow()
+    fun opprettStartBehandlingHendelse(hendelseDto: OpprettStartBehandlingHendelseDto) {
+        postForEntity<Any>(opprettStartBehandlingUri, hendelseDto)
     }
 
-    fun opprettPeriodeHendelse(hendelseDto: OpprettPeriodeHendelseDto): String {
-        return postForEntity<Ressurs<String>>(opprettPeriodeUri, hendelseDto).getDataOrThrow()
+    fun opprettPeriodeHendelse(hendelseDto: OpprettPeriodeHendelseDto) {
+        postForEntity<Any>(opprettPeriodeUri, hendelseDto)
     }
 
     override val pingUri: URI
