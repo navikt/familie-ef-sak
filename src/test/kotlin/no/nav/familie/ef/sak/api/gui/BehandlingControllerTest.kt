3b7871814ddd0b8c9b0c852032e14969cfe54b75
Johan Blomgren <blommish@users.noreply.github.com>
2021-01-20 09:08:48 +0100

    Fikset package

--- src/test/kotlin/no/nav/familie/ef/sak/api/gui/BehandlingControllerTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/api/gui/BehandlingControllerTest.kt
@@ -1,4 +1,4 @@
-package no.nav.familie.ef.sak.no.nav.familie.ef.sak.api.gui
+package no.nav.familie.ef.sak.api.gui
 
 import no.nav.familie.ef.sak.OppslagSpringRunnerTest
 import no.nav.familie.ef.sak.api.dto.BehandlingDto
