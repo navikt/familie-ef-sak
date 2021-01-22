8619e33ab4d394fc46a9151dc7fd2c06cc80b7fd
Johan Blomgren <blommish@users.noreply.github.com>
2021-01-22 15:38:05 +0100

    Lagt til tilgangskontroll av henting av perioder for arena (#441)

--- src/main/kotlin/no/nav/familie/ef/sak/api/ekstern/PerioderOvergangsstønadController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/ekstern/PerioderOvergangsstønadController.kt
@@ -4,7 +4,6 @@ import no.nav.familie.ef.sak.service.PerioderOvergangsstønadService
 import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadRequest
 import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadResponse
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.springframework.context.annotation.Profile
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -12,17 +11,14 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/api/ekstern/periode/overgangsstonad"],
-        consumes = [APPLICATION_JSON_VALUE],
-        produces = [APPLICATION_JSON_VALUE])
-@ProtectedWithClaims(issuer = "azuread")
+                consumes = [APPLICATION_JSON_VALUE],
+                produces = [APPLICATION_JSON_VALUE])
 @Validated
-@Profile("dev") // TODO Kan slettes når det finnes tilgangssjekk. Uklart hvordan denne kommer bli brukt saksbeh/system?
 class PerioderOvergangsstønadController(private val perioderOvergangsstønadService: PerioderOvergangsstønadService) {
 
     @PostMapping
-    fun hentPerioder(@RequestBody request: PerioderOvergangsstønadRequest,
-                     @RequestParam("delvisOvergangsstonad") boolean: Boolean = false)
-            : PerioderOvergangsstønadResponse {
+    @ProtectedWithClaims(issuer = "sts", claimMap = ["sub=srvArena"])
+    fun hentPerioder(@RequestBody request: PerioderOvergangsstønadRequest): PerioderOvergangsstønadResponse {
         return perioderOvergangsstønadService.hentPerioder(request)
     }
 
