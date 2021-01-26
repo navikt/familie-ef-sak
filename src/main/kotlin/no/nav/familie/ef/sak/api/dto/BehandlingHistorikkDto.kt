6d3ecab3c99ba8cdac0e810a7f483d73ddeab95c
Johan Blomgren <blommish@users.noreply.github.com>
2021-01-26 16:40:25 +0100

    Behandlingshistorikk - Sender med utfall og metadata til frontend

--- src/main/kotlin/no/nav/familie/ef/sak/api/dto/BehandlingHistorikkDto.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/dto/BehandlingHistorikkDto.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.sak.api.dto
 
+import no.nav.familie.ef.sak.repository.domain.StegUtfall
 import no.nav.familie.ef.sak.service.steg.StegType
 import java.time.LocalDateTime
 import java.util.*
@@ -8,4 +9,6 @@ data class BehandlingshistorikkDto(val behandlingId: UUID,
                                    var steg: StegType,
                                    val endretAvNavn: String,
                                    val endretAvMail: String,
-                                   val endretTid: LocalDateTime = LocalDateTime.now())
+                                   val endretTid: LocalDateTime = LocalDateTime.now(),
+                                   val utfall: StegUtfall? = null,
+                                   val metadata: Map<Any, Any>? = null)
