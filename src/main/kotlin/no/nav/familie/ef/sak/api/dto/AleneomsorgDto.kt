84af45ceb7180f50f3659a8761a0e463a7dc0db4
My Thao Nguyen <my.thao.nguyen@nav.no>
2021-01-28 14:33:46 +0100

    Legg til dto for aleneomsorg

---
+++ src/main/kotlin/no/nav/familie/ef/sak/api/dto/AleneomsorgDto.kt
@@ -0,0 +1,45 @@
+package no.nav.familie.ef.sak.api.dto
+
+import java.time.LocalDate
+
+data class AleneomsorgDto(
+    val barneId: String,
+    val søknadsgrunnlag: AleneomsorgSøknadsgrunnlagDto,
+    val registergrunnlagDto: AleneomsorgRegistergrunnlagDto
+)
+
+
+data class AleneomsorgSøknadsgrunnlagDto(
+    val navn: String,
+    val fødselsnummer: String?,
+    val fødselsdato: LocalDate?,
+    val termindato: LocalDate,
+    val skalBoBorHosSøker: Boolean?,
+    val forelder: AnnenForelder?,
+    val ikkeOppgittAnnenForelderBegrunnelse: String?,
+    val spørsmålAvtaleOmDeltBosted: Boolean?,
+    val skalAnnenForelderHaSamvær: String?,
+    val harDereSkriftligAvtaleOmSamvær: String?,
+    val hvordanPraktiseresSamværet: String?,
+    val borAnnenForelderISammeHus: String?,
+    val borAnnenForelderISammeHusBeskrivelse: String?,
+    val harDereTidligereBoddSammen: Boolean?,
+    val nårFlyttetDereFraHverandre: LocalDate?,
+    val hvorMyeErDuSammenMedAnnenForelder: String?,
+    val beskrivSamværUtenBarn: String?
+)
+
+data class AleneomsorgRegistergrunnlagDto(
+    val navn: String,
+    val fødselsnummer: String?,
+    val skalBoBorHosSøker: Boolean?,
+    val forelder: AnnenForelder?,
+    )
+
+data class AnnenForelder(
+    val navn: String,
+    val fødselsnummer: String?,
+    val fødselsdato: LocalDate?,
+    val bosattINorge: Boolean,
+    val land: String?,
+)
--- src/main/kotlin/no/nav/familie/ef/sak/api/dto/InngangsvilkårDto.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/dto/InngangsvilkårDto.kt
@@ -1,10 +1,12 @@
 package no.nav.familie.ef.sak.api.dto
 
-import no.nav.familie.ef.sak.repository.domain.søknad.Bosituasjon
+
 
 data class InngangsvilkårDto(val vurderinger: List<VilkårsvurderingDto>,
                              val grunnlag: InngangsvilkårGrunnlagDto)
 
 data class InngangsvilkårGrunnlagDto(val medlemskap: MedlemskapDto,
                                      val sivilstand: SivilstandInngangsvilkårDto,
-                                     val bosituasjon: BosituasjonDto)
+                                     val bosituasjon: BosituasjonDto,
+                                     val aleneomsorg: List<AleneomsorgDto>
+                                    )
