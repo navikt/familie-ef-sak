0c6b934c9c04685546ab95155a4f8bb8e2119026
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-02-22 12:33:57 +0100

    Bruk riktig task-type for journalføring av blankett

--- src/main/kotlin/no/nav/familie/ef/sak/task/JournalførBlankettTask.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/task/JournalførBlankettTask.kt
@@ -3,14 +3,15 @@ package no.nav.familie.ef.sak.task
 import no.nav.familie.ef.sak.repository.domain.Behandling
 import no.nav.familie.ef.sak.service.BehandlingService
 import no.nav.familie.ef.sak.service.steg.StegService
+import no.nav.familie.ef.sak.sikkerhet.SikkerhetContext
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import org.springframework.stereotype.Service
-import java.util.UUID
+import java.util.*
 
 @Service
-@TaskStepBeskrivelse(taskStepType = IverksettMotOppdragTask.TYPE,
+@TaskStepBeskrivelse(taskStepType = JournalførBlankettTask.TYPE,
                      beskrivelse = "Utfører journalføring av saksbehandlingsblankett")
 class JournalførBlankettTask(private val stegService: StegService,
                              private val behandlingService: BehandlingService) : AsyncTaskStep {
@@ -23,8 +24,14 @@ class JournalførBlankettTask(private val stegService: StegService,
 
     companion object {
 
-        fun opprettTask(behandling: Behandling): Task {
-            return Task(type = TYPE, payload = behandling.id.toString())
+        fun opprettTask(behandling: Behandling, personIdent: String): Task {
+            return Task(type = TYPE,
+                        payload = behandling.id.toString(),
+                        properties = Properties().apply {
+                            this["personIdent"] = personIdent
+                            this["behandlingId"] = behandling.id.toString()
+                            this["saksbehandler"] = SikkerhetContext.hentSaksbehandler()
+                        })
         }
 
         const val TYPE = "utførJournalførBlankett"
