e79caad23bf686a1903560beb954f3336e3680f5
My Thao Nguyen <my.thao.nguyen@nav.no>
2021-02-23 10:33:17 +0100

    Sett argument sivilstandsplaner til ?
    Signed-off-by: My Thao Nguyen <my.thao.nguyen@nav.no>

--- src/main/kotlin/no/nav/familie/ef/sak/mapper/SivilstandsplanerMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/mapper/SivilstandsplanerMapper.kt
@@ -6,11 +6,12 @@ import no.nav.familie.ef.sak.repository.domain.søknad.Sivilstandsplaner
 
 object SivilstandsplanerMapper {
 
-    fun tilDto(sivilstandsplaner: Sivilstandsplaner): SivilstandsplanerDto {
-        val samboerDto = sivilstandsplaner.vordendeSamboerEktefelle?.let { PersonMinimumDto(it.navn, it.fødselsdato, it.fødselsnummer) }
+    fun tilDto(sivilstandsplaner: Sivilstandsplaner?): SivilstandsplanerDto {
+        val samboerDto = sivilstandsplaner?.vordendeSamboerEktefelle?.let { PersonMinimumDto(it.navn, it.fødselsdato, it.fødselsnummer) }
 
-        return SivilstandsplanerDto(sivilstandsplaner.harPlaner,
-                                    sivilstandsplaner.fraDato,
+        return SivilstandsplanerDto(sivilstandsplaner?.harPlaner,
+                                    sivilstandsplaner?.fraDato,
                                     samboerDto)
     }
 }
+
