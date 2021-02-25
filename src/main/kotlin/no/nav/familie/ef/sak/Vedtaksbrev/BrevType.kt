0a32274947266cef68a8d24b6f2522a6af8b14f2
eirarset <eirik.arseth@nav.no>
2021-02-25 09:58:03 +0100

    Legg til BrevType-enums

---
+++ src/main/kotlin/no/nav/familie/ef/sak/Vedtaksbrev/BrevType.kt
@@ -0,0 +1,5 @@
+package no.nav.familie.ef.sak.Vedtaksbrev
+
+enum class BrevType(val mal: String, val arkivMetadataType: String) {
+    VEDTAKSBREV("innvilgetVedtakMVP", "VEDTAKSBREV_OVERGANGSSTØNAD")
+}
