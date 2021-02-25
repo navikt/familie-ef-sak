ff43385903d1e943ed06f4f75900bc3d4a5bb981
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-02-26 00:11:21 +0100

    Første versjon av implementasjon av utfør blankettsteg

--- src/main/kotlin/no/nav/familie/ef/sak/repository/BehandlingRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/repository/BehandlingRepository.kt
@@ -16,11 +16,19 @@ interface BehandlingRepository : RepositoryInterface<Behandling, UUID>, InsertUp
     fun findByFagsakIdAndStatus(fagsakId: UUID, status: BehandlingStatus): List<Behandling>
 
     // language=PostgreSQL
-    @Query("""SELECT b.*, be.id AS eksternId_id         
+    @Query("""SELECT b.*, be.id AS eksternid_id         
                      FROM behandling b         
                      JOIN behandling_ekstern be 
                      ON be.behandling_id = b.id         
                      WHERE be.id = :eksternId""")
     fun finnMedEksternId(eksternId: Long): Behandling?
+
+    // language=PostgreSQL
+    @Query("""SELECT f.id FROM fagsak f
+                    JOIN behandling b ON f.id = b.fagsak_id
+                    JOIN fagsak_person fp ON b.fagsak_id=fp.fagsak_id
+                    WHERE b.id = :behandlingId AND fp.opprettet_tid IN 
+                        (SELECT MAX(opprettet_tid) FROM fagsak_person fp2 WHERE fp2.ident = fp.ident)""")
+    fun finnFnrForBehandlingId(behandlingId: UUID): String
 
 }
--- src/main/kotlin/no/nav/familie/ef/sak/service/steg/BlankettSteg.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/service/steg/BlankettSteg.kt
@@ -1,11 +1,22 @@
 package no.nav.familie.ef.sak.service.steg
 
 import no.nav.familie.ef.sak.api.Feil
+import no.nav.familie.ef.sak.integration.JournalpostClient
+import no.nav.familie.ef.sak.repository.BehandlingRepository
 import no.nav.familie.ef.sak.repository.domain.Behandling
+import no.nav.familie.ef.sak.task.FerdigstillBehandlingTask
+import no.nav.familie.kontrakter.felles.dokarkiv.ArkiverDokumentRequest
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokument
+import no.nav.familie.kontrakter.felles.dokarkiv.FilType
+import no.nav.familie.kontrakter.felles.journalpost.Journalposttype
+import no.nav.familie.prosessering.domene.TaskRepository
 import org.springframework.stereotype.Service
 
 @Service
-class BlankettSteg() : BehandlingSteg<Void?> {
+class BlankettSteg(
+        private val behandlingRepository: BehandlingRepository,
+        private val journalpostClient: JournalpostClient,
+        private val taskRepository: TaskRepository) : BehandlingSteg<Void?> {
 
     override fun validerSteg(behandling: Behandling) {
         if (behandling.steg != stegType()) {
@@ -14,7 +25,15 @@ class BlankettSteg() : BehandlingSteg<Void?> {
     }
 
     override fun utførSteg(behandling: Behandling, data: Void?) {
-        TODO("Not yet implemented")
+
+        val journalpostForBehandling = journalpostClient.hentJournalpost(behandling.journalposter.first().journalpostId)
+        val arkiverDokumentRequest = ArkiverDokumentRequest(
+                behandlingRepository.finnFnrForBehandlingId(behandling.id),
+                true,
+                listOf(Dokument("byteArrayFraDb".toByteArray(), FilType.PDFA, null, "Blankett for overgangsstønad", "OVERGANGSSTØNAD_BLANKETT")), //TODO: Hent blankett fra db
+                listOf(),
+                journalpostForBehandling.sak!!.fagsakId) //TODO: Feilhåndtering av sak som ikke finnes?
+        val response = journalpostClient.arkiverDokument(arkiverDokumentRequest)
     }
 
     override fun stegType(): StegType = StegType.JOURNALFØR_BLANKETT
