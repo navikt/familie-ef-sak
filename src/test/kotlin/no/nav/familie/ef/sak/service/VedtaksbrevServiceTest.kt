acdbafae6457e2fd70b28a08b339435b0766e1c9
eirarset <eirik.arseth@nav.no>
2021-03-01 10:17:14 +0100

    Legger til tester som sjekker om vedtaksbrev blir lagret i db

--- src/test/kotlin/no/nav/familie/ef/sak/service/VedtaksbrevServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/service/VedtaksbrevServiceTest.kt
@@ -5,7 +5,9 @@ import no.nav.familie.ef.sak.no.nav.familie.ef.sak.repository.behandling
 import no.nav.familie.ef.sak.no.nav.familie.ef.sak.repository.fagsak
 import no.nav.familie.ef.sak.repository.BehandlingRepository
 import no.nav.familie.ef.sak.repository.FagsakRepository
+import no.nav.familie.ef.sak.repository.VedtaksbrevRepository
 import no.nav.familie.ef.sak.repository.domain.FagsakPerson
+import no.nav.familie.ef.sak.repository.findByIdOrThrow
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -19,6 +21,7 @@ internal class VedtaksbrevServiceTest: OppslagSpringRunnerTest() {
     @Autowired lateinit var vedtaksbrevService: VedtaksbrevService
     @Autowired private lateinit var fagsakRepository: FagsakRepository
     @Autowired private lateinit var behandlingRepository: BehandlingRepository
+    @Autowired private lateinit var vedtaksbrevRepository: VedtaksbrevRepository
 
     private val fagsak = fagsak(setOf(FagsakPerson("")))
     private val behandling = behandling(fagsak)
@@ -41,6 +44,33 @@ internal class VedtaksbrevServiceTest: OppslagSpringRunnerTest() {
 
         Assertions.assertThat(utkast).usingRecursiveComparison().ignoringFields("brevRequest", "pdf").isEqualTo(endelig)
         Assertions.assertThat(utkast.utkastBrevRequest).usingRecursiveComparison().ignoringFields("signaturBeslutter").isEqualTo(endelig.brevRequest)
+    }
+
+    @Test
+    fun `utkast skal lagres i databasen`(){
+        val utkast = vedtaksbrevService.lagreBrevUtkast(behandlingId = behandling.id)
+        val forventetRequest = vedtaksbrevService.lagBrevRequest(behandlingId = behandling.id)
+
+        Assertions.assertThat(utkast).isEqualTo(vedtaksbrevRepository.findByIdOrThrow(behandling.id))
+        Assertions.assertThat(forventetRequest).isEqualTo(vedtaksbrevRepository.findByIdOrThrow(behandling.id).utkastBrevRequest)
+        Assertions.assertThat(vedtaksbrevRepository.findByIdOrThrow(behandling.id).utkastPdf).isNotNull
+    }
+
+    @Test
+    fun `endelig brev skal lagres i databasen`(){
+        vedtaksbrevService.lagreBrevUtkast(behandlingId = behandling.id)
+        val endelig = vedtaksbrevService.lagreEndeligBrev(behandlingId = behandling.id)
+
+        Assertions.assertThat(endelig).isEqualTo(vedtaksbrevRepository.findByIdOrThrow(behandling.id))
+        Assertions.assertThat(vedtaksbrevRepository.findByIdOrThrow(behandling.id).pdf).isNotNull
+    }
+
+    @Test
+    fun `endelig brev skal ikke lagres ved generering av utkast`(){
+        vedtaksbrevService.lagreBrevUtkast(behandlingId = behandling.id)
+
+        Assertions.assertThat(vedtaksbrevRepository.findByIdOrThrow(behandling.id).brevRequest).isNull()
+        Assertions.assertThat(vedtaksbrevRepository.findByIdOrThrow(behandling.id).pdf).isNull()
     }
 
 }
