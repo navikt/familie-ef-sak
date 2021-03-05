8ebfcaf5e110bf31dff97a29fec0d335a7ff3df4
Johan Blomgren <blommish@users.noreply.github.com>
2021-03-05 15:52:45 +0100

    Apiet for å sjekke om en person finnes i infotrygd har endret seg (#541)

--- src/main/kotlin/no/nav/familie/ef/sak/service/InfotrygdService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/service/InfotrygdService.kt
@@ -4,38 +4,25 @@ import no.nav.familie.ef.sak.integration.InfotrygdReplikaClient
 import no.nav.familie.ef.sak.integration.PdlClient
 import no.nav.familie.ef.sak.repository.domain.SøknadType
 import no.nav.familie.kontrakter.ef.felles.StønadType
-import no.nav.familie.kontrakter.ef.infotrygd.StønadTreff
-import no.nav.familie.kontrakter.ef.infotrygd.SøkFlereStønaderRequest
-import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.kontrakter.ef.infotrygd.InfotrygdSøkRequest
 import org.springframework.stereotype.Service
 
 @Service
 class InfotrygdService(private val infotrygdReplikaClient: InfotrygdReplikaClient,
                        private val pdlClient: PdlClient) {
 
-
-    fun eksisterer(ident: String, søknadType: SøknadType): StønadTreff {
-        val response = eksisterer(ident, setOf(søknadType))
-        val stønadTreff = response[søknadType]
-        requireNotNull(stønadTreff) {
-            "Forventer att man skal få treff på $søknadType men fikk ${
-                objectMapper.writeValueAsString(response)
-            }"
-        }
-        return stønadTreff
-    }
-
     /**
-     * Finner om en person eksisterer i infotrygd og om det finnes noen aktiv sak for personen.
+     * Forslag på sjekk om en person eksisterer i infotrygd
      */
-    fun eksisterer(personIdent: String, søknadTyper: Set<SøknadType>): Map<SøknadType, StønadTreff> {
+    fun eksisterer(personIdent: String, søknadTyper: Set<SøknadType> = SøknadType.values().toSet()): Boolean {
         require(søknadTyper.isNotEmpty()) { "Forventer att søknadstyper ikke er empty" }
         val identer = pdlClient.hentPersonidenter(personIdent, true).identer.map { it.ident }.toSet()
-        val søknadType = søknadTyper.map { it.tilStønadType() }.toSet()
-        return infotrygdReplikaClient.eksistererPerson(SøkFlereStønaderRequest(identer, søknadType)).stønader
-                .map { it.key.tilSøknadType() to it.value }.toMap()
+        val stønadTyper = søknadTyper.map { it.tilStønadType() }.toSet()
+        val response = infotrygdReplikaClient.hentInslagHosInfotrygd(InfotrygdSøkRequest(identer))
+        val harVedtak = response.vedtak.any { stønadTyper.contains(it.stønadType) }
+        val harSak = response.saker.any { stønadTyper.contains(it.stønadType) }
+        return harVedtak || harSak
     }
 
     private fun SøknadType.tilStønadType() = StønadType.valueOf(name)
-    private fun StønadType.tilSøknadType() = SøknadType.valueOf(name)
 }
