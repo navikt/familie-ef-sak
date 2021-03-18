6d6632d80a61c662713b7d8cde62eea855664649
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-03-18 09:35:02 +0100

    Må håndtere at code i pdlExtensions kan være null

--- src/main/kotlin/no/nav/familie/ef/sak/integration/dto/pdl/PdlPerson.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/integration/dto/pdl/PdlPerson.kt
@@ -25,7 +25,7 @@ data class PdlBolkResponse<T>(val data: PersonBolk<T>?, val errors: List<PdlErro
 
 data class PdlError(val message: String,
                     val extensions: PdlExtensions?)
-data class PdlExtensions(val code: String) {
+data class PdlExtensions(val code: String?) {
     fun notFound() = code == "not_found"
 }
 
