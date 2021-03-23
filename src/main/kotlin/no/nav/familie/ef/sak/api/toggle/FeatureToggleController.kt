19430c510e54263a997039cd61522baeb3facd48
trondil <56258085+throndi@users.noreply.github.com>
2021-03-23 20:07:19 +0100

    Squashme

--- src/main/kotlin/no/nav/familie/ef/sak/api/toggle/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/toggle/FeatureToggleController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 @Unprotected
 class FeatureToggleController(private val featureToggleService: FeatureToggleService) {
 
-    val funksjonsbrytere = listOf("familie.ef.soknad.feilsituasjon", "familie.ef.soknad.sprakvelger")
+    val funksjonsbrytere = listOf("familie.ef.sak.journalfoer")
 
     @GetMapping
     fun sjekkAlle(): Map<String, Boolean> {
