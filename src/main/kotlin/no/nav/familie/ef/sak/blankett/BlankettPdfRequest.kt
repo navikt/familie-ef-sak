bdfe912c24df808d1b2b32bccf3769b81e02c83d
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-03-25 10:12:03 +0100

    Skal sende med søknadsdato og søkerStønadFraDato til blankett-generering

--- src/main/kotlin/no/nav/familie/ef/sak/blankett/BlankettPdfRequest.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/blankett/BlankettPdfRequest.kt
@@ -1,12 +1,14 @@
 package no.nav.familie.ef.sak.blankett
 
 import no.nav.familie.ef.sak.api.beregning.VedtakDto
+import no.nav.familie.ef.sak.api.dto.SøknadDatoerDto
 import no.nav.familie.ef.sak.api.dto.VilkårDto
 
 data class BlankettPdfRequest(
         val personopplysninger: PersonopplysningerDto,
         val vilkår: VilkårDto,
         val vedtak: VedtakDto,
+        val søknadsdatoer: SøknadDatoerDto
 )
 
 data class PersonopplysningerDto(
