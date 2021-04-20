30d950abcdb2ea6019d89f4fa02ad2d988094270
Johan Blomgren <blommish@users.noreply.github.com>
2021-04-20 17:10:04 +0200

    Opprydding, fjerner ubrukte metoder og ubrukt imports

--- src/main/kotlin/no/nav/familie/ef/sak/repository/TilkjentYtelseRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/repository/TilkjentYtelseRepository.kt
@@ -2,7 +2,6 @@ package no.nav.familie.ef.sak.repository
 
 import no.nav.familie.ef.sak.repository.domain.Stønadstype
 import no.nav.familie.ef.sak.repository.domain.TilkjentYtelse
-import no.nav.familie.kontrakter.felles.oppdrag.OppdragIdForFagsystem
 import org.springframework.data.jdbc.repository.query.Query
 import java.time.LocalDate
 import java.util.*
