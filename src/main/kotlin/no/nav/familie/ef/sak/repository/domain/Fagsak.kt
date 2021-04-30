6d3bb173500c27deec904d925b13d1b01790f650
Johan Blomgren <blommish@users.noreply.github.com>
2021-04-30 13:30:55 +0200

    Sletter kode som ikke skulle bli med i en commit (#643)

--- src/main/kotlin/no/nav/familie/ef/sak/repository/domain/Fagsak.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/repository/domain/Fagsak.kt
@@ -1,12 +1,10 @@
 package no.nav.familie.ef.sak.repository.domain
 
-import com.fasterxml.jackson.module.kotlin.readValue
-import no.nav.familie.kontrakter.felles.objectMapper
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Embedded
 import org.springframework.data.relational.core.mapping.MappedCollection
-import java.util.*
+import java.util.UUID
 
 data class Fagsak(@Id
                   val id: UUID = UUID.randomUUID(),
@@ -28,23 +26,4 @@ enum class Stønadstype {
     OVERGANGSSTØNAD,
     BARNETILSYN,
     SKOLEPENGER
-}
-enum class ResultatType{
-    HENLEGGE,
-    INNVILGE,
-    AVSLÅ
-}
-sealed class VedtakDto
-class Henlegge (val resultatType: ResultatType = ResultatType.HENLEGGE): VedtakDto()
-class Innvilget(val resultatType: ResultatType = ResultatType.INNVILGE,
-                val periodeBegrunnelse: String,
-                val inntektBegrunnelse: String): VedtakDto()
-class Avslå(val resultatType: ResultatType = ResultatType.AVSLÅ,
-            val avslåBegrunnelse: String) : VedtakDto()
-
-fun main() {
-    val a: VedtakDto = Avslå(resultatType = ResultatType.AVSLÅ, "yolo")
-    val json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(a)
-    val dto = objectMapper.readValue<Avslå>(json)
-    println()
 }
