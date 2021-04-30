e79916a889c2873e0d0704e68eb378b22fddfab5
alexandraback <alexandra1.back@gmail.com>
2021-04-30 08:59:29 +0200

    Lagt til integrasjonstest

--- src/main/kotlin/no/nav/familie/ef/sak/util/ObjectMapperProvider.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/util/ObjectMapperProvider.kt
@@ -5,7 +5,6 @@ import no.nav.familie.ef.sak.api.beregning.VedtakDtoModule
 
 class ObjectMapperProvider {
     companion object {
-
         val objectMapper: ObjectMapper = no.nav.familie.kontrakter.felles.objectMapper.registerModule(VedtakDtoModule())
     }
 }
