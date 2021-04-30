6026a6eb574c3d88a3995ccf3b943845f8dbed54
alexandraback <alexandra1.back@gmail.com>
2021-04-30 12:59:41 +0200

    Fjern logging

--- src/main/kotlin/no/nav/familie/ef/sak/api/beregning/VedtakService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/api/beregning/VedtakService.kt
@@ -3,15 +3,12 @@ package no.nav.familie.ef.sak.api.beregning
 import no.nav.familie.ef.sak.repository.VedtakRepository
 import no.nav.familie.ef.sak.repository.domain.Vedtak
 import no.nav.familie.ef.sak.repository.findByIdOrThrow
-import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class VedtakService(private val vedtakRepository: VedtakRepository) {
-
-    private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
     fun lagreVedtak(vedtakDto: VedtakDto, behandlingId: UUID): UUID {
         return vedtakRepository.insert(vedtakDto.tilVedtak(behandlingId)).behandlingId
@@ -20,7 +17,6 @@ class VedtakService(private val vedtakRepository: VedtakRepository) {
     fun slettVedtakHvisFinnes(behandlingId: UUID) {
         vedtakRepository.findByIdOrNull(behandlingId)
                 ?.let { vedtakRepository.deleteById(behandlingId) }
-                .also { secureLogger.info("Sletter vedtak for behandling=${behandlingId}")}
     }
 
     fun hentVedtak(behandlingId: UUID): Vedtak {
@@ -30,7 +26,5 @@ class VedtakService(private val vedtakRepository: VedtakRepository) {
     fun hentVedtakHvisEksisterer(behandlingId: UUID): VedtakDto? {
         return vedtakRepository.findByIdOrNull(behandlingId)
                 ?.tilVedtakDto()
-                .also { secureLogger.info(it.toString()) }
-
     }
 }
