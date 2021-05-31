a0849a7ca61a7e6d8254ecc867035200773e0a70
eirarset <eirik.arseth@nav.no>
2021-05-31 13:55:22 +0200

    Endre rekkefølge migrering

--- src/main/resources/db/migration/V45__vedtaksbrev_med_signatur.sql
+++ src/main/resources/db/migration/V45__vedtaksbrev_med_signatur.sql
@@ -3,7 +3,7 @@ CREATE TABLE vedtaksbrev (
     behandling_id             UUID PRIMARY KEY REFERENCES behandling (id),
     saksbehandler_brevrequest VARCHAR,
     brevmal                   VARCHAR,
-    beslutter_pdf             BYTEA,
     saksbehandlersignatur     VARCHAR,
-    besluttersignatur         VARCHAR
+    besluttersignatur         VARCHAR,
+    beslutter_pdf             BYTEA
 )
