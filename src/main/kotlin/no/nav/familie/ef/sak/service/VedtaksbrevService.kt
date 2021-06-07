6a11c9095f8dd56519e520d57122659f4970eb06
Johan Blomgren <blommish@users.noreply.github.com>
2021-06-07 12:12:16 +0200

    Legger til endepunkt for å hente brev fra databasen, og legger till sjekk for att behandling er i riktig steg/status for å oppdatere/lage de

--- src/main/kotlin/no/nav/familie/ef/sak/service/VedtaksbrevService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/service/VedtaksbrevService.kt
@@ -1,22 +1,35 @@
 package no.nav.familie.ef.sak.service
 
 import com.fasterxml.jackson.databind.JsonNode
+import no.nav.familie.ef.sak.api.Feil
 import no.nav.familie.ef.sak.repository.VedtaksbrevRepository
+import no.nav.familie.ef.sak.repository.domain.BehandlingStatus
 import no.nav.familie.ef.sak.repository.domain.Fil
 import no.nav.familie.ef.sak.repository.domain.Vedtaksbrev
 import no.nav.familie.ef.sak.repository.findByIdOrThrow
+import no.nav.familie.ef.sak.service.steg.StegType
 import no.nav.familie.ef.sak.sikkerhet.SikkerhetContext
 import no.nav.familie.ef.sak.vedtaksbrev.BrevClient
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class VedtaksbrevService(private val brevClient: BrevClient,
-                         private val brevRepository: VedtaksbrevRepository) {
+                         private val brevRepository: VedtaksbrevRepository,
+                         private val behandlingService: BehandlingService) {
 
+    fun hentBrev(behandlingId: UUID) =
+            brevRepository.findByIdOrThrow(behandlingId).beslutterPdf?.bytes
+            ?: error("Savner beslutterPdf for behandling=$behandlingId")
 
     fun lagBeslutterBrev(behandlingId: UUID): ByteArray {
-        // TODO validere at behandlig har rett status/steg?
+        val behandling = behandlingService.hentBehandling(behandlingId)
+        if (behandling.steg != StegType.BESLUTTE_VEDTAK || behandling.status != BehandlingStatus.FATTER_VEDTAK) {
+            throw Feil("Behandling er i feil steg=${behandling.steg} status=${behandling.status}",
+                       httpStatus = HttpStatus.BAD_REQUEST)
+        }
+
         val vedtaksbrev = brevRepository.findByIdOrThrow(behandlingId)
         val besluttervedtaksbrev = vedtaksbrev.copy(besluttersignatur = SikkerhetContext.hentSaksbehandlerNavn(strict = true))
         val beslutterPdf = Fil(brevClient.genererBrev(besluttervedtaksbrev))
@@ -26,7 +39,12 @@ class VedtaksbrevService(private val brevClient: BrevClient,
     }
 
     fun lagSaksbehandlerBrev(behandlingId: UUID, brevrequest: JsonNode, brevmal: String): ByteArray {
-        // TODO validere at behandlig har rett status/steg?
+        val behandling = behandlingService.hentBehandling(behandlingId)
+        if (behandling.status.behandlingErLåstForVidereRedigering()) {
+            throw Feil("Behandling er i feil steg=${behandling.steg} status=${behandling.status}",
+                       httpStatus = HttpStatus.BAD_REQUEST)
+        }
+
         val saksbehandlersignatur = SikkerhetContext.hentSaksbehandlerNavn(strict = true)
         val vedtaksbrev = when (brevRepository.existsById(behandlingId)) {
             true -> brevRepository.update(Vedtaksbrev(behandlingId,
