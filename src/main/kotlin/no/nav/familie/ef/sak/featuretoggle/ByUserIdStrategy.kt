fd55039b95ae4ba7eefb320f280a69e27ee42218
alexandraback <alexandra1.back@gmail.com>
2021-06-16 14:14:33 +0200

    Logger unleash map

--- src/main/kotlin/no/nav/familie/ef/sak/featuretoggle/ByUserIdStrategy.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/featuretoggle/ByUserIdStrategy.kt
@@ -2,14 +2,19 @@ package no.nav.familie.ef.sak.featuretoggle
 
 import no.finn.unleash.strategy.Strategy
 import no.nav.familie.ef.sak.sikkerhet.SikkerhetContext
+import org.slf4j.LoggerFactory
 
 class ByUserIdStrategy : Strategy {
+
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
     override fun getName(): String {
         return "byUserId";
     }
 
     override fun isEnabled(map: MutableMap<String, String>): Boolean {
+        secureLogger.info("map values " +  map.values)
+        secureLogger.info("map keys " +  map.keys)
         return map["user"]
                        ?.split(',')
                        ?.any { SikkerhetContext.hentSaksbehandler(strict = true) === it }
