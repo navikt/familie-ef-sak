06735d3ce4c88b18871ad69a0ec7afa8c60fdcbb
alexandraback <alexandra1.back@gmail.com>
2021-06-22 16:25:17 +0200

    Neste task er opprettFerdigstillOppgave når man iverksetter tekniskopphør

--- src/main/kotlin/no/nav/familie/ef/sak/service/steg/VentePåTekniskOpphørStatus.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/service/steg/VentePåTekniskOpphørStatus.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.sak.service.steg
 
 import no.nav.familie.ef.sak.iverksett.IverksettClient
 import no.nav.familie.ef.sak.repository.domain.Behandling
+import no.nav.familie.ef.sak.task.FerdigstillBehandlingTask
 import no.nav.familie.ef.sak.task.LagSaksbehandlingsblankettTask
 import no.nav.familie.kontrakter.ef.iverksett.IverksettStatus
 import no.nav.familie.prosessering.domene.TaskRepository
@@ -13,16 +14,15 @@ class VentePåTekniskOpphørStatus(private val iverksettClient: IverksettClient,
     override fun utførSteg(behandling: Behandling, data: Void?) {
         return iverksettClient.hentStatus(behandling.id).let {
             when (it) {
-                IverksettStatus.OK_MOT_OPPDRAG -> opprettLagSaksbehandlingsblankettTask(behandling)
+                IverksettStatus.OK_MOT_OPPDRAG -> opprettFerdigstillOppgave(behandling)
                 else -> throw error("Mottok status $it fra iverksett for behandlingId=${behandling.id}")
             }
         }
     }
 
-    fun opprettLagSaksbehandlingsblankettTask(behandling: Behandling) {
-        taskRepository.save(LagSaksbehandlingsblankettTask.opprettTask(behandling.id))
+    fun opprettFerdigstillOppgave(behandling: Behandling) {
+        taskRepository.save(FerdigstillBehandlingTask.opprettTask(behandling))
     }
-
     override fun stegType(): StegType {
         return StegType.VENTE_PÅ_TEKNISK_OPPHØR_STATUS
     }
