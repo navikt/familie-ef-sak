4145e230ad41ba5424d8f2ad8749447b6c694a68
Johan Blomgren <blommish@users.noreply.github.com>
2021-07-02 13:53:42 +0200

    Endrer port på mock for integrasjoner då gcloud auth login bruker den samme

--- src/test/kotlin/no/nav/familie/ef/sak/config/FamilieIntegrasjonerMock.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/config/FamilieIntegrasjonerMock.kt
@@ -78,7 +78,7 @@ class FamilieIntegrasjonerMock(integrasjonerConfig: IntegrasjonerConfig) {
     @Bean("mock-integrasjoner")
     @Profile("mock-integrasjoner")
     fun integrationMockServer(): WireMockServer {
-        val mockServer = WireMockServer(8085)
+        val mockServer = WireMockServer(8385)
         responses.forEach {
             mockServer.stubFor(it)
         }
