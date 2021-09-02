bf3a1a359960835a169159ffd55abd1046322737
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-09-02 10:28:29 +0200

    Fiks integrasjonstest etter endring av api

--- src/test/kotlin/no/nav/familie/ef/sak/config/IverksettClientMock.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/config/IverksettClientMock.kt
@@ -1,8 +1,6 @@
 package no.nav.familie.ef.sak.no.nav.familie.ef.sak.config
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.github.jknack.handlebars.internal.antlr.misc.Utils.readFile
-import com.github.tomakehurst.wiremock.client.WireMock
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
@@ -10,20 +8,10 @@ import io.mockk.mockk
 import no.nav.familie.ef.sak.iverksett.IverksettClient
 import no.nav.familie.kontrakter.ef.iverksett.IverksettStatus
 import no.nav.familie.kontrakter.felles.objectMapper
-import no.nav.familie.kontrakter.felles.simulering.BetalingType
-import no.nav.familie.kontrakter.felles.simulering.DetaljertSimuleringResultat
-import no.nav.familie.kontrakter.felles.simulering.FagOmrådeKode
-import no.nav.familie.kontrakter.felles.simulering.MottakerType
-import no.nav.familie.kontrakter.felles.simulering.PosteringType
-import no.nav.familie.kontrakter.felles.simulering.SimuleringMottaker
-import no.nav.familie.kontrakter.felles.simulering.SimulertPostering
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
-import java.io.File
-import java.math.BigDecimal
-import java.time.LocalDate
 
 @Configuration
 @Profile("mock-iverksett")
@@ -33,15 +21,6 @@ class IverksettClientMock {
     @Primary
     fun iverksettClient(): IverksettClient {
         val iverksettClient = mockk<IverksettClient>()
-
-        val simulertPostering = SimulertPostering(fagOmrådeKode = FagOmrådeKode.ENSLIG_FORSØRGER,
-                                                  fom = LocalDate.of(2021, 1, 1),
-                                                  tom = LocalDate.of(2021, 12, 31),
-                                                  betalingType = BetalingType.DEBIT,
-                                                  beløp = BigDecimal.valueOf(15000L),
-                                                  posteringType = PosteringType.YTELSE,
-                                                  forfallsdato = LocalDate.of(2021, 1, 15),
-                                                  utenInntrekk = false)
 
         every { iverksettClient.simuler(any()) } returns objectMapper.readValue(readFile("simuleringsresultat.json"))
 
