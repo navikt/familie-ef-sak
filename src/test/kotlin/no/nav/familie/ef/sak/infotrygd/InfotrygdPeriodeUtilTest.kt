c41044e2db502d3ab05c6a91fe64fb0de5b94a18
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-29 10:55:39 +0200

    Lagt til noen flere tester
    Signed-off-by: Asbjørn Steinskog <asbjorn@steinskog.me>

--- src/test/kotlin/no/nav/familie/ef/sak/infotrygd/InfotrygdPeriodeUtilTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/infotrygd/InfotrygdPeriodeUtilTest.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ef.sak.infotrygd
 import no.nav.familie.ef.sak.no.nav.familie.ef.sak.infotrygd.InfotrygdPeriodeParser
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 internal class InfotrygdPeriodeUtilTest {
 
@@ -13,5 +14,22 @@ internal class InfotrygdPeriodeUtilTest {
         assertThat(perioder).hasSize(3)
     }
 
-    private fun parseFil(fil: String) = InfotrygdPeriodeParser.parse(this::class.java.classLoader.getResource(fil))
+    @Test
+    internal fun `enkel case med hopp mellom perioder`() {
+        val perioder = parseFil("infotrygd/enkel_case_med_hopp.csv")
+        assertThat(perioder).hasSize(3)
+        val førstePeriode = perioder.first()
+        assertThat(førstePeriode.beløp).isEqualTo(18723)
+    }
+
+    @Test
+    internal fun `samme start dato`() {
+        val perioder = parseFil("infotrygd/samme_start_dato.csv")
+        assertThat(perioder).hasSize(3)
+        val førstePeriode = perioder.first()
+        assertThat(førstePeriode.beløp).isEqualTo(18723)
+    }
+
+    private fun parseFil(fil: String) = InfotrygdPeriodeParser.parse(this::class.java.classLoader.getResource(fil)!!)
+
 }
