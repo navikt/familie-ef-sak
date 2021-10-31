825844e80460e4b70b276e4fc834ff0e5c621517
eirarset <eirik.arseth@nav.no>
2021-11-01 00:29:54 +0100

    Skal returnere mellomlagret brev med enum som avhenig av brevtype

--- src/main/kotlin/no/nav/familie/ef/sak/brev/MellomlagringBrevService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/brev/MellomlagringBrevService.kt
@@ -1,17 +1,23 @@
 package no.nav.familie.ef.sak.brev
 
+import no.nav.familie.ef.sak.brev.domain.Fritekstbrev
 import no.nav.familie.ef.sak.brev.domain.MellomlagretBrev
-import no.nav.familie.ef.sak.brev.dto.MellomlagretBrevResponseDto
+import no.nav.familie.ef.sak.brev.domain.MellomlagretFritekstbrev
+import no.nav.familie.ef.sak.brev.dto.MellomlagretBrevFritekst
+import no.nav.familie.ef.sak.brev.dto.MellomlagretBrevResponse
+import no.nav.familie.ef.sak.brev.dto.MellomlagretBrevSanity
+import no.nav.familie.ef.sak.brev.dto.VedtaksbrevFritekstDto
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.util.UUID
 
 @Service
-class MellomlagringBrevService(private val mellomlagerBrevRepository: MellomlagerBrevRepository) {
+class MellomlagringBrevService(private val mellomlagerBrevRepository: MellomlagerBrevRepository,
+                               private val mellomlagerFritekstbrevRepository: MellomlagerFritekstbrevRepository) {
 
     fun mellomLagreBrev(behandlingId: UUID, brevverdier: String, brevmal: String, sanityVersjon: String): UUID {
-
+        mellomlagerFritekstbrevRepository.deleteById(behandlingId)
         return when (val mellomlagretBrev = mellomlagerBrevRepository.findByIdOrNull(behandlingId)) {
             null -> mellomlagerBrevRepository.insert(MellomlagretBrev(behandlingId,
                                                                       brevverdier,
@@ -26,10 +32,25 @@ class MellomlagringBrevService(private val mellomlagerBrevRepository: Mellomlage
 
     }
 
-    fun hentOgValiderMellomlagretBrev(behhandlingId: UUID, sanityVersjon: String): MellomlagretBrevResponseDto? {
-        val mellomlagretBrev = mellomlagerBrevRepository.findByIdOrNull(behhandlingId)
-        if (sanityVersjon == mellomlagretBrev?.sanityVersjon) {
-            return MellomlagretBrevResponseDto(brevverdier = mellomlagretBrev.brevverdier, brevmal = mellomlagretBrev.brevmal)
+    fun mellomlagreFritekstbrev(mellomlagretBrev: VedtaksbrevFritekstDto): UUID {
+        mellomlagerBrevRepository.deleteById(mellomlagretBrev.behandlingId)
+        val mellomlagretFritekstbrev = MellomlagretFritekstbrev(mellomlagretBrev.behandlingId,
+                                                                Fritekstbrev(overskrift = mellomlagretBrev.overskrift,
+                                                                             avsnitt = mellomlagretBrev.avsnitt))
+
+        return mellomlagerFritekstbrevRepository.insert(mellomlagretFritekstbrev).behandlingId
+    }
+
+    fun hentOgValiderMellomlagretBrev(behhandlingId: UUID, sanityVersjon: String): MellomlagretBrevResponse? {
+        mellomlagerBrevRepository.findByIdOrNull(behhandlingId)?.let {
+            if (sanityVersjon == it.sanityVersjon) {
+                return MellomlagretBrevSanity(brevverdier = it.brevverdier,
+                                              brevmal = it.brevmal)
+            }
+            return null
+        }
+        mellomlagerFritekstbrevRepository.findByIdOrNull(behhandlingId)?.let {
+            return MellomlagretBrevFritekst(brev = it.brev)
         }
         return null
     }
