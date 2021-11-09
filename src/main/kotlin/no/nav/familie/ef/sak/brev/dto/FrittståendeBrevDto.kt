d5ee931bf167db60567ff86c3f34a627beea68a6
Viktor <viktor.grondalen.solberg@nav.no>
2021-11-09 13:01:01 +0100

    oppdatert kontrakter slik at utsendelse av frittstående brev ikke behøver stønadstype

--- src/main/kotlin/no/nav/familie/ef/sak/brev/dto/FrittståendeBrevDto.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/brev/dto/FrittståendeBrevDto.kt
@@ -1,12 +1,10 @@
 package no.nav.familie.ef.sak.brev.dto
 
-import no.nav.familie.kontrakter.ef.felles.StønadType
 import java.util.UUID
 
 data class FrittståendeBrevDto(val overskrift: String,
                                val avsnitt: List<FrittståendeBrevAvsnitt>,
                                val fagsakId: UUID,
-                               val stønadType: StønadType,
                                val brevType: FrittståendeBrevKategori)
 
 data class FrittståendeBrevAvsnitt(val deloverskrift: String, val innhold: String, val skalSkjulesIBrevbygger: Boolean? = false)
