c62e3b91b908415e0985a40ae5cbe701d4c8b529
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-11-09 16:17:05 +0100

    Legger til uthenting av behandligIDer av personidenter

--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingController.kt
@@ -10,19 +10,26 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
-@RequestMapping(path = ["/api/ekstern/behandling"],
-                consumes = [MediaType.APPLICATION_JSON_VALUE],
-                produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping(
+    path = ["/api/ekstern/behandling"],
+    consumes = [MediaType.APPLICATION_JSON_VALUE],
+    produces = [MediaType.APPLICATION_JSON_VALUE]
+)
 @Validated
-class EksternBehandlingController(private val pdlClient: PdlClient,
-                                  private val behandlingRepository: BehandlingRepository) {
+class EksternBehandlingController(
+    private val pdlClient: PdlClient,
+    private val behandlingRepository: BehandlingRepository
+) {
 
 
     /**
@@ -34,8 +41,10 @@ class EksternBehandlingController(private val pdlClient: PdlClient,
      */
     @PostMapping("finnes")
     @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
-    fun finnesBehandlingForPerson(@RequestParam("type") stønadstype: Stønadstype?,
-                                  @RequestBody request: PersonIdent): Ressurs<Boolean> {
+    fun finnesBehandlingForPerson(
+        @RequestParam("type") stønadstype: Stønadstype?,
+        @RequestBody request: PersonIdent
+    ): Ressurs<Boolean> {
         val personidenter = pdlClient.hentPersonidenter(request.ident, historikk = true).identer()
         return finnesBehandlingFor(personidenter, stønadstype)
     }
@@ -46,13 +55,39 @@ class EksternBehandlingController(private val pdlClient: PdlClient,
      */
     @PostMapping("finnes/flere-identer")
     @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
-    fun finnesBehandlingForPersonIdenter(@RequestParam("type") stønadstype: Stønadstype?,
-                                         @RequestBody personidenter: Set<String>): Ressurs<Boolean> {
+    fun finnesBehandlingForPersonIdenter(
+        @RequestParam("type") stønadstype: Stønadstype?,
+        @RequestBody personidenter: Set<String>
+    ): Ressurs<Boolean> {
         return finnesBehandlingFor(personidenter, stønadstype)
     }
 
-    private fun finnesBehandlingFor(personidenter: Set<String>,
-                                    stønadstype: Stønadstype?): Ressurs<Boolean> {
+    @GetMapping("hent/{personidenter}")
+    @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
+    fun hentAlleBehandlingIDerAvPersonidenter(
+        @PathVariable personidenter: Set<String>
+    ): Ressurs<Set<UUID>> {
+        return hentAlleBehandlingIDer(personidenter)
+    }
+
+    private fun hentAlleBehandlingIDer(personidenter: Set<String>): Ressurs<Set<UUID>> {
+        if (personidenter.isEmpty()) {
+            return Ressurs.failure("Finner ikke identer til personen")
+        }
+        if (personidenter.any { it.length != 11 }) {
+            return Ressurs.failure("Støtter kun identer av typen fnr/dnr")
+        }
+        var behandlinger = mutableSetOf<UUID>()
+        Stønadstype.values().forEach {
+            behandlingRepository.finnSisteBehandlingSomIkkeErBlankett(it, personidenter)?.let { behandlinger.add(it.id) }
+        }
+        return Ressurs.success(behandlinger)
+    }
+
+    private fun finnesBehandlingFor(
+        personidenter: Set<String>,
+        stønadstype: Stønadstype?
+    ): Ressurs<Boolean> {
         if (personidenter.isEmpty()) {
             return Ressurs.failure("Finner ikke identer til personen")
         }
@@ -71,8 +106,10 @@ class EksternBehandlingController(private val pdlClient: PdlClient,
      * Hvis siste behandling er teknisk opphør, skal vi returnere false,
      * hvis ikke så skal vi returnere true hvis det finnes en behandling
      */
-    private fun eksistererBehandlingSomIkkeErBlankett(stønadstype: Stønadstype,
-                                                      personidenter: Set<String>): Boolean {
+    private fun eksistererBehandlingSomIkkeErBlankett(
+        stønadstype: Stønadstype,
+        personidenter: Set<String>
+    ): Boolean {
         return behandlingRepository.finnSisteBehandlingSomIkkeErBlankett(stønadstype, personidenter)?.let {
             it.type != BehandlingType.TEKNISK_OPPHØR
         } ?: false
