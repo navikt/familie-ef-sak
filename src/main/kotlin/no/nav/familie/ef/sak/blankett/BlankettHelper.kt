c008eeb0d5552ff25e1fdb2970fd6d4ec0aede92
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-11-10 13:30:58 +0100

    Skal journalføre blankett også når vi saksbehandler vanlige saker

---
+++ src/main/kotlin/no/nav/familie/ef/sak/blankett/BlankettHelper.kt
@@ -0,0 +1,45 @@
+package no.nav.familie.ef.sak.blankett
+
+import no.nav.familie.kontrakter.felles.dokarkiv.Dokumenttype
+import no.nav.familie.kontrakter.felles.dokarkiv.v2.ArkiverDokumentRequest
+import no.nav.familie.kontrakter.felles.dokarkiv.v2.Dokument
+import no.nav.familie.kontrakter.felles.dokarkiv.v2.Filtype
+
+object BlankettHelper {
+
+    fun lagArkiverBlankettRequestMotInfotrygd(personIdent: String,
+                                              pdf: ByteArray,
+                                              enhet: String,
+                                              fagsakId: String?): ArkiverDokumentRequest =
+            lagArkiverDokumentRequest(personIdent = personIdent,
+                                      pdf = pdf,
+                                      fagsakId = fagsakId,
+                                      enhet = enhet,
+                                      dokumenttype = Dokumenttype.OVERGANGSSTØNAD_BLANKETT)
+
+    fun lagArkiverBlankettRequestMotNyLøsning(personIdent: String,
+                                              pdf: ByteArray,
+                                              enhet: String,
+                                              fagsakId: Long): ArkiverDokumentRequest =
+            lagArkiverDokumentRequest(personIdent = personIdent,
+                                      pdf = pdf,
+                                      fagsakId = fagsakId.toString(),
+                                      enhet = enhet,
+                                      dokumenttype = Dokumenttype.OVERGANGSSTØNAD_BLANKETT_SAKSBEHANDLING)
+
+    private fun lagArkiverDokumentRequest(personIdent: String,
+                                          pdf: ByteArray,
+                                          fagsakId: String?,
+                                          enhet: String,
+                                          dokumenttype: Dokumenttype): ArkiverDokumentRequest {
+        val dokument = Dokument(pdf, Filtype.PDFA, null, "Blankett for overgangsstønad", dokumenttype)
+        return ArkiverDokumentRequest(
+                fnr = personIdent,
+                forsøkFerdigstill = true,
+                hoveddokumentvarianter = listOf(dokument),
+                vedleggsdokumenter = listOf(),
+                fagsakId = fagsakId,
+                journalførendeEnhet = enhet)
+    }
+
+}
