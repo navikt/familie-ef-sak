b6d68d526873344763a628dc721aa33dd3dc3636
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-11-10 12:54:06 +0100

    Oppdaterer og rydder i EksternBehandlingServiceController

--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingController.kt
@@ -1,38 +1,29 @@
 package no.nav.familie.ef.sak.ekstern
 
-import no.nav.familie.ef.sak.behandling.BehandlingRepository
-import no.nav.familie.ef.sak.behandling.domain.BehandlingType
 import no.nav.familie.ef.sak.fagsak.domain.Stønadstype
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PdlClient
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.identer
-import no.nav.familie.ef.sak.tilkjentytelse.TilkjentYtelseService
-import no.nav.familie.ef.sak.tilkjentytelse.domain.TilkjentYtelse
 import no.nav.familie.kontrakter.felles.PersonIdent
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
-import java.util.UUID
 
 @RestController
 @RequestMapping(
-    path = ["/api/ekstern/behandling"],
-    consumes = [MediaType.APPLICATION_JSON_VALUE],
-    produces = [MediaType.APPLICATION_JSON_VALUE]
+        path = ["/api/ekstern/behandling"],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
 )
 @Validated
 class EksternBehandlingController(
-    private val pdlClient: PdlClient,
-    private val behandlingRepository: BehandlingRepository,
-    private val tilkjentYtelseService: TilkjentYtelseService
+        private val pdlClient: PdlClient,
+        private val eksternBehandlingService: EksternBehandlingService
 ) {
 
 
@@ -46,11 +37,11 @@ class EksternBehandlingController(
     @PostMapping("finnes")
     @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
     fun finnesBehandlingForPerson(
-        @RequestParam("type") stønadstype: Stønadstype?,
-        @RequestBody request: PersonIdent
+            @RequestParam("type") stønadstype: Stønadstype?,
+            @RequestBody request: PersonIdent
     ): Ressurs<Boolean> {
         val personidenter = pdlClient.hentPersonidenter(request.ident, historikk = true).identer()
-        return finnesBehandlingFor(personidenter, stønadstype)
+        return Ressurs.success(eksternBehandlingService.finnesBehandlingFor(personidenter, stønadstype))
     }
 
     /**
@@ -60,76 +51,21 @@ class EksternBehandlingController(
     @PostMapping("finnes/flere-identer")
     @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
     fun finnesBehandlingForPersonIdenter(
-        @RequestParam("type") stønadstype: Stønadstype?,
-        @RequestBody personidenter: Set<String>
-    ): Ressurs<Boolean> {
-        return finnesBehandlingFor(personidenter, stønadstype)
-    }
-
-    @GetMapping("hent/{personidenter}")
-    @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
-    fun erBehandlingerUtdaterteForPersonidenter(
-        @PathVariable personidenter: Set<String>
+            @RequestParam("type") stønadstype: Stønadstype?,
+            @RequestParam("stoenadSiste12Maaneder") stoenadSiste12Maaneder: Boolean,
+            @RequestBody personidenter: Set<String>
     ): Ressurs<Boolean> {
         if (personidenter.isEmpty()) {
-            return Ressurs.failure("Finner ikke identer til personen")
+            return Ressurs.failure("Minst en ident påkrevd for søk")
         }
         if (personidenter.any { it.length != 11 }) {
             return Ressurs.failure("Støtter kun identer av typen fnr/dnr")
         }
-
-        val behandlinger = hentAlleBehandlingIDer(personidenter)
-        if (behandlinger.isEmpty()) {
-            return Ressurs.failure("Det finnes ingen behandlinger for oppgitte personidenter")
+        val finnesBehandling = eksternBehandlingService.finnesBehandlingFor(personidenter, stønadstype)
+        if (stoenadSiste12Maaneder) {
+            return Ressurs.success(finnesBehandling && eksternBehandlingService.erBehandlingerUtdaterteFor(personidenter))
         }
-        return erBehandlingerUtdaterte(behandlinger)
+        return Ressurs.success(finnesBehandling)
     }
-
-    private fun hentAlleBehandlingIDer(personidenter: Set<String>): Set<UUID> {
-        var behandlinger = mutableSetOf<UUID>()
-        Stønadstype.values().forEach {
-            behandlingRepository.finnSisteBehandlingSomIkkeErBlankett(it, personidenter)?.let { behandlinger.add(it.id) }
-        }
-        return behandlinger
-    }
-
-    private fun erBehandlingerUtdaterte(behandlingIDer: Set<UUID>): Ressurs<Boolean> {
-        var tilkjenteYtelser = mutableSetOf<TilkjentYtelse>()
-        behandlingIDer.forEach { tilkjenteYtelser.add(tilkjentYtelseService.hentForBehandling(it)) }
-        val senesteTomDatoAvAndeler : LocalDate = tilkjenteYtelser.maxOf { it.andelerTilkjentYtelse.maxOf { it.stønadTom } }
-        return Ressurs.success(senesteTomDatoAvAndeler < LocalDate.now().minusYears(1))
-    }
-
-    private fun finnesBehandlingFor(
-        personidenter: Set<String>,
-        stønadstype: Stønadstype?
-    ): Ressurs<Boolean> {
-        if (personidenter.isEmpty()) {
-            return Ressurs.failure("Finner ikke identer til personen")
-        }
-        if (personidenter.any { it.length != 11 }) {
-            return Ressurs.failure("Støtter kun identer av typen fnr/dnr")
-        }
-
-        return if (stønadstype != null) {
-            Ressurs.success(eksistererBehandlingSomIkkeErBlankett(stønadstype, personidenter))
-        } else {
-            Ressurs.success(Stønadstype.values().any { eksistererBehandlingSomIkkeErBlankett(it, personidenter) })
-        }
-    }
-
-    /**
-     * Hvis siste behandling er teknisk opphør, skal vi returnere false,
-     * hvis ikke så skal vi returnere true hvis det finnes en behandling
-     */
-    private fun eksistererBehandlingSomIkkeErBlankett(
-        stønadstype: Stønadstype,
-        personidenter: Set<String>
-    ): Boolean {
-        return behandlingRepository.finnSisteBehandlingSomIkkeErBlankett(stønadstype, personidenter)?.let {
-            it.type != BehandlingType.TEKNISK_OPPHØR
-        } ?: false
-    }
-
 
 }
