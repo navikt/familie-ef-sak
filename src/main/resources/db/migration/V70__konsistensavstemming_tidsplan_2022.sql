12c8af03c5e34d354eff0f8f2e7862d4f505f16e
Johan Blomgren <blommish@users.noreply.github.com>
2021-11-12 08:24:29 +0100

    Lagt inn datoer for konsistensavstemming for 2022 (#960)

---
+++ src/main/resources/db/migration/V70__konsistensavstemming_tidsplan_2022.sql
@@ -0,0 +1,24 @@
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('05.01.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('28.01.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('25.02.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('25.03.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('26.04.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('27.05.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('29.06.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('29.07.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('30.08.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('29.09.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('28.10.2022', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('21.11.2022', 'DD.MM.YYYY'));
