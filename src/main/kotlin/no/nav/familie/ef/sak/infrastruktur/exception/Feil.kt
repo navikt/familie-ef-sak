248f4d5d8dd18e7102e1088e3dfafdeb88c70584
Johan Blomgren <blommish@users.noreply.github.com>
2021-11-22 14:24:51 +0100

    Tar i bruk contract fra kotlin som gjør att eks nullsjekker håndteres godt fra kallende metode

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/exception/Feil.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/exception/Feil.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.ef.sak.infrastruktur.exception
 
 import org.springframework.http.HttpStatus
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 data class ApiFeil(val feil: String, val httpStatus: HttpStatus) : RuntimeException(feil)
 
@@ -13,7 +15,11 @@ class Feil(message: String,
             this(message, null, httpStatus, throwable)
 }
 
+@OptIn(ExperimentalContracts::class)
 inline fun feilHvis(boolean: Boolean, httpStatus: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR, lazyMessage: () -> String) {
+    contract {
+        returns() implies !boolean
+    }
     if (boolean) {
         throw Feil(message = lazyMessage(), frontendFeilmelding = lazyMessage(), httpStatus)
     }
