b4bdd7130db460cd30d8060ca3677c151871aa51
Johan Blomgren <blommish@users.noreply.github.com>
2021-11-25 09:18:33 +0100

    sql feil

--- src/main/resources/db/migration/V73__rekjør_start_behandling.sql
+++ src/main/resources/db/migration/V73__rekjør_start_behandling.sql
@@ -2,4 +2,4 @@ UPDATE task t
 SET status= 'KLAR_TIL_PLUKK'
 WHERE type = 'startBehandlingTask'
   AND status = 'FERDIG'
-  AND EXISTS(SELECT * FROM behandling WHERE id = t.payload AND status = 'FERDIGSTILT');
+  AND EXISTS(SELECT * FROM behandling WHERE id = t.payload::UUID AND status = 'FERDIGSTILT');
