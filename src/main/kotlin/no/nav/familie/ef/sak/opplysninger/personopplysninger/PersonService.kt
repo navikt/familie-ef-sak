28ade05bfbf86fc5f9a20cc254e203df902efcd2
Johan Blomgren <blommish@users.noreply.github.com>
2021-11-26 13:24:53 +0100

    Legger til henting av adressebeskyttelse når vi henter personkort for å kunne bruke til tilgangskontroll

--- src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/PersonService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/PersonService.kt
@@ -1,13 +1,20 @@
 package no.nav.familie.ef.sak.opplysninger.personopplysninger
 
+import no.nav.familie.ef.sak.infrastruktur.config.getCachedOrLoad
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.domene.SøkerMedBarn
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.Familierelasjonsrolle
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.PdlPersonKort
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.PdlSøker
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Service
 
 @Service
-class PersonService(private val pdlClient: PdlClient) {
+class PersonService(
+        private val pdlClient: PdlClient,
+        @Qualifier("shortCache")
+        private val cacheManager: CacheManager
+) {
 
     fun hentSøker(ident: String): PdlSøker {
         return pdlClient.hentSøker(ident)
@@ -20,7 +27,12 @@ class PersonService(private val pdlClient: PdlClient) {
         return SøkerMedBarn(ident, søker, pdlClient.hentBarn(barnIdentifikatorer))
     }
 
+    /**
+     * PDL gjør ingen tilgangskontroll i bolkoppslag, så bruker av denne metode må ha gjort tilgangskontroll
+     */
     fun hentPdlPersonKort(identer: List<String>): Map<String, PdlPersonKort> {
-        return identer.distinct().chunked(100).map { pdlClient.hentPersonKortBolk(it) }.reduce { acc, it -> acc + it }
+        return cacheManager.getCachedOrLoad("pdl-person-kort-bulk", identer.distinct()) { identerUtenCache ->
+            identerUtenCache.chunked(100).map { pdlClient.hentPersonKortBolk(it) }.reduce { acc, it -> acc + it }
+        }
     }
 }
