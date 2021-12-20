120010537e7148150208727ccdfc90e966217361
eirarset <eirik.arseth@nav.no>
2021-12-21 00:37:28 +0100

    Legger til funksjonsbryter for lagring av brevmottaker

--- src/main/kotlin/no/nav/familie/ef/sak/brev/BrevmottakereController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/brev/BrevmottakereController.kt
@@ -1,8 +1,11 @@
 package no.nav.familie.ef.sak.brev
 
+import no.nav.familie.ef.sak.infrastruktur.exception.Feil
+import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -15,7 +18,8 @@ import java.util.UUID
 @RequestMapping(path = ["/api/brevmottakere/"])
 @ProtectedWithClaims(issuer = "azuread")
 class BrevmottakereController(private val tilgangService: TilgangService,
-                              private val brevmottakereService: BrevmottakereService) {
+                              private val brevmottakereService: BrevmottakereService,
+                              private val featureToggleService: FeatureToggleService) {
 
     @GetMapping("/{behandlingId}")
     fun hentBrevmottakere(@PathVariable behandlingId: UUID): Ressurs<BrevmottakereDto?> {
@@ -29,6 +33,10 @@ class BrevmottakereController(private val tilgangService: TilgangService,
                           @RequestBody brevmottakere: BrevmottakereDto): Ressurs<UUID> {
         tilgangService.validerTilgangTilBehandling(behandlingId)
         tilgangService.validerHarSaksbehandlerrolle()
+        if (!featureToggleService.isEnabled("familie.ef.sak.brevmottakere-verge-og-fullmakt")) {
+            throw Feil("Brevmottaker-funksjonaliteten er ikke tilgjengelig",
+                       httpStatus = HttpStatus.BAD_REQUEST)
+        }
 
         return Ressurs.Companion.success(brevmottakereService.lagreBrevmottakere(behandlingId, brevmottakere))
     }
