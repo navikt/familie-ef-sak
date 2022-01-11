280f2277163120f3f56d2bd489efdd6bca1cc7e9
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-01-11 11:09:46 +0100

    Oppgavebeskrivelser legges her for å slippe å bursdagsdatorelatert logikk i Iverksett

---
+++ src/main/kotlin/no/nav/familie/ef/sak/iverksett/oppgaveforbarn/OppgaveBeskrivelse.kt
@@ -0,0 +1,13 @@
+package no.nav.familie.ef.sak.iverksett.oppgaveforbarn
+
+object OppgaveBeskrivelse {
+
+    fun beskrivelseBarnFyllerEttÅr(): String {
+        return "Barn 1 år. Vurder aktivitetsplikten."
+    }
+
+    fun beskrivelseBarnBlirSeksMnd(): String {
+        return "Barn 1/2 år. Send varsel om aktivitetsplikt."
+    }
+
+}
