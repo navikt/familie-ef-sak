b37e999b18e45424f9189dba2787a7bbdbe54198
Johan Blomgren <blommish@users.noreply.github.com>
2022-01-14 10:57:55 +0100

    Teknisk opphør endrer på iverksett mocken, må resettes etterpå

--- src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/IverksettClientMock.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/IverksettClientMock.kt
@@ -8,6 +8,7 @@ import io.mockk.mockk
 import no.nav.familie.ef.sak.iverksett.IverksettClient
 import no.nav.familie.kontrakter.ef.iverksett.IverksettStatus
 import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.kontrakter.felles.simulering.BeriketSimuleringsresultat
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -20,20 +21,17 @@ class IverksettClientMock {
     @Bean
     @Primary
     fun iverksettClient(): IverksettClient {
-        val iverksettClient = mockk<IverksettClient>()
-
-        every { iverksettClient.simuler(any()) } returns objectMapper.readValue(readFile("simuleringsresultat_beriket.json"))
-
-        every { iverksettClient.iverksett(any(), any()) } just Runs
-        every { iverksettClient.iverksettTekniskOpphør(any()) } just Runs
-        every { iverksettClient.hentStatus(any()) } returns IverksettStatus.OK
-        every { iverksettClient.sendBehandlingsstatistikk(any()) } just Runs
-        every { iverksettClient.sendFrittståendeBrev(any()) } just Runs
-
+        clearMock()
         return iverksettClient
     }
 
-    private fun readFile(filnavn: String): String {
-        return this::class.java.getResource("/json/$filnavn").readText()
+    companion object {
+        private val iverksettClient = mockk<IverksettClient>(relaxed = true)
+        private val simuleringsresultat = objectMapper.readValue<BeriketSimuleringsresultat>(
+                this::class.java.getResource("/json/simuleringsresultat_beriket.json")!!.readText())
+        fun clearMock() {
+            every { iverksettClient.simuler(any()) } returns simuleringsresultat
+            every { iverksettClient.hentStatus(any()) } returns IverksettStatus.OK
+        }
     }
 }
