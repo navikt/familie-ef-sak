0b4548ac4b66f83d1eccdeca96ff939823bb424c
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-01-24 13:50:31 +0100

    Skal finne nye barn som har oppstått siden behandlingen når det kommer en personhendelse-nytt-barn for en bruker

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/BarnController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/BarnController.kt
@@ -1,10 +1,12 @@
 package no.nav.familie.ef.sak.behandling
 
+import no.nav.familie.kontrakter.felles.PersonIdent
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,8 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @Validated
 class BarnController(val barnService: BarnService) {
 
-    @GetMapping("{personident}")
-    fun hentFnrForRegisterbarnISisteIverksatteBehandling(@PathVariable personIdent: String): Ressurs<List<String>> {
-        return Ressurs.success(barnService.hentFnrForRegisterbarnISisteIverksatteBehandling(personIdent))
+    @PostMapping("nye-barn")
+    fun finnNyeBarnSidenGjeldendeBehandling(@RequestBody personIdent: PersonIdent): Ressurs<List<String>> {
+        return Ressurs.success(barnService.finnNyeBarnSidenGjeldendeBehandling(personIdent))
     }
 }
--- src/main/kotlin/no/nav/familie/ef/sak/behandling/BarnService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/BarnService.kt
@@ -3,39 +3,36 @@ package no.nav.familie.ef.sak.behandling
 import no.nav.familie.ef.sak.fagsak.FagsakService
 import no.nav.familie.ef.sak.fagsak.domain.Stønadstype
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.GrunnlagsdataService
-import no.nav.familie.ef.sak.opplysninger.søknad.SøknadService
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.gjeldende
+import no.nav.familie.kontrakter.felles.PersonIdent
 import org.springframework.stereotype.Service
 
 @Service
 class BarnService(private val behandlingService: BehandlingService,
                   private val fagsakService: FagsakService,
-//                  private val søknadService: SøknadService,
+                  private val personService: PersonService,
                   private val grunnlagsdataService: GrunnlagsdataService) {
 
-    fun hentFnrForRegisterbarnISisteIverksatteBehandling(personIdent: String): List<String> {
-        val fagsak = fagsakService.finnFagsak(setOf(personIdent), Stønadstype.OVERGANGSSTØNAD)
+    fun finnNyeBarnSidenGjeldendeBehandling(personIdent: PersonIdent): List<String> {
+        val fagsak = fagsakService.finnFagsak(setOf(personIdent.ident), Stønadstype.OVERGANGSSTØNAD)
                      ?: error("Kunne ikke finne fagsak for personident")
         val behandling = behandlingService.finnSisteIverksatteBehandling(fagsak.id)
                          ?: error("Kunne ikke finne behandling for fagsak")
-        val grunnlagsdataMedMetadata = grunnlagsdataService.hentGrunnlagsdata(behandling.id)
-        val allePdlBarnIBehandlingen = grunnlagsdataMedMetadata.grunnlagsdata.barn.map {
+
+        val allePdlBarnIBehandlingen = grunnlagsdataService.hentGrunnlagsdata(behandling.id).grunnlagsdata.barn.map {
             it.personIdent
+        }
+        val alleBarnFraPdl = personService.hentPersonMedBarn(personIdent.ident)
+
+        val barnFraPdlMenIkkeIBehandlingen = alleBarnFraPdl.barn.filter { barnFraPdl ->
+            barnFraPdl.value.fødsel.gjeldende().erUnder18År() && allePdlBarnIBehandlingen.none { it == barnFraPdl.key }
         }
 
         // TODO: Mirja/Hilde sjekker om vi ønsker å fiske frem potensielle terminfødte barn i tillegg,
         //  eller om det er ønskelig at det opprettes oppgave for disse
 
-        return allePdlBarnIBehandlingen
+        return barnFraPdlMenIkkeIBehandlingen.keys.toList()
     }
-
-//    val søknad = søknadService.hentOvergangsstønad(behandling.id)
-    // Søknad sine barn med fnr. De uten ligger kun i søknad
-
-    //        val ufødteBarn = søknad?.barn?.filter { it.erBarnetFødt && it.fødselTermindato != null }
-//
-//        val alleBarnIPdl = emptyList<String>()//
-//        val nyeBarnSomFinnesIBehandlingen = alleBarnIPdl.filter {
-//            it === ufødteBarn.noe()
-//        }
 
 }
