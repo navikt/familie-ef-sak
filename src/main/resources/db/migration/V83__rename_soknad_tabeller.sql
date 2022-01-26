b5482db29d890e50737973b487670d67273b241a
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-01-26 16:16:06 +0100

    Skal rename grunnlag_soknad til soknad_grunnlag

--- src/main/resources/db/migration/V83__rename_soknad_tabeller.sql
+++ src/main/resources/db/migration/V83__rename_soknad_tabeller.sql
@@ -1,3 +1,4 @@
 ALTER TABLE soker RENAME TO soknad_soker;
 ALTER TABLE utenlandsopphold RENAME TO soknad_utenlandsopphold;
 ALTER TABLE barn RENAME TO soknad_barn;
+ALTER TABLE grunnlag_soknad RENAME to soknad_grunnlag;
