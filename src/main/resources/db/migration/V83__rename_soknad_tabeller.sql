e68544c2641adc6b4ad6a333ddd4d7fd09a52926
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-01-26 16:18:20 +0100

    Skal rename tidligere_utdanning til soknad_

--- src/main/resources/db/migration/V83__rename_soknad_tabeller.sql
+++ src/main/resources/db/migration/V83__rename_soknad_tabeller.sql
@@ -2,3 +2,4 @@ ALTER TABLE soker RENAME TO soknad_soker;
 ALTER TABLE utenlandsopphold RENAME TO soknad_utenlandsopphold;
 ALTER TABLE barn RENAME TO soknad_barn;
 ALTER TABLE grunnlag_soknad RENAME to soknad_grunnlag;
+ALTER TABLE tidligere_utdanning RENAME to soknad_tidligere_utdanning;
