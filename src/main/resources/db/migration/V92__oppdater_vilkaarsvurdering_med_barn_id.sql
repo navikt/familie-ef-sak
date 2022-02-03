d749a229d8fbcfb0f746d725a34c6efb7b01527e
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-02-03 08:56:55 +0100

    Fiks bug i spørring

--- src/main/resources/db/migration/V92__oppdater_vilkaarsvurdering_med_barn_id.sql
+++ src/main/resources/db/migration/V92__oppdater_vilkaarsvurdering_med_barn_id.sql
@@ -3,4 +3,4 @@ UPDATE vilkarsvurdering
 SET barn_id = subquery.id
 FROM (SELECT id, behandling_id, soknad_barn_id::TEXT FROM behandling_barn) AS subquery
 WHERE vilkarsvurdering.behandling_id = subquery.behandling_id
-  AND vilkarsvurdering.barn_id = subquery.soknad_barn_id AND false=true;
+  AND vilkarsvurdering.barn_id = subquery.soknad_barn_id;
