1fa0444bb2ab11514ca5b9b204f4795ee565ab20
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-02-04 16:12:10 +0100

    Legg inn feature toggle for revurdering med barn

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/RevurderingsController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/RevurderingsController.kt
@@ -2,6 +2,8 @@ package no.nav.familie.ef.sak.behandling
 
 import no.nav.familie.ef.sak.AuditLoggerEvent
 import no.nav.familie.ef.sak.behandling.dto.RevurderingDto
+import no.nav.familie.ef.sak.infrastruktur.exception.feilHvis
+import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
@@ -17,12 +19,16 @@ import java.util.UUID
 class RevurderingsController(
         private val revurderingService: RevurderingService,
         private val tilgangService: TilgangService,
+        private val featureToggleService: FeatureToggleService,
 ) {
 
     @PostMapping("{fagsakId}")
     fun startRevurdering(@RequestBody revurderingInnhold: RevurderingDto): Ressurs<UUID> {
         tilgangService.validerTilgangTilFagsak(revurderingInnhold.fagsakId, AuditLoggerEvent.CREATE)
         tilgangService.validerHarSaksbehandlerrolle()
+        feilHvis(revurderingInnhold.barn.isNotEmpty() && !featureToggleService.isEnabled("familie.ef.sak.kan-legge-til-nye-barn-paa-revurdering")) {
+            "Feature toggle for revurdering med barn er disabled"
+        }
         val revurdering = revurderingService.opprettRevurderingManuelt(revurderingInnhold)
         return Ressurs.success(revurdering.id)
     }
