fa56505a1b1efa02bce152382e8a1d0bada9c069
ma10s <mattis.janitz@nav.no>
2022-02-09 09:59:16 +0100

    Mock lag tilbakekreving manuel.

--- src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/TilbakekrevingClientTestConfig.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/TilbakekrevingClientTestConfig.kt
@@ -1,12 +1,15 @@
 package no.nav.familie.ef.sak.no.nav.familie.ef.sak.infrastruktur.config
 
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
 import no.nav.familie.ef.sak.tilbakekreving.TilbakekrevingClient
 import no.nav.familie.kontrakter.felles.tilbakekreving.Behandling
 import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsstatus
 import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingstype
 import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsårsakstype
+import no.nav.familie.kontrakter.felles.tilbakekreving.KanBehandlingOpprettesManueltRespons
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
@@ -38,6 +41,10 @@ class TilbakekrevingClientTestConfig {
 
         val dummyPdf = this::class.java.classLoader.getResource("dummy/pdf_dummy.pdf")!!.readBytes()
         every { tilbakekrevingClient.hentForhåndsvisningVarselbrev(any()) } returns dummyPdf
+
+        every { tilbakekrevingClient.opprettManuelTilbakekreving(any(), any(), any()) } just runs
+        val kanBehandleRespons = KanBehandlingOpprettesManueltRespons(kanBehandlingOpprettes = true, melding = "OK")
+        every { tilbakekrevingClient.kanBehandlingOpprettesManuelt(any(), any()) } returns kanBehandleRespons
 
         return tilbakekrevingClient
     }
