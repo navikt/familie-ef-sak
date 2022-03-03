52ddc80f0b86f1c8f3cb69c047edea1b2679a96b
Johan Blomgren <blommish@users.noreply.github.com>
2022-03-03 18:10:36 +0100

    Fjerner services for å patche startdato og aktivitet

--- src/test/kotlin/no/nav/familie/ef/sak/behandling/MigreringServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/behandling/MigreringServiceTest.kt
@@ -26,7 +26,6 @@ import no.nav.familie.ef.sak.infrastruktur.config.IverksettClientMock
 import no.nav.familie.ef.sak.infrastruktur.config.RolleConfig
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.ef.sak.iverksett.IverksettClient
-import no.nav.familie.ef.sak.patch.PatchAktivitetService
 import no.nav.familie.ef.sak.simulering.SimuleringsresultatRepository
 import no.nav.familie.ef.sak.tilbakekreving.TilbakekrevingService
 import no.nav.familie.ef.sak.tilbakekreving.domain.Tilbakekrevingsvalg
@@ -88,7 +87,6 @@ internal class MigreringServiceTest : OppslagSpringRunnerTest() {
     @Autowired private lateinit var rolleConfig: RolleConfig
     @Autowired private lateinit var iverksettClient: IverksettClient
     @Autowired private lateinit var infotrygdReplikaClient: InfotrygdReplikaClient
-    @Autowired private lateinit var patchAktivitetService: PatchAktivitetService
 
     private val periodeFraMåned = YearMonth.now().minusMonths(10)
     private val opphørsmåned = YearMonth.now()
@@ -361,19 +359,6 @@ internal class MigreringServiceTest : OppslagSpringRunnerTest() {
         val migreringInfo = migreringService.hentMigreringInfo(fagsakPerson.id)
 
         assertThat(migreringInfo.kanMigreres).isTrue
-    }
-
-    @Test
-    internal fun `skal patche migrert person - oppdaterer vedtak til reell arbeidssøker`() {
-        val mockPerioder = { mockPerioder(aktivitetstype = InfotrygdAktivitetstype.TILMELDT_SOM_REELL_ARBEIDSSØKER) }
-        val migrering = opprettOgIverksettMigrering(mockPerioder = mockPerioder)
-        assertThat(vedtakService.hentVedtak(migrering.id).perioder!!.perioder.single().aktivitet)
-                .isEqualTo(AktivitetType.MIGRERING)
-
-        patchAktivitetService.patch(true)
-
-        assertThat(vedtakService.hentVedtak(migrering.id).perioder!!.perioder.single().aktivitet)
-                .isEqualTo(AktivitetType.FORSØRGER_REELL_ARBEIDSSØKER)
     }
 
     private fun verifiserVurderinger(migrering: Behandling) {
