2487c00c505c4add6a6943963db27578b3ed6dc7
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-03-07 10:50:06 +0100

    Cucumber økonomitester: Mock tilkjentytelse

--- src/test/kotlin/no/nav/familie/ef/sak/cucumber/steps/StepDefinitions.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/cucumber/steps/StepDefinitions.kt
@@ -4,6 +4,13 @@ import io.cucumber.datatable.DataTable
 import io.cucumber.java.no.Gitt
 import io.cucumber.java.no.Når
 import io.cucumber.java.no.Så
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import no.nav.familie.ef.sak.behandlingsflyt.steg.BeregnYtelseSteg
+import no.nav.familie.ef.sak.beregning.BeregningService
+import no.nav.familie.ef.sak.fagsak.FagsakService
+import no.nav.familie.ef.sak.felles.util.mockFeatureToggleService
 import no.nav.familie.ef.sak.no.nav.familie.ef.sak.cucumber.domeneparser.VedtakDomeneParser
 import no.nav.familie.ef.sak.no.nav.familie.ef.sak.cucumber.domeneparser.VedtakDomeneParser.lagDefaultTilkjentYtelseFraAndel
 import no.nav.familie.ef.sak.no.nav.familie.ef.sak.cucumber.domeneparser.VedtakDomeneParser.lagDefaultTilkjentYtelseUtenAndel
@@ -11,12 +18,18 @@ import no.nav.familie.ef.sak.no.nav.familie.ef.sak.cucumber.domeneparser.VedtakD
 import no.nav.familie.ef.sak.no.nav.familie.ef.sak.cucumber.domeneparser.parseEndringType
 import no.nav.familie.ef.sak.no.nav.familie.ef.sak.cucumber.domeneparser.parseValgfriInt
 import no.nav.familie.ef.sak.repository.behandling
+import no.nav.familie.ef.sak.simulering.SimuleringService
+import no.nav.familie.ef.sak.tilbakekreving.TilbakekrevingService
+import no.nav.familie.ef.sak.tilkjentytelse.TilkjentYtelseService
 import no.nav.familie.ef.sak.tilkjentytelse.domain.TilkjentYtelse
 import no.nav.familie.ef.sak.vedtak.AndelHistorikkBeregner
 import no.nav.familie.ef.sak.vedtak.AndelHistorikkDto
+import no.nav.familie.ef.sak.vedtak.VedtakService
 import no.nav.familie.ef.sak.vedtak.domain.Vedtak
+import no.nav.familie.ef.sak.vedtak.dto.tilVedtakDto
 import org.assertj.core.api.Assertions
 import java.time.LocalDateTime
+import java.util.UUID
 
 class StepDefinitions {
 
@@ -24,6 +37,23 @@ class StepDefinitions {
     private var tilkjentYtelse = mutableListOf<TilkjentYtelse>()
     private var beregnetAndelHistorikkList = listOf<AndelHistorikkDto>()
 
+    private val tilkjentYtelseService = mockk<TilkjentYtelseService>(relaxed = true)
+    private val beregningService = mockk<BeregningService>()
+    private val vedtakService = mockk<VedtakService>(relaxed = true)
+    private val simuleringService = mockk<SimuleringService>()
+    private val tilbakekrevingService = mockk<TilbakekrevingService>(relaxed = true)
+    private val fagsakService = mockk<FagsakService>(relaxed = true)
+    private val featureToggleService = mockFeatureToggleService()
+
+    private val beregnYtelseSteg = BeregnYtelseSteg(tilkjentYtelseService,
+                                        beregningService,
+                                        simuleringService,
+                                        vedtakService,
+                                        tilbakekrevingService,
+                                        fagsakService,
+                                        featureToggleService)
+
+    private val slot = slot<TilkjentYtelse>()
 
     @Gitt("følgende vedtak")
     fun følgende_vedtak(dataTable: DataTable) {
@@ -44,9 +74,46 @@ class StepDefinitions {
     fun `lag andelhistorikk kjøres`() {
 
         val behandlinger = vedtak.map { it.behandlingId }.distinct().mapIndexed { index, id ->
-            behandling(id = id, opprettetTid = LocalDateTime.now().plusMinutes(index.toLong()))
+            val behandling = behandling(id = id, opprettetTid = LocalDateTime.now().plusMinutes(index.toLong()))
+             behandling.id to behandling
+        }.toMap()
+
+        vedtak.forEach {
+            beregnYtelseSteg.utførSteg(behandlinger.getValue(it.behandlingId), it.tilVedtakDto())
         }
-        beregnetAndelHistorikkList = AndelHistorikkBeregner.lagHistorikk(tilkjentYtelse, vedtak, behandlinger, null)
+
+        val tilkjentYtelser = mockTilkjentYtelse()
+        val lagredeVedtak = mockLagreVedtak()
+
+        beregnetAndelHistorikkList = AndelHistorikkBeregner.lagHistorikk(tilkjentYtelser.values.toList(), lagredeVedtak, behandlinger.values.toList(), null)
+    }
+
+    private fun mockLagreVedtak(): MutableList<Vedtak> {
+        val lagredeVedtak = mutableListOf<Vedtak>()
+        every {
+            vedtakService.lagreVedtak(any(), any())
+        } answers {
+            lagredeVedtak.add(firstArg())
+            firstArg()
+        }
+        return lagredeVedtak
+    }
+
+    private fun mockTilkjentYtelse(): MutableMap<UUID, TilkjentYtelse> {
+        val tilkjentYtelser = mutableMapOf<UUID, TilkjentYtelse>()
+        every {
+            tilkjentYtelseService.opprettTilkjentYtelse(any())
+        } answers {
+            val tilkjentYtelse = firstArg<TilkjentYtelse>()
+            tilkjentYtelser.put(tilkjentYtelse.behandlingId, tilkjentYtelse)
+            tilkjentYtelse
+        }
+        every {
+            tilkjentYtelseService.hentForBehandling(any())
+        } answers {
+            tilkjentYtelser.getValue(firstArg())
+        }
+        return tilkjentYtelser
     }
 
     @Så("forvent følgende historikk")
