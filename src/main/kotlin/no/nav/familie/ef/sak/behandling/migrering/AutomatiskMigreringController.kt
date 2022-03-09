7aa50eb1d21daabc6b5be49d78c7f690ffbfc6f5
Johan Blomgren <blommish@users.noreply.github.com>
2022-03-09 10:01:27 +0100

    Path må være i @RequestMapping (#1262)

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/migrering/AutomatiskMigreringController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/migrering/AutomatiskMigreringController.kt
@@ -5,11 +5,13 @@ import no.nav.familie.ef.sak.infrastruktur.exception.brukerfeilHvisIkke
 import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @Unprotected // kommer kunne brukes uten token
-@RestController("api/automatisk-migrering")
+@RestController
+@RequestMapping("api/automatisk-migrering")
 class AutomatiskMigreringController(private val automatiskMigreringService: AutomatiskMigreringService,
                                     private val featureToggleService: FeatureToggleService) {
 
