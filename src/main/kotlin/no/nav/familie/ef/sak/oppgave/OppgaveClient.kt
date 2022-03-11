1a62c4801b09caa476140ff2ba794e5da0385938
Johan Blomgren <blommish@users.noreply.github.com>
2022-03-11 12:05:35 +0100

    Catcher ressursexception med ny versjon av felles som kaster ressursexception hvis det feiler med resurs i response

--- src/main/kotlin/no/nav/familie/ef/sak/oppgave/OppgaveClient.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/oppgave/OppgaveClient.kt
@@ -5,6 +5,7 @@ import no.nav.familie.ef.sak.infrastruktur.config.IntegrasjonerConfig
 import no.nav.familie.ef.sak.infrastruktur.exception.ApiFeil
 import no.nav.familie.ef.sak.infrastruktur.exception.IntegrasjonException
 import no.nav.familie.http.client.AbstractPingableRestClient
+import no.nav.familie.http.client.RessursException
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.oppgave.FinnMappeRequest
 import no.nav.familie.kontrakter.felles.oppgave.FinnMappeResponseDto
@@ -62,8 +63,8 @@ class OppgaveClient(@Qualifier("azure") restOperations: RestOperations,
         try {
             val respons = postForEntity<Ressurs<OppgaveResponse>>(uri, HttpHeaders().medContentTypeJsonUTF8())
             return pakkUtRespons(respons, uri, "fordelOppgave").oppgaveId
-        } catch (e: HttpClientErrorException.BadRequest) {
-            if (e.responseBodyAsString.contains("allerede er ferdigstilt")) {
+        } catch (e: RessursException) {
+            if (e.ressurs.melding.contains("allerede er ferdigstilt")) {
                 throw ApiFeil("Oppgaven med id=$oppgaveId er allerede ferdigstilt. Prøv å hente oppgaver på nytt.", HttpStatus.BAD_REQUEST)
             }
             throw e
