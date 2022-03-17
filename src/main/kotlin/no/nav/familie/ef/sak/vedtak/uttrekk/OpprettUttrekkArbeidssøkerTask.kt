dfa74c079d973e5907045c139f63bfd21e3c385f
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-03-17 18:35:37 +0100

    Legger opprettelser av uttrekk i task-nivå

--- src/main/kotlin/no/nav/familie/ef/sak/vedtak/uttrekk/OpprettUttrekkArbeidssøkerTask.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/vedtak/uttrekk/OpprettUttrekkArbeidssøkerTask.kt
@@ -1,10 +1,14 @@
 package no.nav.familie.ef.sak.vedtak.uttrekk
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.familie.ef.sak.fagsak.FagsakService
 import no.nav.familie.ef.sak.felles.util.DatoFormat.DATE_FORMAT_ISO_YEAR_MONTH
+import no.nav.familie.ef.sak.infrastruktur.config.ObjectMapperProvider.Companion.objectMapper
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.YearMonth
 
@@ -15,11 +19,36 @@ import java.time.YearMonth
 )
 class OpprettUttrekkArbeidssøkerTask(
         private val uttrekkArbeidssøkerService: UttrekkArbeidssøkerService,
+        private val fagsakService: FagsakService,
         private val taskRepository: TaskRepository
 ) : AsyncTaskStep {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun doTask(task: Task) {
-        uttrekkArbeidssøkerService.opprettUttrekkArbeidssøkere()
+        val årMåned = objectMapper.readValue<YearMonth>(task.payload)
+        val uttrekk = uttrekkArbeidssøkerService.hentArbeidssøkereForUttrekk(årMåned)
+        val aktiveIdenter = fagsakService.hentAktiveIdenter(uttrekk.map { it.fagsakId }.toSet())
+
+        var feilede = 0
+        uttrekk.forEach {
+            if (uttrekkArbeidssøkerService.uttrekkFinnes(årMåned, it.fagsakId)) {
+                return@forEach
+            }
+            try {
+                uttrekkArbeidssøkerService.opprettUttrekkArbeidssøkere(årMåned = årMåned,
+                                                                       fagsakId = it.fagsakId,
+                                                                       behandlingIdForVedtak = it.behandlingIdForVedtak,
+                                                                       personIdent = aktiveIdenter[it.fagsakId]
+                                                                                     ?: error("Kunne ikke finne fagsakID"))
+            } catch (ex: Exception) {
+                logger.error(ex.message, ex)
+                ++feilede
+            }
+        }
+        if (feilede > 0) {
+            error("Kunne ikke opprette ${feilede} av ${uttrekk.size} uttrekk")
+        }
     }
 
     override fun onCompletion(task: Task) {
