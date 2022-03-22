5d9ee67a795ec6ab2a23a488044ed1480731719c
eirarset <eirik.arseth@nav.no>
2022-03-22 10:09:50 +0100

    Fritekstbrev: Vil hente html fra familie brev, ved generinring av saksbehandlerbrev og lage pdf mot familie-dokument
    Co-authored-by: Mattis Janitz <mattis.janitz@nav.no>

--- src/main/kotlin/no/nav/familie/ef/sak/brev/VedtaksbrevService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/brev/VedtaksbrevService.kt
@@ -20,7 +20,6 @@ import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonopplysningerService
 import no.nav.familie.ef.sak.repository.findByIdOrThrow
-import no.nav.familie.kontrakter.felles.objectMapper
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import java.util.UUID
@@ -137,32 +136,38 @@ class VedtaksbrevService(private val brevClient: BrevClient,
             "Brev-HTML mangler placeholder for besluttersignatur"
         }
 
-        val beslutterSignatur =  if (signaturMedEnhet.skjulBeslutter) "" else signaturMedEnhet.navn
+        val beslutterSignatur = if (signaturMedEnhet.skjulBeslutter) "" else signaturMedEnhet.navn
         return html.replace(BESLUTTER_SIGNATUR_PLACEHOLDER, beslutterSignatur)
     }
 
-    fun lagSaksbehandlerFritekstbrev(frittståendeBrevDto: VedtaksbrevFritekstDto): ByteArray {
-        val behandling = behandlingService.hentBehandling(frittståendeBrevDto.behandlingId)
+    fun lagSaksbehandlerFritekstbrev(fritekstbrevDto: VedtaksbrevFritekstDto): ByteArray {
+        val behandling = behandlingService.hentBehandling(fritekstbrevDto.behandlingId)
         validerRedigerbarBehandling(behandling)
-        val ident = behandlingService.hentAktivIdent(frittståendeBrevDto.behandlingId)
+        val ident = behandlingService.hentAktivIdent(fritekstbrevDto.behandlingId)
         val navn = personopplysningerService.hentGjeldeneNavn(listOf(ident))
-        val request = FrittståendeBrevRequestDto(overskrift = frittståendeBrevDto.overskrift,
-                                                 avsnitt = frittståendeBrevDto.avsnitt,
+        val request = FrittståendeBrevRequestDto(overskrift = fritekstbrevDto.overskrift,
+                                                 avsnitt = fritekstbrevDto.avsnitt,
                                                  personIdent = ident,
                                                  navn = navn[ident]!!)
 
         val signaturMedEnhet = brevsignaturService.lagSignaturMedEnhet(behandling.fagsakId)
 
 
+        val html = brevClient.genererHtmlFritekstbrev(fritekstBrev = request,
+                                                      saksbehandlerNavn = signaturMedEnhet.navn,
+                                                      enhet = signaturMedEnhet.enhet)
+
+        println("html: $html")
 
 
-        val vedtaksbrev = lagreEllerOppdaterSaksbehandlerVedtaksbrev(behandlingId = frittståendeBrevDto.behandlingId,
-                                                                     brevrequest = objectMapper.writeValueAsString(request),
-                                                                     brevmal = FRITEKST,
-                                                                     saksbehandlersignatur = signaturMedEnhet.navn,
-                                                                     enhet = signaturMedEnhet.enhet)
+        lagreEllerOppdaterSaksbehandlerVedtaksbrev(behandlingId = fritekstbrevDto.behandlingId,
+                                                   brevrequest = "", // TODO: Dette feltet skal fjernes senere
+                                                   brevmal = FRITEKST,
+                                                   saksbehandlersignatur = signaturMedEnhet.navn,
+                                                   enhet = signaturMedEnhet.enhet,
+                                                   saksbehandlerHtml = html)
 
-        return brevClient.genererBrev(vedtaksbrev = vedtaksbrev.tilDto(signaturMedEnhet.skjulBeslutter))
+        return familieDokumentClient.genererPdfFraHtml(html)
     }
 
     private fun validerRedigerbarBehandling(behandling: Behandling) {
