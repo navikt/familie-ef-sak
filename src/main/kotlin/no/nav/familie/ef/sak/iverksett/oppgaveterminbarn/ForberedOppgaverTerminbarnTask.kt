9192bb68c91d0e460a1a0289e8aadfac445200e8
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-03-30 09:22:44 +0200

    Task for forbereding av oppgaver

---
+++ src/main/kotlin/no/nav/familie/ef/sak/iverksett/oppgaveterminbarn/ForberedOppgaverTerminbarnTask.kt
@@ -0,0 +1,50 @@
+package no.nav.familie.ef.sak.iverksett.oppgaveterminbarn
+
+import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
+import no.nav.familie.prosessering.AsyncTaskStep
+import no.nav.familie.prosessering.TaskStepBeskrivelse
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Service
+@TaskStepBeskrivelse(
+        taskStepType = ForberedOppgaverTerminbarnTask.TYPE,
+        beskrivelse = "Oppretter oppgave for barn som fyller 1/2 eller 1 år",
+
+        )
+class ForberedOppgaverTerminbarnTask(val taskRepository: TaskRepository,
+                                     val forberedOppgaverTerminbarnService: ForberedOppgaverTerminbarnService,
+                                     val featureToggleService: FeatureToggleService) : AsyncTaskStep {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    val DATE_FORMAT_ISO_YEAR_MONTH_DAY = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    override fun doTask(task: Task) {
+        if (featureToggleService.isEnabled("familie.ef.iverksett.opprett-oppgaver-terminbarn")) {
+            val sisteKjøring = LocalDate.parse(task.payload)
+            forberedOppgaverTerminbarnService.forberedOppgaverForUfødteTerminbarn(sisteKjøring)
+        } else {
+            logger.warn("Feature toggle opprett-oppgaver-barnsomfylleraar er ikke enablet")
+        }
+    }
+
+    override fun onCompletion(task: Task) {
+        opprettTaskForNesteUke()
+    }
+
+    fun opprettTaskForNesteUke() {
+        val nesteUke = LocalDate.now().plusWeeks(1)
+        val triggerTid = nesteUke.atTime(5, 0)
+        taskRepository.save(Task(TYPE, LocalDate.now().format(DATE_FORMAT_ISO_YEAR_MONTH_DAY)).medTriggerTid(triggerTid))
+    }
+
+    companion object {
+
+        const val TYPE = "forberedOppgaverForBarnTask"
+    }
+}
