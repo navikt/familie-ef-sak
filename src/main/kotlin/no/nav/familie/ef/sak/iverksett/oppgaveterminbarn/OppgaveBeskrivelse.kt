483db750d93eabb719373795683f44f94c4d1a95
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-03-30 09:23:33 +0200

    Oppgavebeskrivelser for terminbarn

---
+++ src/main/kotlin/no/nav/familie/ef/sak/iverksett/oppgaveterminbarn/OppgaveBeskrivelse.kt
@@ -0,0 +1,9 @@
+package no.nav.familie.ef.sak.iverksett.oppgaveterminbarn
+
+object OppgaveBeskrivelse {
+
+    fun beskrivelseUfødtTerminbarn(): String {
+        return "Dagens dato er mere enn fire uker etter termindato for ufødt barn"
+    }
+
+}
