f7749ae905b9f593b7a5291ae50a0aded9a863b0
Trondl <56258085+throndi@users.noreply.github.com>
2022-03-31 13:31:28 +0200

    Update src/main/resources/db/migration/V106_terminbarnoppgave.sql
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>

--- src/main/resources/db/migration/V106_terminbarnoppgave.sql
+++ src/main/resources/db/migration/V106_terminbarnoppgave.sql
@@ -1,5 +1,5 @@
 CREATE TABLE terminbarnoppgave (
-    fagsak_id     UUID,
+    fagsak_id     UUID REFERENCES fagsak (id),
     termindato    TIMESTAMP(3) NOT NULL,
     opprettet_tid TIMESTAMP(3) NOT NULL DEFAULT LOCALTIMESTAMP
 );
