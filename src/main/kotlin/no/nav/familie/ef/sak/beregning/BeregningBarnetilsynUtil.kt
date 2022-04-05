53d9d14a780bd0b2c258f91f77af1d86fc642dc8
ma10s <mattis.janitz@nav.no>
2022-04-05 12:20:33 +0200

    Legger til Maxbeløp for barnetilsyn for 2021 og 2020

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/BeregningBarnetilsynUtil.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/BeregningBarnetilsynUtil.kt
@@ -17,7 +17,13 @@ object BeregningBarnetilsynUtil {
     val satserForBarnetilsyn: List<MaxbeløpBarnetilsynSats> =
             listOf(MaxbeløpBarnetilsynSats(fraOgMedDato = LocalDate.parse("2022-01-01"),
                                            tilOgMedDato = LocalDate.MAX,
-                                           maxbeløp = mapOf(1 to 4250, 2 to 5545, 3 to 6284))
+                                           maxbeløp = mapOf(1 to 4250, 2 to 5545, 3 to 6284)),
+                   MaxbeløpBarnetilsynSats(fraOgMedDato = LocalDate.parse("2021-01-01"),
+                                           tilOgMedDato = YearMonth.of(2021, 12).atEndOfMonth(),
+                                           maxbeløp = mapOf(1 to 4195, 2 to 5474, 3 to 6203)),
+                   MaxbeløpBarnetilsynSats(fraOgMedDato = LocalDate.parse("2020-01-01"),
+                                           tilOgMedDato = YearMonth.of(2020, 12).atEndOfMonth(),
+                                           maxbeløp = mapOf(1 to 4053, 2 to 5289, 3 to 5993))
             )
 
     fun lagBeløpsPeriodeBarnetilsyn(utgiftsperiode: UtgiftsMåned,
