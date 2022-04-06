b42333deb316e68df506da61ecae36d0bc602b7b
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-04-06 23:43:19 +0200

    Sletter erstattet sql

--- src/main/resources/db/migration/V106_terminbarnoppgave.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE terminbarnoppgave (
-    fagsak_id     UUID REFERENCES fagsak (id),
-    termindato    TIMESTAMP(3) NOT NULL,
-    opprettet_tid TIMESTAMP(3) NOT NULL DEFAULT LOCALTIMESTAMP
-);
-
-CREATE INDEX ON terminbarnoppgave (fagsak_id);
