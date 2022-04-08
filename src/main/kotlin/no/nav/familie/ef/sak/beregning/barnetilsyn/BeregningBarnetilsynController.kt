253ac296416f371e6febf59272ea3e7900ea5e11
eirarset <eirik.arseth@nav.no>
2022-04-08 09:45:35 +0200

    Skal hente beregning for barnetilsyn

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/BeregningBarnetilsynController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/BeregningBarnetilsynController.kt
@@ -1,17 +1,27 @@
 package no.nav.familie.ef.sak.beregning.barnetilsyn
 
+import no.nav.familie.ef.sak.AuditLoggerEvent
+import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
+import no.nav.familie.ef.sak.vedtak.VedtakService
+import no.nav.familie.ef.sak.vedtak.dto.InnvilgelseBarnetilsyn
+import no.nav.familie.ef.sak.vedtak.dto.tilVedtakDto
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 
 @RestController
 @RequestMapping(path = ["/api/beregning/barnetilsyn"])
 @ProtectedWithClaims(issuer = "azuread")
-class BeregningBarnetilsynController(private val beregningBarnetilsynService: BeregningBarnetilsynService) {
+class BeregningBarnetilsynController(private val beregningBarnetilsynService: BeregningBarnetilsynService,
+                                     private val tilgangService: TilgangService,
+                                     private val vedtakService: VedtakService) {
 
 
     @PostMapping
@@ -22,6 +32,21 @@ class BeregningBarnetilsynController(private val beregningBarnetilsynService: Be
         return Ressurs.success(beregningBarnetilsynService.beregnYtelseBarnetilsyn(barnetilsynBeregningRequest.utgiftsperioder,
                                                                                    barnetilsynBeregningRequest.kontantstøtteperioder,
                                                                                    barnetilsynBeregningRequest.tilleggsstønadsperioder))
+    }
+
+    @GetMapping("/{behandlingId}")
+    fun hentBeregning(@PathVariable behandlingId: UUID): Ressurs<List<BeløpsperiodeBarnetilsynDto>> {
+        tilgangService.validerTilgangTilBehandling(behandlingId, AuditLoggerEvent.UPDATE)
+
+
+        val vedtak = vedtakService.hentVedtak(behandlingId).tilVedtakDto()
+
+        if (vedtak is InnvilgelseBarnetilsyn) {
+            return Ressurs.success(beregningBarnetilsynService.beregnYtelseBarnetilsyn(vedtak.perioder,
+                                                                                       vedtak.perioderKontantstøtte,
+                                                                                       vedtak.tilleggsstønad.perioder))
+        }
+        error("Kan ikke hente beregning for vedtakstype ${vedtak._type}")
     }
 
 
