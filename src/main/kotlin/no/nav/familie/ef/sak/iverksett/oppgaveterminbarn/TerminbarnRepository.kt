c415c988722c82d0fc25eb8f1a1d41b7c8aa75c2
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-04-25 15:43:12 +0200

    Henter ut kun barn i sql spørringen som ikke finnes i terminbarntabellen allerede

--- src/main/kotlin/no/nav/familie/ef/sak/iverksett/oppgaveterminbarn/TerminbarnRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/iverksett/oppgaveterminbarn/TerminbarnRepository.kt
@@ -5,15 +5,11 @@ import no.nav.familie.ef.sak.repository.RepositoryInterface
 import no.nav.familie.kontrakter.felles.ef.StønadType
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.stereotype.Repository
-import java.time.LocalDate
 import java.util.UUID
 
 @Repository
 interface TerminbarnRepository : RepositoryInterface<TerminbarnOppgave, UUID>,
                                  InsertUpdateRepository<TerminbarnOppgave> {
-
-    @Query
-    fun existsByFagsakIdAndTermindato(fagsakId: UUID, termindato: LocalDate): Boolean
 
     // language=PostgreSQL
     @Query("""
@@ -21,7 +17,8 @@ interface TerminbarnRepository : RepositoryInterface<TerminbarnOppgave, UUID>,
         FROM gjeldende_iverksatte_behandlinger b
          JOIN fagsak_ekstern fe ON fe.fagsak_id = b.fagsak_id
          JOIN behandling_barn bb ON bb.behandling_id = b.id
-        WHERE b.stonadstype=:stønadstype AND bb.person_ident IS NULL AND bb.fodsel_termindato < date(NOW() - INTERVAL '4 week') 
+        WHERE b.stonadstype=:stønadType AND bb.person_ident IS NULL AND bb.fodsel_termindato < date(NOW() - INTERVAL '4 week') 
+        AND NOT EXISTS(SELECT 1 FROM terminbarn_oppgave WHERE fagsak_id = b.fagsak_id AND termindato = bb.fodsel_termindato)
         """)
     fun finnBarnAvGjeldendeIverksatteBehandlingerUtgåtteTerminbarn(stønadType: StønadType): List<TerminbarnTilUtplukkForOppgave>
 }
