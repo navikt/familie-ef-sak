98ba9b00585a28db3b07ed671219c6db8aa2acdd
Johan Blomgren <blommish@users.noreply.github.com>
2022-04-28 11:56:51 +0200

    Splitter konsistensavstemmingen i chunks for å ikke få timeout når vi sender veldig mange avstemminger i en request

--- src/main/kotlin/no/nav/familie/ef/sak/behandlingsflyt/task/KonsistensavstemmingTask.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandlingsflyt/task/KonsistensavstemmingTask.kt
@@ -7,6 +7,7 @@ import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.domene.TaskRepository
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.util.Properties
@@ -17,7 +18,7 @@ data class KonsistensavstemmingPayload(val stønadstype: StønadType,
 @Service
 @TaskStepBeskrivelse(taskStepType = KonsistensavstemmingTask.TYPE, beskrivelse = "Utfører konsistensavstemming mot økonomi.")
 class KonsistensavstemmingTask(
-        private val avstemmingService: AvstemmingService,
+        private val avstemmingService: AvstemmingService
 ) : AsyncTaskStep {
 
     override fun doTask(task: Task) {
