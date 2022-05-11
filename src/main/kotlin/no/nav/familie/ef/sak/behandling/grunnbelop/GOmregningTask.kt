b2e7668624856823c915d60dc33377e7175174da
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2022-05-11 16:57:09 +0200

    Lagt til dry run mode. (#1476)

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/GOmregningTask.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/GOmregningTask.kt
@@ -1,11 +1,15 @@
 package no.nav.familie.ef.sak.behandling.grunnbelop
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.familie.ef.sak.beregning.DryRunException
 import no.nav.familie.ef.sak.beregning.OmregningService
+import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.Properties
 import java.util.UUID
@@ -16,11 +20,20 @@ import java.util.UUID
                      settTilManuellOppfølgning = true,
                      triggerTidVedFeilISekunder = 15 * 60L,
                      beskrivelse = "G-omregning")
-class GOmregningTask(private val omregningService: OmregningService) : AsyncTaskStep {
+class GOmregningTask(private val omregningService: OmregningService,
+                     private val featureToggleService: FeatureToggleService) : AsyncTaskStep {
+
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
 
     override fun doTask(task: Task) {
         val behandlingId = objectMapper.readValue<GOmregningTaskPayload>(task.payload).behandlingId
-        omregningService.utførGOmregning(behandlingId)
+        try {
+            omregningService.utførGOmregning(behandlingId,
+                                             featureToggleService.isEnabled("familie.ef.sak.omberegning.live.run"))
+        } catch (e: DryRunException) {
+            logger.info("G-OmberegningTask for behandlingId $behandlingId ruller tilbake fordi den er kjørt i dry run-modus.")
+        }
     }
 
     companion object {
@@ -39,6 +52,4 @@ class GOmregningTask(private val omregningService: OmregningService) : AsyncTask
     }
 }
 
-data class GOmregningTaskPayload(
-        val behandlingId: UUID
-)
+data class GOmregningTaskPayload(val behandlingId: UUID)
