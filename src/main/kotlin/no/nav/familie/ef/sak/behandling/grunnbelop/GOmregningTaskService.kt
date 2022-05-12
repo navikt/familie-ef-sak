deb9fb4af4bd5ed18dedf1fe93f82e70bbee11ff
Bent J. Lorentzen <bent.lorentzen@nav.no>
2022-05-12 12:33:55 +0200

    Ekstra toggle-punkt for å hindre masse feilede tasks.

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/GOmregningTaskService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/GOmregningTaskService.kt
@@ -2,15 +2,21 @@ package no.nav.familie.ef.sak.behandling.grunnbelop
 
 import no.nav.familie.ef.sak.beregning.nyesteGrunnbeløpGyldigFraOgMed
 import no.nav.familie.ef.sak.fagsak.FagsakRepository
+import no.nav.familie.ef.sak.infrastruktur.exception.feilHvisIkke
+import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.prosessering.domene.TaskRepository
 import org.springframework.stereotype.Service
 
 @Service
 class GOmregningTaskService(val fagsakRepository: FagsakRepository,
+                            val featureToggleService: FeatureToggleService,
                             val taskRepository: TaskRepository) {
 
     fun opprettGOmregningTaskForBehandlingerMedUtdatertG(): Int {
 
+        feilHvisIkke(featureToggleService.isEnabled("familie.ef.sak.omberegning")) {
+            "Feature toggle for omberegning er disabled"
+        }
         val fagsakIder = fagsakRepository.finnFagsakerMedUtdatertGBelop(nyesteGrunnbeløpGyldigFraOgMed)
         val gOmregningTasks = GOmregningTask.opprettTasks(fagsakIder)
         taskRepository.saveAll(gOmregningTasks)
