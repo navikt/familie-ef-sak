2604a748a03b9a70895e34bde75cb3b10d883f8b
Bent J. Lorentzen <bent.lorentzen@nav.no>
2022-05-23 12:30:04 +0200

    Triggertid for g-omberegnig

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/GOmregningTaskService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/GOmregningTaskService.kt
@@ -4,6 +4,8 @@ import no.nav.familie.ef.sak.beregning.nyesteGrunnbeløpGyldigFraOgMed
 import no.nav.familie.ef.sak.fagsak.FagsakRepository
 import no.nav.familie.ef.sak.infrastruktur.exception.feilHvisIkke
 import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
@@ -12,17 +14,20 @@ class GOmregningTaskService(val fagsakRepository: FagsakRepository,
                             val featureToggleService: FeatureToggleService,
                             val gOmregningTask: GOmregningTask) {
 
-    @Scheduled(cron = "0 15 12 23 5 ?")
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @Scheduled(cron = "0 45 12 23 5 ?")
     fun opprettGOmregningTaskForBehandlingerMedUtdatertG(): Int {
 
         feilHvisIkke(featureToggleService.isEnabled("familie.ef.sak.omberegning")) {
             "Feature toggle for omberegning er disabled"
         }
+        logger.info("Starter opprettelse av tasker for G-omregning.")
         val fagsakIder = fagsakRepository.finnFerdigstilteFagsakerMedUtdatertGBelop(nyesteGrunnbeløpGyldigFraOgMed)
         fagsakIder.forEach {
             gOmregningTask.opprettTask(it)
         }
-
+        logger.info("Opprettet ${fagsakIder.size} tasker for G-omregning.")
         return fagsakIder.size
     }
 
