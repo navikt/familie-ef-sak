54c11039eb6503e04c670b6087122204b7e5fbdc
Johan Blomgren <blommish@users.noreply.github.com>
2022-05-25 12:50:35 +0200

    Vid manuell revurdering med årsak g-omregning så skal vi validere at … (#1534)
    * Vid manuell revurdering med årsak g-omregning så skal vi validere at perioder er likt fra før

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/OmregningService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/OmregningService.kt
@@ -203,7 +203,8 @@ class DryRunBeregnYtelseSteg(tilkjentYtelseService: TilkjentYtelseService,
                              vedtakService: VedtakService,
                              tilbakekrevingService: TilbakekrevingService,
                              barnService: BarnService,
-                             fagsakService: FagsakService) {
+                             fagsakService: FagsakService,
+                             validerOmregningService: ValiderOmregningService) {
 
     private val beregnYtelseSteg = BeregnYtelseSteg(tilkjentYtelseService,
                                                     beregningService,
@@ -213,7 +214,8 @@ class DryRunBeregnYtelseSteg(tilkjentYtelseService: TilkjentYtelseService,
                                                     vedtakService,
                                                     tilbakekrevingService,
                                                     barnService,
-                                                    fagsakService)
+                                                    fagsakService,
+                                                    validerOmregningService)
 
     fun utførSteg(saksbehandling: Saksbehandling, data: VedtakDto) {
         beregnYtelseSteg.utførSteg(saksbehandling, data)
