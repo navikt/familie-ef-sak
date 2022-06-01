44f9577bad17f549cb0e498a7c4cc23cff06a08a
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-06-01 13:27:09 +0200

    Merge

--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/bisys/BisysBarnetilsynService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/bisys/BisysBarnetilsynService.kt
@@ -18,59 +18,71 @@ import java.time.LocalDate
 
 @Service
 class BisysBarnetilsynService(
-        private val personService: PersonService,
-        private val fagsakService: FagsakService,
-        private val barnService: BarnService,
-        private val tilkjentYtelseService: TilkjentYtelseService,
-        private val infotrygdService: InfotrygdService) {
+    private val personService: PersonService,
+    private val fagsakService: FagsakService,
+    private val barnService: BarnService,
+    private val tilkjentYtelseService: TilkjentYtelseService,
+    private val infotrygdService: InfotrygdService
+) {
 
     fun hentBarnetilsynperioderFraEfOgInfotrygd(personIdent: String, fomDato: LocalDate): BarnetilsynBisysResponse {
         return BarnetilsynBisysResponse(kombinerBarnetilsynperioderFraEfOgInfotrygd(personIdent, fomDato))
     }
 
-    private fun kombinerBarnetilsynperioderFraEfOgInfotrygd(personIdent: String,
-                                                            fomDato: LocalDate): List<BarnetilsynBisysPeriode> {
+    private fun kombinerBarnetilsynperioderFraEfOgInfotrygd(
+        personIdent: String,
+        fomDato: LocalDate
+    ): List<BarnetilsynBisysPeriode> {
         return (hentInfotrygdPerioderBarnetilsyn(personIdent, fomDato) + hentPerioderBarnetilsyn(personIdent, fomDato))
-                .sortedBy { it.periode.fom }
+            .sortedBy { it.periode.fom }
     }
 
     private fun hentPerioderBarnetilsyn(personIdent: String, fomDato: LocalDate): List<BarnetilsynBisysPeriode> {
 
         val personIdenter = personService.hentPersonIdenter(personIdent).identer()
         val fagsak: Fagsak = fagsakService.finnFagsak(personIdenter, StønadType.BARNETILSYN)
-                             ?: error("Kunne ikke finne fagsak for personident")
+            ?: error("Kunne ikke finne fagsak for personident")
 
         val historikk = tilkjentYtelseService.hentHistorikk(fagsak.id, null)
-                .filter { it.erIkkeFjernet() }
-                .filter { it.andel.beløp > 0 && it.andel.stønadTil >= fomDato }
+            .filter { it.erIkkeFjernet() }
+            .filter { it.andel.beløp > 0 && it.andel.stønadTil >= fomDato }
 
         val barnIdenter = historikk.flatMap { it.andel.barn }
-                .distinct()
-                .let { barnService.hentBehandlingBarnForBarnIder(it) }
-                .associate { it.id to it.personIdent }
+            .distinct()
+            .let { barnService.hentBehandlingBarnForBarnIder(it) }
+            .associate { it.id to it.personIdent }
 
         val barnetilsynBisysPerioder = historikk.map { andel ->
-            BarnetilsynBisysPeriode(Periode(andel.andel.stønadFra, andel.andel.stønadTil),
-                                    andel.andel.barn.map {
-                                        barnIdenter[it]
-                                        ?: error("Fant ingen personident for barn=$it")
-                                    },
-                                    andel.andel.beløp,
-                                    Datakilde.EF)
+            BarnetilsynBisysPeriode(
+                Periode(andel.andel.stønadFra, andel.andel.stønadTil),
+                andel.andel.barn.map {
+                    barnIdenter[it]
+                        ?: error("Fant ingen personident for barn=$it")
+                },
+                andel.andel.beløp,
+                Datakilde.EF
+            )
         }
         return barnetilsynBisysPerioder
     }
 
-    private fun hentInfotrygdPerioderBarnetilsyn(personIdent: String, fomDato: LocalDate): List<BarnetilsynBisysPeriode> {
+    private fun hentInfotrygdPerioderBarnetilsyn(
+        personIdent: String,
+        fomDato: LocalDate
+    ): List<BarnetilsynBisysPeriode> {
         val barnetilsynBisysPerioder =
-                infotrygdService.hentPerioderFraReplika(personIdent,
-                                                        setOf(StønadType.BARNETILSYN)).barnetilsyn.filter { it.stønadTom >= fomDato }
-                        .map { periode ->
-                            BarnetilsynBisysPeriode(Periode(periode.stønadFom, periode.stønadTom),
-                                                    periode.barnIdenter,
-                                                    periode.månedsbeløp,
-                                                    Datakilde.INFOTRYGD)
-                        }
+            infotrygdService.hentPerioderFraReplika(
+                personIdent,
+                setOf(StønadType.BARNETILSYN)
+            ).barnetilsyn.filter { it.stønadTom >= fomDato }
+                .map { periode ->
+                    BarnetilsynBisysPeriode(
+                        Periode(periode.stønadFom, periode.stønadTom),
+                        periode.barnIdenter,
+                        periode.månedsbeløp,
+                        Datakilde.INFOTRYGD
+                    )
+                }
         return barnetilsynBisysPerioder
     }
 }
