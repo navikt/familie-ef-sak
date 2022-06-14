fe50ce3534d03dbd4d13390bd28ff97e87ce42c6
Johan Blomgren <blommish@users.noreply.github.com>
2022-06-14 20:19:10 +0200

    Skal bruke maksbeløpet for inneværende år hvis det ikke er lagt inn for neste år ennå (#1595)

--- src/test/kotlin/no/nav/familie/ef/sak/beregning/skolepenger/SkolepengerMaksbeløpTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/beregning/skolepenger/SkolepengerMaksbeløpTest.kt
@@ -8,6 +8,7 @@ import no.nav.familie.ef.sak.vedtak.domain.SkolepengerStudietype.HØGSKOLE_UNIVE
 import no.nav.familie.ef.sak.vedtak.domain.SkolepengerStudietype.VIDEREGÅENDE
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.Year
 
@@ -39,6 +40,37 @@ internal class SkolepengerMaksbeløpTest {
         assertThatThrownBy { assertThat(maksbeløpForÅr(VIDEREGÅENDE, Year.of(2019))) }
             .isInstanceOf(ApiFeil::class.java)
             .withFailMessage("Finner ikke maksbeløp for studietype=VIDEREGÅENDE skoleår=19/20")
+    }
+
+    /**
+     * Disse testene må oppdateres med år når man legger inn nytt maksbeløp for neste år
+     * * [ÅR_OM_2_ÅR] må oppdateres
+     * * Beløp må oppdateres
+     */
+    @Nested
+    inner class MaksBeløpEtÅrFremITiden {
+
+        private val ÅR_OM_2_ÅR = Year.of(2024)
+
+        @Test
+        internal fun `maksbeløp for universitet et år frem i tiden skal returnere forrige år sitt verdi`() {
+            assertThat(maksbeløpForÅr(HØGSKOLE_UNIVERSITET, Year.of(2023))).isEqualTo(69_500)
+        }
+
+        @Test
+        internal fun `maksbeløp for videregående et år frem i tiden skal returnere forrige år sitt verdi`() {
+            assertThat(maksbeløpForÅr(VIDEREGÅENDE, Year.of(2023))).isEqualTo(29_002)
+        }
+
+        @Test
+        internal fun `maksbeløp for skoleår 2 år frem i tiden kaster exception`() {
+            assertThatThrownBy { assertThat(maksbeløpForÅr(HØGSKOLE_UNIVERSITET, ÅR_OM_2_ÅR)) }
+                .isInstanceOf(ApiFeil::class.java)
+                .hasMessageContaining("Finner ikke maksbeløp for studietype=HØGSKOLE_UNIVERSITET")
+            assertThatThrownBy { assertThat(maksbeløpForÅr(VIDEREGÅENDE, ÅR_OM_2_ÅR)) }
+                .isInstanceOf(ApiFeil::class.java)
+                .hasMessageContaining("Finner ikke maksbeløp for studietype=VIDEREGÅENDE")
+        }
     }
 
     private fun maksbeløpForÅr(studietype: SkolepengerStudietype, skoleår: Year): Int {
