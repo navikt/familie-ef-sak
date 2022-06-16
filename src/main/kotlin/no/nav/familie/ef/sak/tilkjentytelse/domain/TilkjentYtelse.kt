c24e8be321ddaf05455c255a72e56608e977cdc2
Johan Blomgren <blommish@users.noreply.github.com>
2022-06-16 11:30:31 +0200

    Dokumentasjon av startdato i tilkjent ytelse

--- src/main/kotlin/no/nav/familie/ef/sak/tilkjentytelse/domain/TilkjentYtelse.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/tilkjentytelse/domain/TilkjentYtelse.kt
@@ -12,6 +12,21 @@ import java.time.LocalDateTime
 import java.time.temporal.TemporalAdjusters
 import java.util.UUID
 
+/**
+ * @param startdato dato for når vi tidligst er ansvarlige for nye perioder.
+ * Når denne er satt kan den aldri flyttes fremover i tid, ettersom vi må ta ansvar for det tidspunktet vi behandlet saken fra.
+ *  Den kan endres tilbake i tid hvis vi gjør vedtak med en dato før forrige startdato.
+ * I en førstegangsbehandling så settes startdato til den første andelens startdato
+ * Når man revurderer så settes startdato i den nye til det tidligste av:
+ *   forrige startdato, tidligste startdato på nye andeler, eller opphørsdato dersom denne er før forrige startdato
+ *
+ * Startdato brukes for å sende over korrekte utbetalingsoppdrag til økonomi.
+ * For migrerte saker er startdato en viktig indikator på når dette systemet har overtatt utbetalingsansvaret.
+ * All utbetaling før startdato vil i så fall finnes i andre systemer.
+ *
+ * Startdato brukes også når vi skal slå sammen perioder tvers Infotrygd og ef-sak,
+ * der vi bruker startdato for å avkorte perioder fra infotrygd, spesielt i de tilfelle der startdato er før laveste fom-dato for en andel
+ */
 data class TilkjentYtelse(
     @Id
     val id: UUID = UUID.randomUUID(),
