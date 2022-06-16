d9fe48d6eeac8aeef8263560bacaa3b69bc0d2b8
Viktor <viktor.grondalen.solberg@nav.no>
2022-06-16 16:12:38 +0200

    skal sende med utgifter fra Ãsøknaden i underUtdanning istedet for  via et eget kall i søknadsController

--- src/main/kotlin/no/nav/familie/ef/sak/opplysninger/søknad/SkolepengerSøknadsutgifterDto.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.familie.ef.sak.opplysninger.søknad
-
-data class SkolepengerSøknadsutgifterDto(
-    val semesteravgift: Int? = null,
-    val studieavgift: Int? = null,
-    val eksamensgebyr: Int? = null
-)
