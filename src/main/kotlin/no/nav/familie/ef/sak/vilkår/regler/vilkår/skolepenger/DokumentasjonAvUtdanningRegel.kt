dd98f7cbd6a7d2356d5452aefa73a39a89f64811
Viktor <viktor.grondalen.solberg@nav.no>
2022-06-20 14:08:31 +0200

    oppdatert kontrakter, endret navn på nytt spørsmål til noe mer beskrivende

--- src/main/kotlin/no/nav/familie/ef/sak/vilkår/regler/vilkår/skolepenger/DokumentasjonAvUtdanningRegel.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/vilkår/regler/vilkår/skolepenger/DokumentasjonAvUtdanningRegel.kt
@@ -10,8 +10,8 @@ import no.nav.familie.ef.sak.vilkår.regler.regelIder
 
 class DokumentasjonAvUtdanningRegel : Vilkårsregel(
     vilkårType = VilkårType.DOKUMENTASJON_AV_UTDANNING,
-    regler = setOf(DOKUMENTASJON_AV_UTDANNING, DOKUMENTASJON_AV_UTGIFTER),
-    hovedregler = regelIder(DOKUMENTASJON_AV_UTDANNING, DOKUMENTASJON_AV_UTGIFTER)
+    regler = setOf(DOKUMENTASJON_AV_UTDANNING, DOKUMENTASJON_AV_UTGIFTER_UTDANNING),
+    hovedregler = regelIder(DOKUMENTASJON_AV_UTDANNING, DOKUMENTASJON_AV_UTGIFTER_UTDANNING)
 ) {
 
     companion object {
@@ -24,9 +24,9 @@ class DokumentasjonAvUtdanningRegel : Vilkårsregel(
                 )
             )
 
-        private val DOKUMENTASJON_AV_UTGIFTER =
+        private val DOKUMENTASJON_AV_UTGIFTER_UTDANNING =
             RegelSteg(
-                regelId = RegelId.DOKUMENTASJON_AV_UTGIFTER,
+                regelId = RegelId.DOKUMENTASJON_AV_UTGIFTER_UTDANNING,
                 jaNeiSvarRegel(
                     hvisJa = SluttSvarRegel.OPPFYLT_MED_PÅKREVD_BEGRUNNELSE,
                     hvisNei = SluttSvarRegel.IKKE_OPPFYLT_MED_PÅKREVD_BEGRUNNELSE
