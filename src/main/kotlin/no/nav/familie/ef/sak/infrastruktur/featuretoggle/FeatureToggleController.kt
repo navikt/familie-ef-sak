a186f56422a7a28195eb2f7a93bb005d688e828b
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-06-24 15:24:55 +0200

    Toggle prefylling av vedtaksperioder

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
@@ -21,7 +21,8 @@ class FeatureToggleController(private val featureToggleService: FeatureToggleSer
         Toggle.FRONTEND_JOURNALFØRING_KAN_LEGGE_TIL_TERMINBARN,
         Toggle.SKOLEPENGER,
         Toggle.SKOLEPENGER_OPPHØR,
-        Toggle.FRONTEND_SKOLEPENGER_REVURDERING
+        Toggle.FRONTEND_SKOLEPENGER_REVURDERING,
+        Toggle.FRONTEND_PREFYLL_VEDTAKSPERIODER
     )
 
     @GetMapping
