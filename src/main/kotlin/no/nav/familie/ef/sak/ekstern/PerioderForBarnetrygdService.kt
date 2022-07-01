d4777254609630c9da74652e32c0e520970240ac
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-07-01 10:05:19 +0200

    Skal ikke returnere overlappende infotrygdperioder når BA etterspør perioder med full overgangsstønad

--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/PerioderForBarnetrygdService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/PerioderForBarnetrygdService.kt
@@ -1,9 +1,12 @@
 package no.nav.familie.ef.sak.ekstern
 
+import no.nav.familie.ef.sak.felles.dto.Periode
 import no.nav.familie.ef.sak.infotrygd.InternPeriode
 import no.nav.familie.ef.sak.infotrygd.PeriodeService
 import no.nav.familie.kontrakter.felles.PersonIdent
 import no.nav.familie.kontrakter.felles.ef.PeriodeOvergangsstønad
+import no.nav.familie.kontrakter.felles.ef.PeriodeOvergangsstønad.Datakilde.EF
+import no.nav.familie.kontrakter.felles.ef.PeriodeOvergangsstønad.Datakilde.INFOTRYGD
 import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadResponse
 import org.springframework.stereotype.Service
 
@@ -18,12 +21,52 @@ class PerioderForBarnetrygdService(
 ) {
 
     fun hentPerioderMedFullOvergangsstønad(request: PersonIdent): PerioderOvergangsstønadResponse {
-        val perioder = periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(request.ident)
+        val perioderPåDatakilde = periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(request.ident)
             .filter(InternPeriode::erFullOvergangsstønad)
             .map(InternPeriode::tilEksternPeriodeOvergangsstønad)
+            .groupBy { it.datakilde }
 
-        return PerioderOvergangsstønadResponse(perioder)
+        val perioderInfotrygd = infotrygdperioderUtenOverlapp(perioderPåDatakilde.getOrDefault(INFOTRYGD, emptyList()))
+        val perioderEF = perioderPåDatakilde.getOrDefault(EF, emptyList())
+
+        return PerioderOvergangsstønadResponse(perioderEF + perioderInfotrygd)
     }
+
+    private fun infotrygdperioderUtenOverlapp(perioder: List<PeriodeOvergangsstønad>) =
+        perioder
+            .sortedWith(compareByDescending<PeriodeOvergangsstønad> { it.tomDato }.thenByDescending { it.fomDato })
+            .fold<PeriodeOvergangsstønad, MutableList<PeriodeOvergangsstønad>>(mutableListOf()) { acc, gjeldende ->
+                acc.fjernDuplikatOgSplittOverlappendePeriode(gjeldende)
+            }
+            .sortedByDescending { it.fomDato }
+}
+
+private fun PeriodeOvergangsstønad.tilPeriode(): Periode = Periode(this.fomDato, this.tomDato)
+private fun PeriodeOvergangsstønad.omsluttesAv(annen: PeriodeOvergangsstønad): Boolean =
+    this.tilPeriode().omsluttesAv(annen.tilPeriode())
+
+private fun MutableList<PeriodeOvergangsstønad>.fjernDuplikatOgSplittOverlappendePeriode(gjeldende: PeriodeOvergangsstønad): MutableList<PeriodeOvergangsstønad> {
+    val forrige = this.removeLastOrNull()
+    when {
+        forrige == null -> this.add(gjeldende)
+        gjeldende.omsluttesAv(forrige) -> this.add(forrige)
+        forrige.omsluttesAv(gjeldende) -> this.add(gjeldende)
+        gjeldende.fomDato >= forrige.fomDato -> this.add(forrige) // I praksis omsluttes denne av forrige og tidligere perioder
+        gjeldende.tomDato >= forrige.fomDato -> {
+            this.add(forrige.copy(fomDato = maxOf(forrige.fomDato, gjeldende.fomDato)))
+            this.add(
+                gjeldende.copy(
+                    fomDato = minOf(forrige.fomDato, gjeldende.fomDato),
+                    tomDato = maxOf(forrige.fomDato, gjeldende.fomDato).minusDays(1)
+                )
+            )
+        }
+        else -> {
+            this.add(forrige)
+            this.add(gjeldende)
+        }
+    }
+    return this
 }
 
 private fun InternPeriode.tilEksternPeriodeOvergangsstønad(): PeriodeOvergangsstønad =
--- src/test/kotlin/no/nav/familie/ef/sak/ekstern/PerioderForBarnetrygdServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/ekstern/PerioderForBarnetrygdServiceTest.kt
@@ -5,8 +5,10 @@ import io.mockk.mockk
 import no.nav.familie.ef.sak.infotrygd.InternPeriodeTestUtil.lagInternPeriode
 import no.nav.familie.ef.sak.infotrygd.PeriodeService
 import no.nav.familie.kontrakter.felles.PersonIdent
+import no.nav.familie.kontrakter.felles.ef.PeriodeOvergangsstønad
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 internal class PerioderForBarnetrygdServiceTest {
 
@@ -27,5 +29,104 @@ internal class PerioderForBarnetrygdServiceTest {
     internal fun `skal returnere perioder`() {
         every { periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(any()) } returns listOf(lagInternPeriode(beløp = 1))
         assertThat(service.hentPerioderMedFullOvergangsstønad(PersonIdent(personIdent)).perioder).hasSize(1)
+    }
+
+    @Test
+    internal fun `skal splitte opp overlappende perioder`() {
+        val periode1 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 10, 1), stønadTom = LocalDate.of(2009, 1, 31))
+        val periode2 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 12, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        every { periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(any()) } returns listOf(
+            periode1,
+            periode2,
+        )
+        val perioder = service.hentPerioderMedFullOvergangsstønad(PersonIdent(personIdent)).perioder
+        assertThat(perioder).hasSize(2)
+        assertThat(perioder.first().fomDato).isEqualTo(periode2.stønadFom)
+        assertThat(perioder.first().tomDato).isEqualTo(periode2.stønadTom)
+        assertThat(perioder.last().fomDato).isEqualTo(periode1.stønadFom)
+        assertThat(perioder.last().tomDato).isEqualTo(periode2.stønadFom.minusDays(1))
+    }
+
+    @Test
+    internal fun `feilsituasjon prod - skal returnere en periode når den ene omsluttes av den andre`() {
+        val periode1 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 12, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        val periode2 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 10, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        every { periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(any()) } returns
+            listOf(periode1, periode2).sortedByDescending { it.stønadFom }
+        val perioder = service.hentPerioderMedFullOvergangsstønad(PersonIdent(personIdent)).perioder
+        assertThat(perioder).hasSize(1)
+        assertThat(perioder.first().fomDato).isEqualTo(periode2.stønadFom)
+        assertThat(perioder.first().tomDato).isEqualTo(periode2.stønadTom)
+    }
+
+    @Test
+    internal fun `skal ikke endre på perioder som kommer fra EF`() {
+        val periode1 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 12, 1), stønadTom = LocalDate.of(2009, 3, 31), datakilde = PeriodeOvergangsstønad.Datakilde.EF)
+        val periode2 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 10, 1), stønadTom = LocalDate.of(2009, 3, 31), datakilde = PeriodeOvergangsstønad.Datakilde.EF)
+        every { periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(any()) } returns
+            listOf(periode1, periode2).sortedByDescending { it.stønadFom }
+        val perioder = service.hentPerioderMedFullOvergangsstønad(PersonIdent(personIdent)).perioder
+        assertThat(perioder).hasSize(2)
+        assertThat(perioder.first().fomDato).isEqualTo(periode1.stønadFom)
+        assertThat(perioder.first().tomDato).isEqualTo(periode1.stønadTom)
+        assertThat(perioder.last().fomDato).isEqualTo(periode2.stønadFom)
+        assertThat(perioder.last().tomDato).isEqualTo(periode1.stønadTom)
+    }
+
+    @Test
+    internal fun `skal fjerne perioder som omsluttes og splitte opp perioder som overlapper for Infotrygdperioder`() {
+        val periode1 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 12, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        val periode2 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 8, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        val periode3 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 9, 1), stønadTom = LocalDate.of(2009, 1, 31))
+        val periode4 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 1, 1), stønadTom = LocalDate.of(2009, 5, 31))
+        val periode5 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 11, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        val periode6 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2005, 1, 1), stønadTom = LocalDate.of(2005, 3, 31))
+        every { periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(any()) } returns
+            listOf(periode1, periode2, periode3, periode4, periode5, periode6)
+        val perioder = service.hentPerioderMedFullOvergangsstønad(PersonIdent(personIdent)).perioder
+        assertThat(perioder).hasSize(2)
+
+        assertThat(perioder[0].fomDato).isEqualTo(periode4.stønadFom)
+        assertThat(perioder[0].tomDato).isEqualTo(periode4.stønadTom)
+        assertThat(perioder[1].fomDato).isEqualTo(periode6.stønadFom)
+        assertThat(perioder[1].tomDato).isEqualTo(periode6.stønadTom)
+    }
+
+    @Test
+    internal fun `skal fjerne perioder som omsluttes og splitte opp perioder som overlapper for Infotrygdperioder2`() {
+        val periode1 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 12, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        val periode2 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 8, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        val periode3 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 9, 1), stønadTom = LocalDate.of(2009, 1, 31))
+        val periode4 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 10, 1), stønadTom = LocalDate.of(2009, 5, 31))
+        val periode5 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 11, 1), stønadTom = LocalDate.of(2009, 3, 31))
+        val periode6 =
+            lagInternPeriode(beløp = 1, stønadFom = LocalDate.of(2008, 9, 1), stønadTom = LocalDate.of(2009, 1, 31))
+
+        every { periodeService.hentPerioderForOvergangsstønadFraEfOgInfotrygd(any()) } returns
+            listOf(periode1, periode2, periode3, periode4, periode5, periode6)
+        val perioder = service.hentPerioderMedFullOvergangsstønad(PersonIdent(personIdent)).perioder
+        assertThat(perioder).hasSize(2)
+
+        assertThat(perioder[0].fomDato).isEqualTo(periode4.stønadFom)
+        assertThat(perioder[0].tomDato).isEqualTo(periode4.stønadTom)
+        assertThat(perioder[1].fomDato).isEqualTo(periode2.stønadFom)
+        assertThat(perioder[1].tomDato).isEqualTo(periode4.stønadFom.minusDays(1))
     }
 }
