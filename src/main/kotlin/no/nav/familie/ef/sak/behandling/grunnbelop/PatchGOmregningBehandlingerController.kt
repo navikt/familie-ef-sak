06e629771476b8a77d7746925e14e5f8ab231138
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-07-04 13:22:06 +0200

    Rettinger etter QA

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/PatchGOmregningBehandlingerController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/PatchGOmregningBehandlingerController.kt
@@ -2,18 +2,25 @@ package no.nav.familie.ef.sak.behandling.grunnbelop
 
 import no.nav.familie.ef.sak.behandling.BehandlingRepository
 import no.nav.familie.ef.sak.behandlingsflyt.task.BehandlingsstatistikkTask
+import no.nav.familie.ef.sak.behandlingsflyt.task.BehandlingsstatistikkTaskPayload
 import no.nav.familie.kontrakter.ef.felles.BehandlingÅrsak
 import no.nav.familie.kontrakter.ef.iverksett.Hendelse
 import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
 import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.util.Properties
+import java.util.UUID
 
 @RestController
 @RequestMapping(path = ["/api/patch-g-omregning-behandlinger"], produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -27,29 +34,26 @@ class PatchGOmregningBehandlingerController(
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @PostMapping("opprett-tasks")
+    @Transactional
     fun patchBehandlingsstatistikk(@RequestBody liveRun: LiveRun): Ressurs<String> {
         val behandlingerMedÅrsakGOmregning = behandlingRepository.findByÅrsak(BehandlingÅrsak.G_OMREGNING)
 
         logger.info(
-            "Patch behandlingsstatistikk: Funnet ${behandlingerMedÅrsakGOmregning.size} behandlinger med årsak G-Omregning. " +
-                "Oppretter task med status påbegynt og ferdig for disse."
+            "Patch behandlingsstatistikk: Funnet ${behandlingerMedÅrsakGOmregning.filter { it.sporbar.opprettetAv != "VL" }.size} behandlinger med årsak G-Omregning og som er opprettet automatisk. " +
+                "Oppretter task med status påbegynt og ferdig for disse. (skal persistere ${liveRun.skalPersistere})"
         )
 
-        behandlingerMedÅrsakGOmregning.forEach {
-            val taskPåbegynt = BehandlingsstatistikkTask.opprettTask(
+        behandlingerMedÅrsakGOmregning.filter { it.sporbar.opprettetAv != "VL" }.forEach {
+            val taskPåbegynt = opprettTask(
                 it.id,
                 Hendelse.PÅBEGYNT,
-                it.sporbar.opprettetTid,
-                "VL",
-                null
+                it.sporbar.opprettetTid
             )
 
-            val taskFerdigstilt = BehandlingsstatistikkTask.opprettTask(
+            val taskFerdigstilt = opprettTask(
                 it.id,
                 Hendelse.FERDIG,
-                it.sporbar.opprettetTid,
-                "VL",
-                null
+                it.sporbar.endret.endretTid
             )
             if (liveRun.skalPersistere) {
                 taskRepository.save(taskPåbegynt)
@@ -63,3 +67,28 @@ class PatchGOmregningBehandlingerController(
 }
 
 data class LiveRun(val skalPersistere: Boolean)
+
+private fun opprettTask(
+    behandlingId: UUID,
+    hendelse: Hendelse,
+    hendelseTidspunkt: LocalDateTime = LocalDateTime.now()
+): Task =
+    Task(
+        type = BehandlingsstatistikkTask.TYPE,
+        payload = objectMapper.writeValueAsString(
+            BehandlingsstatistikkTaskPayload(
+                behandlingId,
+                hendelse,
+                hendelseTidspunkt,
+                "VL",
+                null
+            )
+        ),
+        properties = Properties().apply {
+            this["saksbehandler"] = "VL"
+            this["behandlingId"] = behandlingId.toString()
+            this["hendelse"] = hendelse.name
+            this["hendelseTidspunkt"] = hendelseTidspunkt.toString()
+            this["oppgaveId"] = ""
+        }
+    )
