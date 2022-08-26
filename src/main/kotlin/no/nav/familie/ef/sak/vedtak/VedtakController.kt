fba643b1e691e5d3a13cf56d2caf7033d809ec09
Johan Blomgren <blommish@users.noreply.github.com>
2022-08-26 13:47:04 +0200

    Endrer til riktig featuretoggle (#1727)

--- src/main/kotlin/no/nav/familie/ef/sak/vedtak/VedtakController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/vedtak/VedtakController.kt
@@ -94,7 +94,7 @@ class VedtakController(
         @PathVariable fra: YearMonth
     ): Ressurs<VedtakDto> {
         tilgangService.validerTilgangTilFagsak(fagsakId, AuditLoggerEvent.ACCESS)
-        brukerfeilHvisIkke(featureToggleService.isEnabled(Toggle.FRONTEND_VIS_IKKE_PUBLISERTE_BREVMALER)) {
+        brukerfeilHvisIkke(featureToggleService.isEnabled(Toggle.FRONTEND_PREFYLL_VEDTAKSPERIODER)) {
             "Feil vid henting av vedtakshistorikk. Det virker som at du sitter med en eldre versjon av saksbehandling, prøv å laste siden på nytt"
         }
         return Ressurs.success(vedtakHistorikkService.hentVedtakForOvergangsstønadFraDato(fagsakId, fra))
