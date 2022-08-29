fc774343dc673adbed0a6790ad6509e1d3fec663
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-08-29 15:51:20 +0200

    Skal tilby endepunkt for ef-mottak som skal vurdere manuell eller automatisk journalføring basert på om det kan opprettes en førstegangsbehandling

--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingController.kt
@@ -1,11 +1,17 @@
 package no.nav.familie.ef.sak.ekstern
 
+import no.nav.familie.ef.sak.infrastruktur.exception.Feil
+import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
+import no.nav.familie.kontrakter.felles.PersonIdent
 import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.ef.StønadType
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -32,5 +38,27 @@ class EksternBehandlingController(
             return Ressurs.failure("Støtter kun identer av typen fnr/dnr")
         }
         return Ressurs.success(eksternBehandlingService.harLøpendeStønad(personidenter))
+    }
+
+    /**
+     * Skal bare brukes av familie-ef-mottak for å vurdere om en journalføring skal automatisk ferdigstilles
+     * eller manuelt gjennomgås.
+     */
+    @PostMapping("kan-opprette-forstegangsbehandling")
+    @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
+    fun kanOppretteFørstegangsbehandling(
+        @RequestBody personIdent: PersonIdent,
+        @RequestParam type: StønadType
+    ): Ressurs<Boolean> {
+        if (!SikkerhetContext.kallKommerFraFamilieEfMottak()) {
+            throw Feil(message = "Kallet utføres ikke av en autorisert klient", httpStatus = HttpStatus.UNAUTHORIZED)
+        }
+        if (personIdent.ident.isEmpty()) {
+            return Ressurs.failure("Mangler personident")
+        }
+        if (personIdent.ident.length != 11) {
+            return Ressurs.failure("Ugyldig antall tegn på personIdent")
+        }
+        return Ressurs.success(eksternBehandlingService.kanOppretteFørstegangsbehandling(personIdent.ident, type))
     }
 }
--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/EksternBehandlingService.kt
@@ -1,7 +1,11 @@
 package no.nav.familie.ef.sak.ekstern
 
 import no.nav.familie.ef.sak.behandling.BehandlingService
+import no.nav.familie.ef.sak.behandling.domain.BehandlingResultat
 import no.nav.familie.ef.sak.fagsak.FagsakService
+import no.nav.familie.ef.sak.fagsak.domain.Fagsak
+import no.nav.familie.ef.sak.infotrygd.InfotrygdService
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
 import no.nav.familie.ef.sak.tilkjentytelse.TilkjentYtelseService
 import no.nav.familie.ef.sak.tilkjentytelse.domain.AndelTilkjentYtelse
 import no.nav.familie.kontrakter.felles.ef.StønadType
@@ -13,7 +17,9 @@ import java.util.UUID
 class EksternBehandlingService(
     val tilkjentYtelseService: TilkjentYtelseService,
     val behandlingService: BehandlingService,
-    val fagsakService: FagsakService
+    val fagsakService: FagsakService,
+    val personService: PersonService,
+    val infotrygdService: InfotrygdService,
 ) {
 
     fun harLøpendeStønad(personidenter: Set<String>): Boolean {
@@ -30,5 +36,22 @@ class EksternBehandlingService(
             .mapNotNull { behandlingService.finnSisteIverksatteBehandling(it.id) }
             .map { it.id }
             .toSet()
+    }
+
+    fun kanOppretteFørstegangsbehandling(ident: String, type: StønadType): Boolean {
+        val allePersonIdenter = personService.hentPersonIdenter(ident).identer.map { it.ident }.toSet()
+        val fagsak = fagsakService.finnFagsak(allePersonIdenter, type)
+
+        val harIngenReelleBehandlinger = fagsak?.let { harIngenReelleBehandlinger(it) } ?: true
+        val harIngenInnslagIInfotrygd = !infotrygdService.eksisterer(ident, setOf(type))
+
+        return harIngenReelleBehandlinger && harIngenInnslagIInfotrygd
+    }
+
+    fun harIngenReelleBehandlinger(fagsak: Fagsak): Boolean {
+        val reelleBehandlinger = behandlingService.hentBehandlinger(fagsak.id)
+            .filter { it.resultat != BehandlingResultat.HENLAGT }
+
+        return reelleBehandlinger.isEmpty()
     }
 }
