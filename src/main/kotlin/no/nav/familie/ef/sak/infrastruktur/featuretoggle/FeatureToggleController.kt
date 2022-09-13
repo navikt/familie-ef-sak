1ef74ffa6f21b797387e73c0a68caded60ce0fc4
Johan Blomgren <blommish@users.noreply.github.com>
2022-09-13 13:07:53 +0200

    Legger til featuretoggle for å vise toggle for opprett klage i frontend

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
@@ -25,7 +25,8 @@ class FeatureToggleController(private val featureToggleService: FeatureToggleSer
         Toggle.FRONTEND_SKOLEPENGER_REVURDERING,
         Toggle.FRONTEND_PREFYLL_VEDTAKSPERIODER,
         Toggle.FRONTEND_GJENBRUKE_VILKÅR,
-        Toggle.FRONTEND_VIS_VEDTAK_LEGG_TIL_RAD_KNAPP
+        Toggle.FRONTEND_VIS_VEDTAK_LEGG_TIL_RAD_KNAPP,
+        Toggle.FRONTEND_OPPRETT_KLAGE
     )
 
     @GetMapping
