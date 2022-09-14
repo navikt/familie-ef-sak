ed512aca41d03ee2cfb3ba8fe7c2aa5e8535a9ed
Johan Blomgren <blommish@users.noreply.github.com>
2022-09-14 09:48:37 +0200

    Endrer api for å hente åpne klager i infotrygd til å returnere et set… (#1773)
    * Endrer api for å hente åpne klager i infotrygd til å returnere et set med stønadstyper for å enklere kunne håndtere dataen i frontend

--- src/main/kotlin/no/nav/familie/ef/sak/klage/dto/ÅpneKlagerDto.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.familie.ef.sak.klage.dto
-
-data class ÅpneKlagerDto(val infotrygd: ÅpneKlagerInfotrygd)
-
-data class ÅpneKlagerInfotrygd(val overgangsstønad: Boolean, val barnetilsyn: Boolean, val skolepenger: Boolean)
