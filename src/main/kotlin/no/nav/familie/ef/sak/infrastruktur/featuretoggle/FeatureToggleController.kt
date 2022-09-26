62f49dc614aa306d4248c8afcd83c1352bd4d290
Viktor <viktor.grondalen.solberg@nav.no>
2022-09-26 13:22:39 +0200

    skal fjerne feature toggle for gjenbrukk av inngangsvilkår

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
@@ -24,7 +24,6 @@ class FeatureToggleController(private val featureToggleService: FeatureToggleSer
         Toggle.SKOLEPENGER_OPPHØR,
         Toggle.FRONTEND_SKOLEPENGER_REVURDERING,
         Toggle.FRONTEND_PREFYLL_VEDTAKSPERIODER,
-        Toggle.FRONTEND_GJENBRUKE_VILKÅR,
         Toggle.FRONTEND_VIS_VEDTAK_LEGG_TIL_RAD_KNAPP,
         Toggle.FRONTEND_OPPRETT_KLAGE
     )
