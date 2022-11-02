13edb2feb7427ac7bf234226944ca3486949fc35
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-02 13:59:57 +0100

    Bruk vedtakstidspunkt fra behandling i BehandlingDto (#1865)

--- src/test/kotlin/no/nav/familie/ef/sak/service/BehandlingshistorikkServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/service/BehandlingshistorikkServiceTest.kt
@@ -3,15 +3,12 @@ package no.nav.familie.ef.sak.service
 import no.nav.familie.ef.sak.OppslagSpringRunnerTest
 import no.nav.familie.ef.sak.behandling.BehandlingRepository
 import no.nav.familie.ef.sak.behandling.domain.Behandling
-import no.nav.familie.ef.sak.behandling.domain.BehandlingStatus
 import no.nav.familie.ef.sak.behandlingsflyt.steg.StegType
 import no.nav.familie.ef.sak.behandlingshistorikk.BehandlingshistorikkRepository
 import no.nav.familie.ef.sak.behandlingshistorikk.BehandlingshistorikkService
 import no.nav.familie.ef.sak.behandlingshistorikk.domain.Behandlingshistorikk
 import no.nav.familie.ef.sak.behandlingshistorikk.domain.tilHendelseshistorikkDto
 import no.nav.familie.ef.sak.behandlingshistorikk.dto.HendelseshistorikkDto
-import no.nav.familie.ef.sak.fagsak.domain.PersonIdent
-import no.nav.familie.ef.sak.felles.domain.SporbarUtils
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.ef.sak.repository.behandling
 import no.nav.familie.ef.sak.repository.fagsak
@@ -95,53 +92,6 @@ internal class BehandlingshistorikkServiceTest : OppslagSpringRunnerTest() {
 
         siste = behandlingshistorikkService.finnSisteBehandlingshistorikk(behandlingId = behandling.id, behandling.steg)
         assertThat(siste!!.opprettetAvNavn).isEqualTo("B")
-    }
-
-    @Test
-    fun `finn sist endret tispunkt for to forskjellige behandlinger, ignorer feil stegtype`() {
-        val stegType = StegType.SEND_TIL_BESLUTTER
-
-        val fagsak1 = testoppsettService.lagreFagsak(fagsak(identer = setOf(PersonIdent("16"))))
-
-        val behandling1 =
-            behandlingRepository.insert(behandling(fagsak = fagsak1, steg = stegType, status = BehandlingStatus.FERDIGSTILT))
-        val behandling2 = behandlingRepository.insert(behandling(fagsak = fagsak1, steg = stegType))
-
-        val tidspunkt = SporbarUtils.now()
-
-        insert(behandling1, "A", tidspunkt.minusDays(1))
-        insert(behandling1, "A", tidspunkt.plusDays(1))
-        insert(behandling1, "A", tidspunkt)
-
-        insert(behandling2, "A", tidspunkt.minusDays(1))
-        insert(behandling2, "A", tidspunkt.plusDays(1))
-        insert(behandling2, "A", tidspunkt)
-
-        val sistEndret = behandlingshistorikkRepository
-            .finnSisteEndringstidspunktForBehandlinger(fagsak1.id, stegType)
-            .sortedBy { it.second }
-        assertThat(sistEndret.size).isEqualTo(2)
-        assertThat(sistEndret[0].second.toLocalDateTime()).isEqualTo(tidspunkt.plusDays(1))
-        assertThat(sistEndret[1].second.toLocalDateTime()).isEqualTo(tidspunkt.plusDays(1))
-    }
-
-    @Test
-    internal fun `skal kun finne behandlingshistorikk til gitt fagsak`() {
-        val stegType = StegType.SEND_TIL_BESLUTTER
-        val tidspunkt = SporbarUtils.now()
-
-        val fagsak1 = testoppsettService.lagreFagsak(fagsak(identer = setOf(PersonIdent("16"))))
-        val fagsak2 = testoppsettService.lagreFagsak(fagsak(identer = setOf(PersonIdent("17"))))
-        val behandling1 = behandlingRepository.insert(behandling(fagsak = fagsak1, steg = stegType))
-        val behandling2 = behandlingRepository.insert(behandling(fagsak = fagsak2, steg = stegType))
-
-        insert(behandling1, "A", tidspunkt.minusDays(1))
-        insert(behandling2, "A", tidspunkt.plusDays(1))
-
-        val sistEndret = behandlingshistorikkRepository
-            .finnSisteEndringstidspunktForBehandlinger(fagsak1.id, stegType)
-
-        assertThat(sistEndret.map { it.first }).containsExactlyInAnyOrder(behandling1.id)
     }
 
     private fun insert(
