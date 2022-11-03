cca37b9cbc43c8499a794881d1eba02e451498de
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-11-03 14:04:08 +0100

    Del av perioderefaktorering: grunnbeløpsdato til grunnbeløpsmåned

--- src/main/kotlin/no/nav/familie/ef/sak/tilkjentytelse/domain/TilkjentYtelse.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/tilkjentytelse/domain/TilkjentYtelse.kt
@@ -7,11 +7,12 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Embedded
 import java.time.LocalDate
+import java.time.YearMonth
 import java.time.temporal.TemporalAdjusters
 import java.util.UUID
 
 /**
- * @param startdato dato for når vi tidligst er ansvarlige for nye perioder.
+ * @param startmåned dato for når vi tidligst er ansvarlige for nye perioder.
  * Når denne er satt kan den aldri flyttes fremover i tid, ettersom vi må ta ansvar for det tidspunktet vi behandlet saken fra.
  *  Den kan endres tilbake i tid hvis vi gjør vedtak med en dato før forrige startdato.
  * I en førstegangsbehandling så settes startdato til den første andelens startdato
@@ -35,13 +36,13 @@ data class TilkjentYtelse(
     val samordningsfradragType: SamordningsfradragType? = null,
     val startdato: LocalDate,
     @Column("grunnbelopsdato")
-    val grunnbeløpsdato: LocalDate = nyesteGrunnbeløp.periode.fomDato,
+    val grunnbeløpsmåned: YearMonth = nyesteGrunnbeløp.periode.fom,
     @Embedded(onEmpty = Embedded.OnEmpty.USE_EMPTY)
     val sporbar: Sporbar = Sporbar()
 ) {
 
     fun taMedAndelerFremTilDato(fom: LocalDate): List<AndelTilkjentYtelse> = andelerTilkjentYtelse
-        .filter { andel -> andel.stønadTom < fom || andel.stønadFom < fom }
+        .filter { andel -> andel.periode.fomDato < fom }
         .map { andel ->
             if (andel.erStønadOverlappende(fom)) {
                 andel.copy(stønadTom = fom.minusMonths(1).with(TemporalAdjusters.lastDayOfMonth()))
