dcaaf17bc1ded273b104b3a2d86ff226ae13f94e
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-07 12:13:49 +0100

    Migrering ignorer etterbetaling (#1870)
    * Skal ignorere etterbetaling hvis saksbehandler har godkjent etterbetalingen

--- src/main/kotlin/no/nav/familie/ef/sak/fagsak/MigreringController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/fagsak/MigreringController.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.sak.fagsak
 
 import no.nav.familie.ef.sak.AuditLoggerEvent
 import no.nav.familie.ef.sak.behandling.migrering.MigreringService
+import no.nav.familie.ef.sak.fagsak.dto.MigrerOvergangsstønadDto
 import no.nav.familie.ef.sak.fagsak.dto.MigreringInfo
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
 import no.nav.familie.kontrakter.felles.Ressurs
@@ -10,6 +11,7 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
@@ -30,9 +32,17 @@ class MigreringController(
     }
 
     @PostMapping("{fagsakPersonId}")
-    fun migrerOvergangsstønad(@PathVariable fagsakPersonId: UUID): Ressurs<UUID> {
+    fun migrerOvergangsstønad(
+        @PathVariable fagsakPersonId: UUID,
+        @RequestBody request: MigrerOvergangsstønadDto?
+    ): Ressurs<UUID> {
         tilgangService.validerTilgangTilFagsakPerson(fagsakPersonId, AuditLoggerEvent.CREATE)
-        return Ressurs.success(migreringService.migrerOvergangsstønad(fagsakPersonId))
+        return Ressurs.success(
+            migreringService.migrerOvergangsstønad(
+                fagsakPersonId,
+                request ?: MigrerOvergangsstønadDto()
+            )
+        )
     }
 
     @PostMapping("{fagsakPersonId}/barnetilsyn")
