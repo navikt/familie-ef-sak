f2f31d10ce747a96475ba548d19f6c524974a895
Johan Blomgren <blommish@users.noreply.github.com>
2022-12-06 10:26:03 +0100

    fixed imports

--- src/main/kotlin/no/nav/familie/ef/sak/vedtak/VedtakRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/vedtak/VedtakRepository.kt
@@ -2,11 +2,8 @@ package no.nav.familie.ef.sak.vedtak
 
 import no.nav.familie.ef.sak.repository.InsertUpdateRepository
 import no.nav.familie.ef.sak.repository.RepositoryInterface
-import no.nav.familie.ef.sak.vedtak.domain.PeriodeWrapper
 import no.nav.familie.ef.sak.vedtak.domain.Vedtak
 import no.nav.familie.ef.sak.vedtak.dto.ResultatType
-import org.springframework.data.jdbc.repository.query.Modifying
-import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
@@ -14,4 +11,5 @@ import java.util.UUID
 interface VedtakRepository : RepositoryInterface<Vedtak, UUID>, InsertUpdateRepository<Vedtak> {
 
     fun findAllByResultatType(resultatType: ResultatType): List<Vedtak>
+
 }
