744f491fd3f4875074068e25fe235add9661cb1f
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-12-07 12:25:18 +0100

    Rettelse

--- src/main/kotlin/no/nav/familie/ef/sak/iverksett/oppgaveterminbarn/OppgaverTerminbarnDryrunController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/iverksett/oppgaveterminbarn/OppgaverTerminbarnDryrunController.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.sak.iverksett.oppgaveterminbarn
 
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Unprotected
 @RequestMapping(path = ["/api/oppgaver-for-terminbarn"])
 class OppgaverTerminbarnDryrunController(private val forberedOppgaverTerminbarnService: ForberedOppgaverTerminbarnService) {
 
