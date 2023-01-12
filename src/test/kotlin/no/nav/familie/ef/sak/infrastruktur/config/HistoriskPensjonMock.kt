d3de1dcb96942d8032ab5c46f1388d97816529e6
Trond L <56258085+throndi@users.noreply.github.com>
2023-01-12 21:29:29 +0100

    Rettelse

--- src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/HistoriskPensjonMock.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/HistoriskPensjonMock.kt
@@ -5,9 +5,11 @@ import io.mockk.mockk
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pensjon.HistoriskPensjonClient
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pensjon.HistoriskPensjonResponse
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 
+@Configuration
 class HistoriskPensjonMock {
 
     @Profile("mock-historiskpensjon")
@@ -15,7 +17,7 @@ class HistoriskPensjonMock {
     @Primary
     fun historiskPensjonClient(): HistoriskPensjonClient {
         val mockk = mockk<HistoriskPensjonClient>()
-        every { mockk.harPensjon(any()) } returns HistoriskPensjonResponse(false, "")
+        every { mockk.harPensjon(any()) } returns HistoriskPensjonResponse(true, "")
         return mockk
     }
 }
