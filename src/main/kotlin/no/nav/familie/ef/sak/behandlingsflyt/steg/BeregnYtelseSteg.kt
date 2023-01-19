0c57138034b9df0e18b43e3cf3c4ab1cb31eb7d8
Johan Blomgren <blommish@users.noreply.github.com>
2023-01-19 17:01:27 +0100

    Legger til navn på argument

--- src/main/kotlin/no/nav/familie/ef/sak/behandlingsflyt/steg/BeregnYtelseSteg.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandlingsflyt/steg/BeregnYtelseSteg.kt
@@ -569,9 +569,9 @@ class BeregnYtelseSteg(
         val beløpsperioder = beregningBarnetilsynService.beregnYtelseBarnetilsyn(
             vedtak.perioder
                 .filter { !it.erMidlertidigOpphør || it.periodetype == PeriodetypeBarnetilsyn.ORDINÆR },
-            vedtak.perioderKontantstøtte,
-            vedtak.tilleggsstønad.perioder,
-            saksbehandling.erMigrering
+            kontantstøttePerioder = vedtak.perioderKontantstøtte,
+            tilleggsstønadsperioder = vedtak.tilleggsstønad.perioder,
+            erMigrering = saksbehandling.erMigrering
         )
         validerRiktigResultattypeForInnvilgetBarnetilsyn(beløpsperioder, vedtak)
         return beløpsperioder
