641db1ba7f2e070ecb2f0746f6c58e6a3e9c9948
Johan Blomgren <blommish@users.noreply.github.com>
2023-02-03 09:51:19 +0100

    Skal kunne hente historisk pensjon på fagsakId då fagsakPersonId ikke er tilgjengelig fra behandling

--- src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/pensjon/HistoriskPensjonService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/pensjon/HistoriskPensjonService.kt
@@ -1,37 +1,50 @@
 package no.nav.familie.ef.sak.opplysninger.personopplysninger.pensjon
 
 import no.nav.familie.ef.sak.fagsak.FagsakPersonService
+import no.nav.familie.ef.sak.fagsak.FagsakService
+import no.nav.familie.ef.sak.infrastruktur.config.getValue
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.identer
-import org.springframework.cache.annotation.CacheConfig
-import org.springframework.cache.annotation.Cacheable
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
-@CacheConfig(cacheManager = "longCache")
 class HistoriskPensjonService(
     private val historiskPensjonClient: HistoriskPensjonClient,
-    val fagsakPersonService: FagsakPersonService,
-    val personService: PersonService
+    private val fagsakPersonService: FagsakPersonService,
+    private val fagsakService: FagsakService,
+    private val personService: PersonService,
+    @Qualifier("longCache")
+    private val cacheManager: CacheManager,
 ) {
 
-    @Cacheable
     fun hentHistoriskPensjon(fagsakPersonId: UUID): HistoriskPensjonResponse {
-        val aktivIdent = fagsakPersonService.hentAktivIdent(fagsakPersonId)
+        return hentHistoriskPensjon(fagsakPersonService.hentAktivIdent(fagsakPersonId))
+    }
+
+    fun hentHistoriskPensjonForFagsak(fagsakId: UUID): HistoriskPensjonResponse {
+        return hentHistoriskPensjon(fagsakService.hentAktivIdent(fagsakId))
+    }
+
+    private fun hentHistoriskPensjon(aktivIdent: String): HistoriskPensjonResponse {
         val identer = personService.hentPersonIdenter(aktivIdent).identer()
         return hentHistoriskPensjon(aktivIdent, identer)
     }
 
-    @Cacheable("hentHistoriskPensjon_aktivIdent")
     fun hentHistoriskPensjon(
         aktivIdent: String,
         identer: Set<String>,
-    ) = historiskPensjonClient.harPensjon(aktivIdent, identer)
+    ): HistoriskPensjonResponse {
+        return cacheManager.getValue("historiskPensjon", aktivIdent) {
+            historiskPensjonClient.harPensjon(aktivIdent, identer)
+        }
+    }
 }
 
 data class HistoriskPensjonResponse(val harPensjonsdata: Boolean, val webAppUrl: String)
 data class EnsligForsoergerRequest(
     val aktivtFoedselsnummer: String,
-    val alleRelaterteFoedselsnummer: Set<String>
+    val alleRelaterteFoedselsnummer: Set<String>,
 )
