bb2c3158cab24bfe92969aeee3e97be22071ca8c
ma10s <mattis.janitz@nav.no>
2023-02-16 12:04:59 +0100

    Vil ikke bruke now() i config. Mappertester som er hardkodet vil da ikke fungere ettersom tiden går.

--- src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/PdlClientConfig.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/config/PdlClientConfig.kt
@@ -219,8 +219,8 @@ class PdlClientConfig {
                     bostedsadresse = bostedsadresse(),
                     deltBosted = listOf(
                         DeltBosted(
-                            LocalDate.now().minusDays(2),
-                            LocalDate.now().plusDays(2),
+                            LocalDate.of(2023,1,1),
+                            LocalDate.of(2053,1,1),
                             null,
                             null,
                             no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.Metadata(false)
