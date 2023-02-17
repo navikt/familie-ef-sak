0a468b33716dd9c6aca8f9bdf70c2193a3169eb1
eirarset <eirik.arseth@nav.no>
2023-02-17 14:23:09 +0100

    Skal bruke statuskode istedenfor feilmelding for å detektere versjonsfeil

--- src/main/kotlin/no/nav/familie/ef/sak/oppgave/OppgaveClient.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/oppgave/OppgaveClient.kt
@@ -74,7 +74,7 @@ class OppgaveClient(
                     "Oppgaven med id=$oppgaveId er allerede ferdigstilt. Prøv å hente oppgaver på nytt.",
                     HttpStatus.BAD_REQUEST
                 )
-            } else if (e.ressurs.melding.contains("Versjonskonflikt")) {
+            } else if (e.httpStatus == HttpStatus.CONFLICT) {
                 throw ApiFeil(
                     "Oppgaven har endret seg siden du sist hentet oppgaver. For å kunne gjøre endringer må du hente oppgaver på nytt.",
                     HttpStatus.CONFLICT
