b0ae77feaf09ee79d22da1f423b9774a6049781f
charliemidtlyng <charlie.midtlyng@bekk.no>
2023-03-07 13:44:04 +0100

    For å se personopplysninger for relasjoner kan det søkes etter personer og deretter opprette fagsakperson for disse. Da vil personopplysningssiden være tilgjengelig med fagsakpersonId

--- src/main/kotlin/no/nav/familie/ef/sak/fagsak/FagsakPersonController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/fagsak/FagsakPersonController.kt
@@ -3,12 +3,19 @@ package no.nav.familie.ef.sak.fagsak
 import no.nav.familie.ef.sak.AuditLoggerEvent
 import no.nav.familie.ef.sak.fagsak.dto.FagsakPersonDto
 import no.nav.familie.ef.sak.fagsak.dto.FagsakPersonUtvidetDto
+import no.nav.familie.ef.sak.felles.dto.PersonIdentDto
+import no.nav.familie.ef.sak.felles.util.FnrUtil
+import no.nav.familie.ef.sak.felles.util.FnrUtil.validerIdent
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.identer
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
@@ -20,7 +27,8 @@ import java.util.UUID
 class FagsakPersonController(
     private val tilgangService: TilgangService,
     private val fagsakPersonService: FagsakPersonService,
-    private val fagsakService: FagsakService
+    private val fagsakService: FagsakService,
+    private val personService: PersonService,
 ) {
 
     @GetMapping("{fagsakPersonId}")
@@ -33,8 +41,8 @@ class FagsakPersonController(
                 person.id,
                 overgangsstønad = fagsaker.overgangsstønad?.id,
                 barnetilsyn = fagsaker.barnetilsyn?.id,
-                skolepenger = fagsaker.skolepenger?.id
-            )
+                skolepenger = fagsaker.skolepenger?.id,
+            ),
         )
     }
 
@@ -48,8 +56,22 @@ class FagsakPersonController(
                 person.id,
                 overgangsstønad = fagsaker.overgangsstønad?.let { fagsakService.fagsakTilDto(it) },
                 barnetilsyn = fagsaker.barnetilsyn?.let { fagsakService.fagsakTilDto(it) },
-                skolepenger = fagsaker.skolepenger?.let { fagsakService.fagsakTilDto(it) }
-            )
+                skolepenger = fagsaker.skolepenger?.let { fagsakService.fagsakTilDto(it) },
+            ),
+        )
+    }
+
+    @PostMapping("", "/person")
+    fun hentFagsakPersonIdForPerson(@RequestBody personIdentRequest: PersonIdentDto): Ressurs<UUID> {
+        validerIdent(personIdentRequest.personIdent)
+        val personIdenter = personService.hentPersonIdenter(personIdentRequest.personIdent)
+        tilgangService.validerTilgangTilPersonMedBarn(personIdentRequest.personIdent, AuditLoggerEvent.ACCESS)
+        val fagsakPersonId = fagsakPersonService.hentEllerOpprettPerson(
+            personIdenter.identer(),
+            personIdenter.gjeldende().ident,
+        ).id
+        return Ressurs.success(
+            fagsakPersonId,
         )
     }
 }
