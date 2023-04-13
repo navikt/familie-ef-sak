060a6081b77bff34d4dfc9a821311400f7f0ccc2
sarahjelle <sara.hjelle@bekk.no>
2023-04-13 10:00:27 +0200

    Skal ikke opprette behandle sak oppgave om behandling har status fatter, iverksetter eller ferdigstill

--- src/main/kotlin/no/nav/familie/ef/sak/behandlingsflyt/task/OpprettOppgaveTask.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandlingsflyt/task/OpprettOppgaveTask.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.ef.sak.behandlingsflyt.task
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.familie.ef.sak.behandling.BehandlingService
+import no.nav.familie.ef.sak.behandling.domain.BehandlingStatus
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.ef.sak.oppgave.OppgaveService
 import no.nav.familie.kontrakter.felles.objectMapper
@@ -8,6 +10,7 @@ import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.util.Properties
@@ -19,7 +22,9 @@ import java.util.UUID
     beskrivelse = "Opprett oppgave i GOSYS for behandling",
     maxAntallFeil = 3,
 )
-class OpprettOppgaveTask(private val oppgaveService: OppgaveService) : AsyncTaskStep {
+class OpprettOppgaveTask(private val oppgaveService: OppgaveService, private val behandlingService: BehandlingService) : AsyncTaskStep {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     /**
      * Då payload er unik per task type, så settes unik inn
@@ -34,12 +39,23 @@ class OpprettOppgaveTask(private val oppgaveService: OppgaveService) : AsyncTask
 
     override fun doTask(task: Task) {
         val data = objectMapper.readValue<OpprettOppgaveTaskData>(task.payload)
+        val oppgavetype = data.oppgavetype
+
+        if(oppgavetype == Oppgavetype.BehandleSak ) {
+            val behandling = behandlingService.hentBehandling(data.behandlingId)
+
+            if(listOf(BehandlingStatus.FATTER_VEDTAK, BehandlingStatus.IVERKSETTER_VEDTAK, BehandlingStatus.FERDIGSTILT).contains(behandling.status)){
+                logger.info("Opprettet ikke oppgave med oppgavetype = $oppgavetype fordi status = ${behandling.status}")
+            }
+        }
+
         val oppgaveId = oppgaveService.opprettOppgave(
             behandlingId = data.behandlingId,
-            oppgavetype = data.oppgavetype,
+            oppgavetype = oppgavetype,
             tilordnetNavIdent = data.tilordnetNavIdent,
             beskrivelse = data.beskrivelse,
         )
+
         task.metadata.setProperty("oppgaveId", oppgaveId.toString())
     }
 
