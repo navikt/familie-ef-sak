63a05cd3daf6046f4a29ee8f0bf99ca56e17a98c
Johan Blomgren <blommish@users.noreply.github.com>
2023-04-25 10:43:00 +0200

    Må encode norske tegn i url etter oppgradering av spring

---
+++ src/test/kotlin/no/nav/familie/ef/sak/tilbakekreving/TilbakekrevingClientTest.kt
@@ -0,0 +1,65 @@
+package no.nav.familie.ef.sak.tilbakekreving
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import no.nav.familie.kontrakter.felles.Ressurs.Companion.success
+import no.nav.familie.kontrakter.felles.ef.StønadType
+import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.kontrakter.felles.tilbakekreving.KanBehandlingOpprettesManueltRespons
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.web.client.RestOperations
+import org.springframework.web.util.UriUtils
+import java.net.URI
+import java.nio.charset.Charset
+
+internal class TilbakekrevingClientTest {
+
+    @AfterEach
+    fun tearDownEachTest() {
+        wiremockServerItem.resetAll()
+    }
+
+    @Test
+    internal fun `skal håndtere Ø i url`() {
+        val encodedStønadstype = UriUtils.encodePath(StønadType.OVERGANGSSTØNAD.name, Charset.forName("UTF-8"))
+        val eksternFagsakId = 1L
+        val jsonResponse = objectMapper.writeValueAsString(success(KanBehandlingOpprettesManueltRespons(true, "")))
+        val url =
+            "/api/ytelsestype/$encodedStønadstype/fagsak/$eksternFagsakId/kanBehandlingOpprettesManuelt/v1"
+        wiremockServerItem.stubFor(
+            WireMock.get(WireMock.urlEqualTo(url))
+                .willReturn(WireMock.okJson(jsonResponse))
+        )
+        val kanBehandlingOpprettesManuelt =
+            client.kanBehandlingOpprettesManuelt(StønadType.OVERGANGSSTØNAD, eksternFagsakId)
+
+        assertThat(kanBehandlingOpprettesManuelt.kanBehandlingOpprettes).isTrue
+    }
+
+    companion object {
+
+        private val restOperations: RestOperations = RestTemplateBuilder().build()
+        lateinit var client: TilbakekrevingClient
+        lateinit var wiremockServerItem: WireMockServer
+
+        @BeforeAll
+        @JvmStatic
+        fun initClass() {
+            wiremockServerItem = WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+            wiremockServerItem.start()
+            client = TilbakekrevingClient(restOperations, URI.create(wiremockServerItem.baseUrl()))
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            wiremockServerItem.stop()
+        }
+    }
+}
