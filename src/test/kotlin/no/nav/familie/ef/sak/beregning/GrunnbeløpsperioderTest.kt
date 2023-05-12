f4340fe74e6b3316aeb1bf6e3ebbf3486707aaf5
eirarset <eirik.arseth@nav.no>
2023-05-12 10:04:10 +0200

    Skal verifisere at nyesteGrunnbeløp er etter 2021 og at forrigeGrunnbeløp er før nyesteGrunnbeløp

---
+++ src/test/kotlin/no/nav/familie/ef/sak/beregning/GrunnbeløpsperioderTest.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.ef.sak.beregning
+
+import org.junit.jupiter.api.Test
+import java.time.YearMonth
+import kotlin.test.assertTrue
+
+internal class GrunnbeløpsperioderTest {
+
+
+    @Test
+    fun nyesteGrunnbeløp() {
+        assertTrue {   Grunnbeløpsperioder.nyesteGrunnbeløp.periode.fom > YearMonth.of(2021, 5)}
+    }
+
+    @Test
+    fun forrigeGrunnbeløp() {
+        assertTrue {   Grunnbeløpsperioder.forrigeGrunnbeløp.periode.fom < Grunnbeløpsperioder.nyesteGrunnbeløp.periode.fom}
+    }
+}
