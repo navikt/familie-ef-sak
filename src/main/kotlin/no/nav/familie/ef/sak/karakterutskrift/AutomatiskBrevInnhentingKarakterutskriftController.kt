936f572e325438c928ec92489455769de90f8c9c
eirarset <eirik.arseth@nav.no>
2023-05-25 13:58:11 +0200

    Skal kunne teste opprettelse av av tasks for enkeltoppgaver
    For å kunne teste grundig ønsker vi en måte å opprette karakterutskriftBrevTask for enkeltoppgaver. Dette skal kune være tilgjengelig i dev

--- src/main/kotlin/no/nav/familie/ef/sak/karakterutskrift/AutomatiskBrevInnhentingKarakterutskriftController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/karakterutskrift/AutomatiskBrevInnhentingKarakterutskriftController.kt
@@ -1,9 +1,13 @@
 package no.nav.familie.ef.sak.karakterutskrift
 
+import no.nav.familie.ef.sak.felles.util.EnvUtil
 import no.nav.familie.ef.sak.infrastruktur.exception.feilHvis
+import no.nav.familie.ef.sak.infrastruktur.exception.feilHvisIkke
 import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.sak.infrastruktur.featuretoggle.Toggle
+import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -25,6 +29,14 @@ class AutomatiskBrevInnhentingKarakterutskriftController(
             "Toggle for automatiske brev for innhenting av karakterutskrift er ikke påskrudd"
         }
         automatiskBrevInnhentingKarakterutskriftService.opprettTasks(KarakterutskriftBrevtype.HOVEDPERIODE, liveRun = karakterUtskriftRequest.liveRun)
+    }
+
+    @PostMapping("/test/opprett-task/oppgave/{oppgaveId}")
+    fun opprettTaskForOppgave(@PathVariable oppgaveId: Long): Ressurs<Unit> {
+        feilHvisIkke(EnvUtil.erIDev()) {
+            "Kan ikke opprette KarakterutskriftbrevTask hvis miljø ikke er dev"
+        }
+        return Ressurs.success(automatiskBrevInnhentingKarakterutskriftService.opprettTaskForOppgave(oppgaveId))
     }
 }
 
