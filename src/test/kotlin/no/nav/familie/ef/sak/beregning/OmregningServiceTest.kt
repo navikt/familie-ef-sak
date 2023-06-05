0fac20b88946a3ee63b3cd6d944fa29a2233af09
ma10s <mattis.janitz@nav.no>
2023-06-05 10:17:27 +0200

    Vil fjerne utkommentert, gammel kode som ble brukt for å teste med indeksjustering av reell inntekt (ikke nedrunnet).

--- src/test/kotlin/no/nav/familie/ef/sak/beregning/OmregningServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/beregning/OmregningServiceTest.kt
@@ -171,21 +171,11 @@ internal class OmregningServiceTest : OppslagSpringRunnerTest() {
             assertThat(iverksettDto.vedtak.tilkjentYtelse?.andelerTilkjentYtelse?.size).isEqualTo(2) // skal være splittet
             // Sjekk andel etter ny g omregningsdato
             val andelTilkjentYtelseOmregnet = finnAndelEtterNyGDato(iverksettDto)!!
-//            assertThat(andelTilkjentYtelseOmregnet.inntekt).isEqualTo(289300) // justert med R
             assertThat(andelTilkjentYtelseOmregnet.inntekt).isEqualTo(289100) // justert med F
-            // assertThat(andelTilkjentYtelseOmregnet.beløp).isEqualTo(12143) // Justert med R
             assertThat(andelTilkjentYtelseOmregnet.beløp).isEqualTo(12151) // Justert med F
-            //   assertThat(andelTilkjentYtelseOmregnet.beløp).isEqualTo(12155)  beløp med 1000 kr nedrunding i beregning
-            // samme beløp - 12155 -> R justert inntekt og 1000 ned i beregning
 
             // Sjekk inntektsperiode etter ny G omregning
             val inntektsperiodeEtterGomregning = finnInntektsperiodeEtterNyGDato(iverksettDto.behandling.behandlingId, 2022)
-            /* Justert med R
-            assertThat(inntektsperiodeEtterGomregning.dagsats?.toInt()).isEqualTo(210)
-            assertThat(inntektsperiodeEtterGomregning.månedsinntekt?.toInt()).isEqualTo(2097)
-            assertThat(inntektsperiodeEtterGomregning.inntekt.toInt()).isEqualTo(209548)
-            assertThat(inntektsperiodeEtterGomregning.totalinntekt().toInt()).isEqualTo(289312)
-             */
 
             assertThat(inntektsperiodeEtterGomregning.dagsats?.toInt()).isEqualTo(0)
             assertThat(inntektsperiodeEtterGomregning.månedsinntekt?.toInt()).isEqualTo(0)
