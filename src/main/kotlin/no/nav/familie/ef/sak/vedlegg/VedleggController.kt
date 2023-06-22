46d8b42b40df430c6f8667f85405591d39a3aaec
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-06-22 13:06:08 +0200

    Gjør journalpoststatus og dokumenttype nullable

--- src/main/kotlin/no/nav/familie/ef/sak/vedlegg/VedleggController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/vedlegg/VedleggController.kt
@@ -52,6 +52,6 @@ class VedleggController(
 data class VedleggRequest(
     val fagsakPersonId: UUID,
     val arkivtemaer: List<Arkivtema>?,
-    val dokumenttype: String,
-    val journalpostStatus: String
+    val dokumenttype: String?,
+    val journalpostStatus: String?
 )
