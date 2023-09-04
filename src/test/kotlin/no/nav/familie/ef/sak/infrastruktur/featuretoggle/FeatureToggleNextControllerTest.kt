85ff6ddefabba440dedf1479d33b97da2d03ba08
throndi <56258085+throndi@users.noreply.github.com>
2023-09-04 14:05:18 +0200

    Henter toggle med toggleId

---
+++ src/test/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleNextControllerTest.kt
@@ -0,0 +1,12 @@
+package no.nav.familie.ef.sak.infrastruktur.featuretoggle
+
+import org.junit.jupiter.api.Test
+
+class FeatureToggleNextControllerTest {
+
+    @Test
+    fun wtf() {
+        val toggle = Toggle.byToggleId("familie.ef.sak.utviklere-med-veilederrolle")
+        val d = 0
+    }
+}
