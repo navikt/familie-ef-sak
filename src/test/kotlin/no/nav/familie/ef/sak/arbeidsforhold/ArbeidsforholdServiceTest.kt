251bba403dfd9e20125b6b36775ade18d2f0521a
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-09-10 21:50:44 +0200

    Linterendringer

--- src/test/kotlin/no/nav/familie/ef/sak/arbeidsforhold/ArbeidsforholdServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/arbeidsforhold/ArbeidsforholdServiceTest.kt
@@ -2,20 +2,15 @@ package no.nav.familie.ef.sak.arbeidsforhold
 
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.mockkObject
 import no.nav.familie.ef.sak.arbeidsforhold.ekstern.ArbeidsforholdClient
 import no.nav.familie.ef.sak.arbeidsforhold.ekstern.ArbeidsforholdService
-import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.arbeidsforhold.Ansettelsesperiode
 import no.nav.familie.kontrakter.felles.arbeidsforhold.Arbeidsforhold
 import no.nav.familie.kontrakter.felles.arbeidsforhold.Periode
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class ArbeidsforholdServiceTest {
@@ -25,25 +20,43 @@ class ArbeidsforholdServiceTest {
 
     @BeforeEach
     fun setup() {
-        every { arbeidsforholdClient.hentArbeidsforhold(any(), any())} returns Ressurs.success(listOf(Arbeidsforhold(ansettelsesperiode = Ansettelsesperiode(
-            Periode(LocalDate.now().minusYears(2), null)
-        ))))
+        every { arbeidsforholdClient.hentArbeidsforhold(any(), any()) } returns Ressurs.success(
+            listOf(
+                Arbeidsforhold(
+                    ansettelsesperiode = Ansettelsesperiode(
+                        Periode(LocalDate.now().minusYears(2), null),
+                    ),
+                ),
+            ),
+        )
     }
 
     @Test
     fun `finnes avsluttet arbeidsforhold siste 6 mnd`() {
-        every { arbeidsforholdClient.hentArbeidsforhold(any(), any())} returns Ressurs.success(listOf(Arbeidsforhold(ansettelsesperiode = Ansettelsesperiode(
-            Periode(LocalDate.now().minusYears(2), LocalDate.now())
-        ))))
+        every { arbeidsforholdClient.hentArbeidsforhold(any(), any()) } returns Ressurs.success(
+            listOf(
+                Arbeidsforhold(
+                    ansettelsesperiode = Ansettelsesperiode(
+                        Periode(LocalDate.now().minusYears(2), LocalDate.now()),
+                    ),
+                ),
+            ),
+        )
         val finnesAvsluttetArbeidsforhold = arbeidsforholdService.finnesAvsluttetArbeidsforholdSisteGittAntallMåneder("1")
         assertThat(finnesAvsluttetArbeidsforhold).isTrue
     }
 
     @Test
     fun `finnes ikke avsluttet arbeidsforhold siste 6 mnd`() {
-        every { arbeidsforholdClient.hentArbeidsforhold(any(), any())} returns Ressurs.success(listOf(Arbeidsforhold(ansettelsesperiode = Ansettelsesperiode(
-            Periode(LocalDate.now().minusYears(2), null)
-        ))))
+        every { arbeidsforholdClient.hentArbeidsforhold(any(), any()) } returns Ressurs.success(
+            listOf(
+                Arbeidsforhold(
+                    ansettelsesperiode = Ansettelsesperiode(
+                        Periode(LocalDate.now().minusYears(2), null),
+                    ),
+                ),
+            ),
+        )
         val finnesAvsluttetArbeidsforhold = arbeidsforholdService.finnesAvsluttetArbeidsforholdSisteGittAntallMåneder("1")
         assertThat(finnesAvsluttetArbeidsforhold).isFalse
     }
