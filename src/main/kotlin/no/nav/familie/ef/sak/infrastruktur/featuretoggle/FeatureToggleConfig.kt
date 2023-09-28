2744d7e5d95e8dd20be613980ecd5342391ddf28
throndi <56258085+throndi@users.noreply.github.com>
2023-09-28 09:56:57 +0200

    Fjerner oppbønning av UnleashService i FeatureToggleConfig siden denne bønnes opp i felles

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleConfig.kt
@@ -1,27 +1,14 @@
 package no.nav.familie.ef.sak.infrastruktur.featuretoggle
 
 import io.getunleash.strategy.Strategy
-import no.nav.familie.unleash.DefaultUnleashService
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
 @Configuration
-class FeatureToggleConfig(
-    @Value("\${UNLEASH_SERVER_API_URL}") private val apiUrl: String,
-    @Value("\${UNLEASH_SERVER_API_TOKEN}") private val apiToken: String,
-    @Value("\${NAIS_APP_NAME}") private val appName: String,
-) {
+class FeatureToggleConfig {
 
     @Bean
     fun strategies(): List<Strategy> {
         return listOf(ByUserIdStrategy(), ByEnvironmentStrategy())
-    }
-
-    @Profile("!mock-featuretoggle")
-    @Bean
-    fun defaultUnleashService(strategies: List<Strategy>): DefaultUnleashService {
-        return DefaultUnleashService(apiUrl, apiToken, appName, strategies)
     }
 }
