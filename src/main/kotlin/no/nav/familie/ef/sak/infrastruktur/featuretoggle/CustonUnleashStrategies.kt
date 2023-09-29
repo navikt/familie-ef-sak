5f96b3181cf9d5fbea317ca7f642ee7bbbdb922f
Trondl <56258085+throndi@users.noreply.github.com>
2023-09-29 10:29:25 +0200

    Retter på local config ifm Unleash Next (#2411)
    * Fjerner oppbønning av UnleashService i FeatureToggleConfig siden denne bønnes opp i felles
    
    * DefaultUnleashService må fjernes fra FeatureToggleMock
    
    * Endrer navn på FeatureToggleService til CustomUnleashStrategies

---
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/CustonUnleashStrategies.kt
@@ -0,0 +1,14 @@
+package no.nav.familie.ef.sak.infrastruktur.featuretoggle
+
+import io.getunleash.strategy.Strategy
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CustonUnleashStrategies {
+
+    @Bean
+    fun strategies(): List<Strategy> {
+        return listOf(ByUserIdStrategy(), ByEnvironmentStrategy())
+    }
+}
