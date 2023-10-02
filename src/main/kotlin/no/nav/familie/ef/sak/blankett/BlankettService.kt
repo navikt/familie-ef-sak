46ffb237a54f214eb7b8b143755ff9c36f2f0234
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2023-10-02 13:08:23 +0200

    Ønsker å ta med vedtakshistorikk i blankett da det kan hende at saksbehandler refererer til dette. Prøver derfor å sende med tidligere historikk til blankett.

--- src/main/kotlin/no/nav/familie/ef/sak/blankett/BlankettService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/blankett/BlankettService.kt
@@ -5,12 +5,14 @@ import no.nav.familie.ef.sak.behandling.Saksbehandling
 import no.nav.familie.ef.sak.behandling.dto.tilDto
 import no.nav.familie.ef.sak.behandling.ÅrsakRevurderingService
 import no.nav.familie.ef.sak.felles.domain.Fil
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.GrunnlagsdataService
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonopplysningerService
 import no.nav.familie.ef.sak.opplysninger.søknad.SøknadDatoerDto
 import no.nav.familie.ef.sak.opplysninger.søknad.SøknadService
 import no.nav.familie.ef.sak.vedtak.VedtakService
 import no.nav.familie.ef.sak.vedtak.dto.VedtakDto
 import no.nav.familie.ef.sak.vilkår.VurderingService
+import no.nav.familie.ef.sak.vilkår.dto.tilDto
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -24,15 +26,23 @@ class BlankettService(
     private val personopplysningerService: PersonopplysningerService,
     private val vedtakService: VedtakService,
     private val årsakRevurderingService: ÅrsakRevurderingService,
+    private val grunnlagsdataService: GrunnlagsdataService,
 ) {
 
     fun lagBlankett(behandlingId: UUID): ByteArray {
         val behandling = behandlingService.hentSaksbehandling(behandlingId)
+        val registergrunnlagData = grunnlagsdataService.hentGrunnlagsdata(behandlingId)
+        val grunnlagsdata = registergrunnlagData.grunnlagsdata
+        grunnlagsdata.tidligereVedtaksperioder.tilDto()
+        val tidligereVedtaksperioder = grunnlagsdata.tidligereVedtaksperioder.tilDto()
+        println("lagBlankett - tidligereVedtaksperioder: $tidligereVedtaksperioder") // TODO slett
+
         val blankettPdfRequest = BlankettPdfRequest(
             BlankettPdfBehandling(
                 årsak = behandling.årsak,
                 stønadstype = behandling.stønadstype,
                 årsakRevurdering = årsakRevurderingService.hentÅrsakRevurdering(behandlingId)?.tilDto(),
+                tidligereVedtaksperioder = tidligereVedtaksperioder,
             ),
             lagPersonopplysningerDto(behandling),
             vurderingService.hentEllerOpprettVurderinger(behandlingId),
