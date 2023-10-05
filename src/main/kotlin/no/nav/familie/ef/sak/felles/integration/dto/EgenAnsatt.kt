da396fbbcb8bc32926c0038424e058929a37d655
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-10-05 13:32:18 +0200

    Rettet request-body

--- src/main/kotlin/no/nav/familie/ef/sak/felles/integration/dto/EgenAnsatt.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/felles/integration/dto/EgenAnsatt.kt
@@ -1,5 +1,5 @@
 package no.nav.familie.ef.sak.felles.integration.dto
 
-data class EgenAnsattRequest(val ident: String)
+data class EgenAnsattRequest(val personident: String)
 
 data class EgenAnsattResponse(val erEgenAnsatt: Boolean)
