b74a400efa9ef0b5b01e0f49ec106763ac775853
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2023-10-10 12:42:21 +0200

    Linting...

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/BeregningController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/BeregningController.kt
@@ -13,7 +13,13 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
 import java.util.UUID
 
 @RestController
