d49ee08be9502f476abb3c300bd482f272a713eb
Trondl <56258085+throndi@users.noreply.github.com>
2023-11-27 09:47:44 +0100

    Datoer for konsistensavstemming 2024 (#2466)

---
+++ src/main/resources/db/migration/V141__konsistensavstemming_tidsplan_2024.sql
@@ -0,0 +1,24 @@
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('04.01.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('30.01.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('28.02.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('29.03.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('25.04.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('24.05.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('28.06.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('30.07.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('30.08.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('27.09.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('30.10.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('25.11.2024', 'DD.MM.YYYY'));
