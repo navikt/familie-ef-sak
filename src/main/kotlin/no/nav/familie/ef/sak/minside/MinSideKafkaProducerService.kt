536ab24b0215b3a828980d4e86ff5ff991aa28d7
charliemidtlyng <charlie.midtlyng@bekk.no>
2023-12-19 14:53:55 +0100

    Skal ha et forvaltningsendepunkt for å aktivere og deaktivere personer mtp tilgang til minside-mikrofrontend for EF

---
+++ src/main/kotlin/no/nav/familie/ef/sak/minside/MinSideKafkaProducerService.kt
@@ -0,0 +1,43 @@
+package no.nav.familie.ef.sak.minside
+
+import no.nav.familie.log.IdUtils
+import no.nav.familie.log.mdc.MDCConstants
+import no.nav.tms.microfrontend.MicrofrontendMessageBuilder
+import no.nav.tms.microfrontend.Sensitivitet
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class MinSideKafkaProducerService(private val kafkaTemplate: KafkaTemplate<String, String>) {
+
+    @Value("\${MIN_SIDE_TOPIC}")
+    lateinit var topic: String
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    fun aktiver(personIdent: String) {
+        val melding = MicrofrontendMessageBuilder.enable {
+            ident = personIdent
+            initiatedBy = "teamfamilie"
+            microfrontendId = "familie-ef-mikrofrontend-minside"
+            sensitivitet = Sensitivitet.HIGH
+        }.text()
+        val callId = MDC.get(MDCConstants.MDC_CALL_ID) ?: IdUtils.generateId()
+        logger.info("Sender aktivere minside melding for callId=$callId")
+        kafkaTemplate.send(topic, callId, melding)
+    }
+
+    fun deaktiver(personIdent: String) {
+        val melding = MicrofrontendMessageBuilder.disable {
+            ident = personIdent
+            initiatedBy = "teamfamilie"
+            microfrontendId = "familie-ef-mikrofrontend-minside"
+        }.text()
+        val callId = MDC.get(MDCConstants.MDC_CALL_ID) ?: IdUtils.generateId()
+        logger.info("Sender deaktivere minside melding for callId=$callId")
+        kafkaTemplate.send(topic, callId, melding)
+    }
+}
