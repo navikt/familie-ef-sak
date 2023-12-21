0472ceb1c1cc119ef35fb16e1cb784e38c53d9be
throndi <56258085+throndi@users.noreply.github.com>
2023-12-21 15:08:37 +0100

    Fjerner leader-client sjekk

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
@@ -7,7 +7,6 @@ import no.nav.familie.ef.sak.vedtak.dto.PeriodeMedBeløpDto
 import no.nav.familie.ef.sak.vedtak.dto.UtgiftsperiodeDto
 import no.nav.familie.ef.sak.vedtak.historikk.AndelHistorikkDto
 import no.nav.familie.ef.sak.vedtak.historikk.VedtakHistorikkService
-import no.nav.familie.leader.LeaderClient
 import no.nav.familie.prosessering.internal.TaskService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -103,11 +102,9 @@ class BarnetilsynSatsendringService(
 
     @Transactional
     fun opprettTask() {
-        if (LeaderClient.isLeader() == true) {
-            logger.info("Oppretter satsendring-task")
-            val task = BarnetilsynSatsendringTask.opprettTask()
-            taskService.save(task)
-        }
+        logger.info("Oppretter satsendring-task")
+        val task = BarnetilsynSatsendringTask.opprettTask()
+        taskService.save(task)
     }
 }
 
