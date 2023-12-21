ff4ef3d4844836a223b0cd64ec46ef07eedb991a
throndi <56258085+throndi@users.noreply.github.com>
2023-12-21 14:24:02 +0100

    Utfører task bare hvis det er en leader-pod

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
@@ -7,6 +7,7 @@ import no.nav.familie.ef.sak.vedtak.dto.PeriodeMedBeløpDto
 import no.nav.familie.ef.sak.vedtak.dto.UtgiftsperiodeDto
 import no.nav.familie.ef.sak.vedtak.historikk.AndelHistorikkDto
 import no.nav.familie.ef.sak.vedtak.historikk.VedtakHistorikkService
+import no.nav.familie.leader.LeaderClient
 import no.nav.familie.prosessering.internal.TaskService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -102,9 +103,11 @@ class BarnetilsynSatsendringService(
 
     @Transactional
     fun opprettTask() {
-        logger.info("Oppretter satsendring-task")
-        val task = BarnetilsynSatsendringTask.opprettTask()
-        taskService.save(task)
+        if (LeaderClient.isLeader() == true) {
+            logger.info("Oppretter satsendring-task")
+            val task = BarnetilsynSatsendringTask.opprettTask()
+            taskService.save(task)
+        }
     }
 }
 
