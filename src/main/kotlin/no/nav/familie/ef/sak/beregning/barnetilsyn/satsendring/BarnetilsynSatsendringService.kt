d05d000a961fe62dcea7ceb28b45affb44f67485
Trondl <56258085+throndi@users.noreply.github.com>
2023-12-22 15:42:32 +0100

    Logger antall fagsaker som må revurderes etter satsendring (#2490)
    * Logger antall fagsaker som må revurderes etter satsendring
    
    * ktlint

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
@@ -36,7 +36,10 @@ class BarnetilsynSatsendringService(
         if (barnetilsynGjeldeneAvstemmingsfeil.isEmpty()) {
             val fagsakerSomMåRevurderesGrunnetSatsendring =
                 finnFagsakerSomSkalSatsendresMedNySats(true)
-
+            logger.info(
+                "Antall fagsaker som må revurderes grunnet satsendring: " +
+                    "${fagsakerSomMåRevurderesGrunnetSatsendring.size}",
+            )
             fagsakerSomMåRevurderesGrunnetSatsendring.forEach {
                 logger.info("${it.fagsakId}: skal revurderes/endres etter satsendring")
             }
@@ -67,7 +70,10 @@ class BarnetilsynSatsendringService(
         nåværendeAndelerForNesteÅr.any { it.andel.periode.overlapper(nyMånedsberegning.periode) && it.andel.beløp < nyMånedsberegning.beløp }
     }
 
-    private fun gjørNyBeregning(andelerNesteÅr: List<AndelHistorikkDto>, brukIkkeVedtatteSatser: Boolean = false): List<BeløpsperiodeBarnetilsynDto> {
+    private fun gjørNyBeregning(
+        andelerNesteÅr: List<AndelHistorikkDto>,
+        brukIkkeVedtatteSatser: Boolean = false,
+    ): List<BeløpsperiodeBarnetilsynDto> {
         val utgiftsperiode = mapAndelerForNesteÅrTilUtgiftsperiodeDto(andelerNesteÅr)
 
         val simulertNyBeregning =
