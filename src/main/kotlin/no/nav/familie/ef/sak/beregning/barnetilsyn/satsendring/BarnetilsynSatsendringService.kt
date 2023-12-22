5cf1ec7245cd5afa0c41b6157b8ea7c53414960d
Trondl <56258085+throndi@users.noreply.github.com>
2023-12-22 10:31:55 +0100

    Fjerner leader-client sjekk, og legger til unik payload (#2488)
    * Fjerner leader-client sjekk
    
    * Lar payload være YearMonth ved oppretting av task. Tar tilbake if-sjekk om tasken finnes fra før, siden vi nå har unik payload
    
    * Logger om scheduler kjøres, og legger til LeaderClient for å unngå race conditions
    
    * Fjernet random sleep

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringService.kt
@@ -104,9 +104,13 @@ class BarnetilsynSatsendringService(
     @Transactional
     fun opprettTask() {
         if (LeaderClient.isLeader() == true) {
-            logger.info("Oppretter satsendring-task")
-            val task = BarnetilsynSatsendringTask.opprettTask()
-            taskService.save(task)
+            val payload = YearMonth.now().toString()
+            val finnesTask = taskService.finnTaskMedPayloadOgType(payload, BarnetilsynSatsendringTask.TYPE)
+            if (finnesTask == null) {
+                logger.info("Oppretter satsendring-task, da den ikke finnes fra før")
+                val task = BarnetilsynSatsendringTask.opprettTask(payload)
+                taskService.save(task)
+            }
         }
     }
 }
