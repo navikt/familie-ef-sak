50ec211f607f5fe2a04150f12c8c7a79cf2da789
charliemidtlyng <charlie.midtlyng@bekk.no>
2024-01-08 12:45:33 +0100

    Skal filtrere vekk behandlinger som har 0 i beløp, 0 i inntekt og følgelig bare samordningsfradrag i utplukk for utdaterte g-verdier siden saksbehandlerne ikke revurderer null-utbetalinger

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/FinnBehandlingerMedGammelGTask.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/grunnbelop/FinnBehandlingerMedGammelGTask.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ef.sak.behandling.grunnbelop
 import no.nav.familie.ef.sak.behandling.BehandlingRepository
 import no.nav.familie.ef.sak.beregning.Grunnbeløpsperioder
 import no.nav.familie.ef.sak.infrastruktur.exception.feilHvis
+import no.nav.familie.ef.sak.tilkjentytelse.TilkjentYtelseService
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
@@ -16,15 +17,29 @@ import org.springframework.stereotype.Service
 )
 class FinnBehandlingerMedGammelGTask(
     val behandlingRepository: BehandlingRepository,
+    val tilkjentYtelseService: TilkjentYtelseService
 ) : AsyncTaskStep {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     override fun doTask(task: Task) {
         logger.info("Starter jobb som finner behandlinger som ikke har blitt g-omregnet")
-        val behandlingsIdMedUtdatertG = behandlingRepository.finnFerdigstilteBehandlingerMedUtdatertGBelopSomMåBehandlesManuelt(Grunnbeløpsperioder.nyesteGrunnbeløp.periode.fomDato)
-        behandlingsIdMedUtdatertG.forEach { behandlingsID -> logger.info("Behandling med id $behandlingsID har utdatert G") }
-        feilHvis(behandlingsIdMedUtdatertG.isNotEmpty()) { "Ferdigstilte behandlinger med utdatert G" }
+        val gjeldendeGrunnbeløpFraOgMedDato = Grunnbeløpsperioder.nyesteGrunnbeløp.periode.fomDato
+        val behandlingsIdMedUtdatertG = behandlingRepository.finnFerdigstilteBehandlingerMedUtdatertGBelopSomMåBehandlesManuelt(
+            gjeldendeGrunnbeløpFraOgMedDato
+        )
+        val behandlingerSomBurdeBlittGOmregnet = behandlingsIdMedUtdatertG.filter { behandlingId ->
+            val tilkjentYtelse = tilkjentYtelseService.hentForBehandling(behandlingId)
+            val harSamordningMenIkkeOmregnet = tilkjentYtelse.andelerTilkjentYtelse
+                .filter { it.stønadTom >= gjeldendeGrunnbeløpFraOgMedDato }
+                .all { it.beløp == 0 && it.samordningsfradrag > 0 && it.inntektsreduksjon == 0 }
+            if (harSamordningMenIkkeOmregnet) {
+                logger.info("Behandling $behandlingId har kun samordningsfradrag og er følgelig ikke g-omregnet manuelt av noen saksbehandlere")
+            }
+            !harSamordningMenIkkeOmregnet
+        }
+        behandlingerSomBurdeBlittGOmregnet.forEach { behandlingId -> logger.info("Behandling med id $behandlingId har utdatert G") }
+        feilHvis(behandlingerSomBurdeBlittGOmregnet.isNotEmpty()) { "Ferdigstilte behandlinger med utdatert G" }
     }
 
     companion object {
