7fcb50c00a267cc8c923e23f92472c3141cbcc0e
Mattis Janitz <mattis.janitz@nav.no>
2024-01-12 12:42:15 +0100

    Swaggerforbedringer (#2511)
    * Vil legge til mulighet for å logge inn med sso (forvalter rolle)
    
    * Vil legge til filtermulighet på Alle, Ekstern og Forvalter

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/config/SwaggerDocumentationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/config/SwaggerDocumentationConfig.kt
@@ -2,30 +2,60 @@ package no.nav.familie.ef.sak.infrastruktur.config
 
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.OAuthFlow
+import io.swagger.v3.oas.models.security.OAuthFlows
+import io.swagger.v3.oas.models.security.Scopes
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerDocumentationConfig {
-
-    private val bearer = "Bearer"
-
+class SwaggerDocumentationConfig(
+    @Value("\${AUTHORIZATION_URL}")
+    val authorizationUrl: String,
+    @Value("\${AZUREAD_TOKEN_ENDPOINT_URL}")
+    val tokenUrl: String,
+    @Value("\${API_SCOPE}")
+    val apiScope: String,
+) {
     @Bean
     fun openApi(): OpenAPI {
-        return OpenAPI().info(Info().title("Familie ef sak api"))
-            .components(Components().addSecuritySchemes(bearer, bearerTokenSecurityScheme()))
-            .addSecurityItem(SecurityRequirement().addList(bearer, listOf("read", "write")))
+        return OpenAPI()
+            .components(Components().addSecuritySchemes("oauth2", securitySchemes()))
+            .addSecurityItem(SecurityRequirement().addList("oauth2", listOf("read", "write")))
     }
 
-    private fun bearerTokenSecurityScheme(): SecurityScheme {
+    @Bean
+    fun eksternOpenApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder().group("Ekstern").packagesToScan("no.nav.familie.ef.sak.ekstern").build()
+    }
+
+    @Bean
+    fun internOpenApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder().group("Alle").packagesToScan("no.nav.familie.ef.sak").build()
+    }
+
+    @Bean
+    fun forvalterOpenApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder().group("Forvalter").packagesToScan("no.nav.familie.ef.sak.forvaltning").build()
+    }
+
+    private fun securitySchemes(): SecurityScheme {
         return SecurityScheme()
-            .type(SecurityScheme.Type.APIKEY)
-            .scheme(bearer)
-            .bearerFormat("JWT")
+            .name("oauth2")
+            .type(SecurityScheme.Type.OAUTH2)
+            .scheme("oauth2")
             .`in`(SecurityScheme.In.HEADER)
-            .name("Authorization")
+            .flows(
+                OAuthFlows()
+                    .authorizationCode(
+                        OAuthFlow().authorizationUrl(authorizationUrl)
+                            .tokenUrl(tokenUrl)
+                            .scopes(Scopes().addString(apiScope, "read,write")),
+                    ),
+            )
     }
 }
