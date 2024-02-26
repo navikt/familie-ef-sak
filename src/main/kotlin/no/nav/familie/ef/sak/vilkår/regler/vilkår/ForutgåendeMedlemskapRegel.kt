316bbb3803c46bd5b7ecb6648eb90060cfb5d002
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-02-26 15:43:14 +0100

    Skal ikke importere med "*"

--- src/main/kotlin/no/nav/familie/ef/sak/vilkår/regler/vilkår/ForutgåendeMedlemskapRegel.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/vilkår/regler/vilkår/ForutgåendeMedlemskapRegel.kt
@@ -4,15 +4,22 @@ import no.nav.familie.ef.sak.opplysninger.personopplysninger.dto.Folkeregisterpe
 import no.nav.familie.ef.sak.vilkår.Delvilkårsvurdering
 import no.nav.familie.ef.sak.vilkår.VilkårType
 import no.nav.familie.ef.sak.vilkår.Vilkårsresultat
-import no.nav.familie.ef.sak.vilkår.regler.*
-import java.util.*
+import no.nav.familie.ef.sak.vilkår.regler.HovedregelMetadata
+import no.nav.familie.ef.sak.vilkår.regler.NesteRegel
+import no.nav.familie.ef.sak.vilkår.regler.RegelId
+import no.nav.familie.ef.sak.vilkår.regler.RegelSteg
+import no.nav.familie.ef.sak.vilkår.regler.SluttSvarRegel
+import no.nav.familie.ef.sak.vilkår.regler.SvarId
+import no.nav.familie.ef.sak.vilkår.regler.Vilkårsregel
+import no.nav.familie.ef.sak.vilkår.regler.jaNeiSvarRegel
+import no.nav.familie.ef.sak.vilkår.regler.regelIder
+import java.util.UUID
 
 class ForutgåendeMedlemskapRegel : Vilkårsregel(
     vilkårType = VilkårType.FORUTGÅENDE_MEDLEMSKAP,
     regler = setOf(SØKER_MEDLEM_I_FOLKETRYGDEN, MEDLEMSKAP_UNNTAK),
     hovedregler = regelIder(SØKER_MEDLEM_I_FOLKETRYGDEN),
 ) {
-
     override fun initiereDelvilkårsvurdering(
         metadata: HovedregelMetadata,
         resultat: Vilkårsresultat,
@@ -36,19 +43,19 @@ class ForutgåendeMedlemskapRegel : Vilkårsregel(
     }
 
     companion object {
-
-        private val unntakSvarMapping = setOf(
-            SvarId.MEDLEM_MER_ENN_5_ÅR_AVBRUDD_MINDRE_ENN_10_ÅR,
-            SvarId.MEDLEM_MER_ENN_7_ÅR_AVBRUDD_MER_ENN_10ÅR,
-            SvarId.I_LANDET_FOR_GJENFORENING_ELLER_GIFTE_SEG,
-            SvarId.ANDRE_FORELDER_MEDLEM_SISTE_5_ÅR,
-            SvarId.ANDRE_FORELDER_MEDLEM_MINST_5_ÅR_AVBRUDD_MINDRE_ENN_10_ÅR,
-            SvarId.ANDRE_FORELDER_MEDLEM_MINST_7_ÅR_AVBRUDD_MER_ENN_10_ÅR,
-            SvarId.TOTALVURDERING_OPPFYLLER_FORSKRIFT,
-            SvarId.MEDLEM_MER_ENN_5_ÅR_EØS,
-            SvarId.MEDLEM_MER_ENN_5_ÅR_EØS_ANNEN_FORELDER_TRYGDEDEKKET_I_NORGE,
-        ).associateWith { SluttSvarRegel.OPPFYLT_MED_PÅKREVD_BEGRUNNELSE } +
-            mapOf(SvarId.NEI to SluttSvarRegel.IKKE_OPPFYLT_MED_PÅKREVD_BEGRUNNELSE)
+        private val unntakSvarMapping =
+            setOf(
+                SvarId.MEDLEM_MER_ENN_5_ÅR_AVBRUDD_MINDRE_ENN_10_ÅR,
+                SvarId.MEDLEM_MER_ENN_7_ÅR_AVBRUDD_MER_ENN_10ÅR,
+                SvarId.I_LANDET_FOR_GJENFORENING_ELLER_GIFTE_SEG,
+                SvarId.ANDRE_FORELDER_MEDLEM_SISTE_5_ÅR,
+                SvarId.ANDRE_FORELDER_MEDLEM_MINST_5_ÅR_AVBRUDD_MINDRE_ENN_10_ÅR,
+                SvarId.ANDRE_FORELDER_MEDLEM_MINST_7_ÅR_AVBRUDD_MER_ENN_10_ÅR,
+                SvarId.TOTALVURDERING_OPPFYLLER_FORSKRIFT,
+                SvarId.MEDLEM_MER_ENN_5_ÅR_EØS,
+                SvarId.MEDLEM_MER_ENN_5_ÅR_EØS_ANNEN_FORELDER_TRYGDEDEKKET_I_NORGE,
+            ).associateWith { SluttSvarRegel.OPPFYLT_MED_PÅKREVD_BEGRUNNELSE } +
+                mapOf(SvarId.NEI to SluttSvarRegel.IKKE_OPPFYLT_MED_PÅKREVD_BEGRUNNELSE)
         private val MEDLEMSKAP_UNNTAK =
             RegelSteg(
                 regelId = RegelId.MEDLEMSKAP_UNNTAK,
@@ -58,10 +65,11 @@ class ForutgåendeMedlemskapRegel : Vilkårsregel(
         private val SØKER_MEDLEM_I_FOLKETRYGDEN =
             RegelSteg(
                 regelId = RegelId.SØKER_MEDLEM_I_FOLKETRYGDEN,
-                svarMapping = jaNeiSvarRegel(
-                    hvisJa = SluttSvarRegel.OPPFYLT_MED_PÅKREVD_BEGRUNNELSE,
-                    hvisNei = NesteRegel(MEDLEMSKAP_UNNTAK.regelId),
-                ),
+                svarMapping =
+                    jaNeiSvarRegel(
+                        hvisJa = SluttSvarRegel.OPPFYLT_MED_PÅKREVD_BEGRUNNELSE,
+                        hvisNei = NesteRegel(MEDLEMSKAP_UNNTAK.regelId),
+                    ),
             )
     }
 }
