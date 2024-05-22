9d8e62f0b173e04d1e5c4982de79b667d55f875a
ma10s <mattis.janitz@nav.no>
2024-05-22 15:11:54 +0200

    Vil slette tilbakekrevingsvalg dersom vi i nyeste simulering ikke har noen feilutbetaling, eller dersom beløp i gammel/ny er forskjellig og tilbakekrevingsvalg er "automatisk behandle under fire rettsgebyr".

---
+++ src/main/kotlin/no/nav/familie/ef/sak/tilbakekreving/TilbakekrevingOppryddingService.kt
@@ -0,0 +1,58 @@
+package no.nav.familie.ef.sak.tilbakekreving
+
+import no.nav.familie.ef.sak.behandling.BehandlingRepository
+import no.nav.familie.ef.sak.infrastruktur.exception.feilHvis
+import no.nav.familie.ef.sak.repository.findByIdOrThrow
+import no.nav.familie.ef.sak.simulering.SimuleringsresultatRepository
+import no.nav.familie.ef.sak.tilbakekreving.domain.Tilbakekrevingsvalg
+import no.nav.familie.kontrakter.felles.simulering.Simuleringsoppsummering
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class TilbakekrevingOppryddingService(
+    private val tilbakekrevingRepository: TilbakekrevingRepository,
+    private val simuleringsresultatRepository: SimuleringsresultatRepository,
+    private val behandlingRepository: BehandlingRepository,
+) {
+    fun slettTilbakekrevingsvalgHvisIngenFeilutbetalingEllerForskjelligBeløp(
+        behandlingId: UUID,
+        nySimuleringsoppsummering: Simuleringsoppsummering,
+    ) {
+        if (nySimuleringsoppsummering.harIngenFeilutbetaling()) {
+            slettTilbakekrevingsvalg(behandlingId)
+        } else {
+            val oppsummering = hentLagretSimmuleringsresultat(behandlingId)
+            if (oppsummering != null && nySimuleringsoppsummering.harUlikeBeløp(oppsummering)) {
+                slettTilbakekrevingsvalgUnder4rettsgebyr(behandlingId)
+            }
+        }
+    }
+
+    private fun slettTilbakekrevingsvalgUnder4rettsgebyr(behandlingId: UUID) {
+        val tilbakekreving = tilbakekrevingRepository.findByIdOrThrow(behandlingId)
+        if (tilbakekreving.valg == Tilbakekrevingsvalg.OPPRETT_AUTOMATISK) {
+            slettTilbakekrevingsvalg(behandlingId)
+        }
+    }
+
+    private fun hentLagretSimmuleringsresultat(behandlingId: UUID): Simuleringsoppsummering? {
+        val simuleringsresultat = simuleringsresultatRepository.findByIdOrNull(behandlingId)
+        return simuleringsresultat?.beriketData?.oppsummering
+    }
+
+    private fun Simuleringsoppsummering.harIngenFeilutbetaling() = this.feilutbetaling.toLong() == 0L
+
+    private fun Simuleringsoppsummering.harUlikeBeløp(oppsummering: Simuleringsoppsummering): Boolean {
+        return this.feilutbetaling != oppsummering.feilutbetaling || this.etterbetaling != oppsummering.etterbetaling
+    }
+
+    fun slettTilbakekrevingsvalg(behandlingId: UUID) {
+        val behandling = behandlingRepository.findByIdOrThrow(behandlingId)
+        feilHvis(behandling.status.behandlingErLåstForVidereRedigering()) {
+            "Kan ikke slette tilbakekrevingsvalg for behandling=$behandlingId da den er låst"
+        }
+        tilbakekrevingRepository.deleteById(behandlingId)
+    }
+}
