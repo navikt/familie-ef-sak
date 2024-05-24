3148805543955893e9ae6b851cf9ad5bfc74b1fb
charliemidtlyng <charlie.midtlyng@bekk.no>
2024-05-24 09:25:23 +0200

    Skal tåle at historisk pensjon er nede når vi oppretter behandlinger og henter ut info om stønadshistorikk for personer

--- src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/pensjon/HistoriskPensjonClient.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/pensjon/HistoriskPensjonClient.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ef.sak.opplysninger.personopplysninger.pensjon
 
 import no.nav.familie.http.client.AbstractRestClient
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -14,17 +15,29 @@ class HistoriskPensjonClient(
     private val historiskPensjonUri: URI,
     @Qualifier("azure") restOperations: RestOperations,
 ) : AbstractRestClient(restOperations, "pensjon") {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     private fun lagHarPensjonUri() =
         UriComponentsBuilder.fromUri(historiskPensjonUri).pathSegment("api/ensligForsoerger/harPensjonsdata")
             .build().toUri()
 
-    fun harPensjon(
+    val manglendeRespons = HistoriskPensjonResponse(false, "")
+
+    fun hentHistoriskPensjonStatusForIdent(
         aktivIdent: String,
         alleRelaterteFoedselsnummer: Set<String>,
-    ): HistoriskPensjonResponse {
-        return postForEntity(
-            lagHarPensjonUri(),
-            EnsligForsoergerRequest(aktivIdent, alleRelaterteFoedselsnummer),
-        )
+    ): HistoriskPensjonDto {
+        repeat(2) {
+            try {
+                return postForEntity<HistoriskPensjonResponse>(
+                    lagHarPensjonUri(),
+                    EnsligForsoergerRequest(aktivIdent, alleRelaterteFoedselsnummer),
+                ).tilDto()
+            } catch (e: Exception) {
+                logger.error("Kunne ikke kalle historisk pensjon for uthenting")
+                secureLogger.error("Kunne ikke kalle historisk pensjon for uthenting", e)
+            }
+        }
+        return HistoriskPensjonDto(HistoriskPensjonStatus.UKJENT, null)
     }
 }
