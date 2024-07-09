e6b385267d1743a89f988ec2ce76cd5007f97c09
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-07-09 13:08:15 +0200

    Logg fullmaktresponse

--- src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/fullmakt/FullmaktService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/opplysninger/personopplysninger/fullmakt/FullmaktService.kt
@@ -2,15 +2,19 @@ package no.nav.familie.ef.sak.opplysninger.personopplysninger.fullmakt
 
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.Fullmakt
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.MotpartsRolle
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class FullmaktService(
     val fullmaktClient: FullmaktClient,
 ) {
+
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
     fun hentFullmakt(ident: String): List<Fullmakt> {
         val fullmaktResponse = fullmaktClient.hentFullmakt(ident)
-
+        secureLogger.info("FullmaktResponse: $fullmaktResponse")
         return fullmaktResponse.map {
             Fullmakt(
                 it.gyldigFraOgMed,
