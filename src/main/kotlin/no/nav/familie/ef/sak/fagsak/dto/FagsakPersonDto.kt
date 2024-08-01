7d52b2d0b8667594256d71f1e8f26460c84648e6
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-08-01 12:53:05 +0200

    ekspanderer fagsakperson/utvidet endepunktet slik at det kan fjernes i påfølgende PR (#2645)

--- src/main/kotlin/no/nav/familie/ef/sak/fagsak/dto/FagsakPersonDto.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/fagsak/dto/FagsakPersonDto.kt
@@ -4,16 +4,6 @@ import java.util.UUID
 
 class FagsakPersonDto(
     val id: UUID,
-    val overgangsstønad: UUID?,
-    val barnetilsyn: UUID?,
-    val skolepenger: UUID?,
-)
-
-/**
- * Inneholder id til fagsakPerson samt de ulike fagsakene til personen for å kunne vise opp på behandlingsoversikten
- */
-class FagsakPersonUtvidetDto(
-    val id: UUID,
     val overgangsstønad: FagsakDto?,
     val barnetilsyn: FagsakDto?,
     val skolepenger: FagsakDto?,
