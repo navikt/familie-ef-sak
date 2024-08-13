cc7479f22ccbca144166517c291647c9936f38f5
ma10s <mattis.janitz@nav.no>
2024-08-13 15:53:21 +0200

    Vil legge til mulighet for å kjøre testkode som leder. Vil teste at vi ikke oppretter BehandlingUtenOppgaveTask dersom den er opprettet tidligere. Vil teste at vi oppretter med riktig payload.

--- src/test/kotlin/no/nav/familie/ef/sak/behandling/oppgavekontroll/BehandlingsoppgaveServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/behandling/oppgavekontroll/BehandlingsoppgaveServiceTest.kt
@@ -2,17 +2,22 @@ package no.nav.familie.ef.sak.behandling.oppgavekontroll
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import no.nav.familie.ef.sak.behandling.BehandlingService
 import no.nav.familie.ef.sak.fagsak.FagsakService
 import no.nav.familie.ef.sak.oppgave.OppgaveService
 import no.nav.familie.ef.sak.repository.behandling
 import no.nav.familie.ef.sak.repository.fagsak
+import no.nav.familie.ef.sak.testutil.kjørSomLeader
 import no.nav.familie.kontrakter.felles.ef.StønadType
 import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveResponseDto
 import no.nav.familie.kontrakter.felles.oppgave.Oppgave
+import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.internal.TaskService
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.temporal.IsoFields
 
 class BehandlingsoppgaveServiceTest {
     private val taskService = mockk<TaskService>(relaxed = true)
@@ -21,6 +26,61 @@ class BehandlingsoppgaveServiceTest {
     private val oppgaveService: OppgaveService = mockk()
 
     val behandlingsoppgaveService = BehandlingsoppgaveService(taskService, behandlingService, fagsakService, oppgaveService)
+
+    @Test
+    internal fun `Skal bruke år og ukenummer når vi ser etter allerede opprettet task`() {
+        val payloadOgUnikNøkkel = payloadMedÅrOgUkenummer()
+        kjørSomLeader {
+            behandlingsoppgaveService.opprettTask()
+        }
+        verify(exactly = 1) { taskService.finnTaskMedPayloadOgType(payloadOgUnikNøkkel, BehandlingUtenOppgaveTask.TYPE)}
+    }
+
+    @Test
+    internal fun `Skal ikke opprette ny task om det finnes en fra før`() {
+        val payloadOgUnikNøkkel = payloadMedÅrOgUkenummer()
+        val task = BehandlingUtenOppgaveTask.opprettTask(payloadOgUnikNøkkel)
+        kjørSomLeader {
+            every { taskService.finnTaskMedPayloadOgType(payloadOgUnikNøkkel, BehandlingUtenOppgaveTask.TYPE) } returns task
+            behandlingsoppgaveService.opprettTask()
+        }
+
+        verify(exactly = 0) { taskService.save(any<Task>()) }
+    }
+
+    @Test
+    internal fun `Skal lage task om det ikke finnes en fra før`() {
+        val payloadOgUnikNøkkel = payloadMedÅrOgUkenummer()
+        val task = BehandlingUtenOppgaveTask.opprettTask(payloadOgUnikNøkkel)
+
+        kjørSomLeader {
+            every { taskService.finnTaskMedPayloadOgType(payloadOgUnikNøkkel, BehandlingUtenOppgaveTask.TYPE) } returns null
+            behandlingsoppgaveService.opprettTask()
+            every { taskService.finnTaskMedPayloadOgType(payloadOgUnikNøkkel, BehandlingUtenOppgaveTask.TYPE) } returns task
+            behandlingsoppgaveService.opprettTask()
+            behandlingsoppgaveService.opprettTask()
+        }
+
+        verify(exactly = 1) { taskService.save(any<Task>()) }
+    }
+
+    @Test
+    internal fun `Skal ikke lage task dersom ikke leder`() {
+
+        kjørSomLeader(erLeder = false) {
+            behandlingsoppgaveService.opprettTask()
+        }
+
+        verify(exactly = 0)  { taskService.finnTaskMedPayloadOgType(any(), any()) }
+        verify(exactly = 0) { taskService.save(any()) }
+    }
+
+    private fun payloadMedÅrOgUkenummer(): String {
+        val ukenummer = LocalDate.now().get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
+        val year = LocalDate.now().year
+        val payloadOgUnikNøkkel = "År:$year Uke:$ukenummer"
+        return payloadOgUnikNøkkel
+    }
 
     @Test
     internal fun `Skal returnere riktig antall uten oppgave når vi finner to`() {
