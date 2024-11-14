f3c38455f3641119360fadccd1988c2b7ce645f2
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-11-14 23:40:06 +0100

    Ident i oppgave finnes under identer og ikke personident

--- src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigService.kt
@@ -4,12 +4,14 @@ import no.nav.familie.ef.sak.behandling.BehandlingService
 import no.nav.familie.ef.sak.fagsak.FagsakService
 import no.nav.familie.ef.sak.oppgave.OppgaveService
 import no.nav.familie.ef.sak.oppgave.OppgaveUtil
+import no.nav.familie.ef.sak.sigrun.SigrunService
 import no.nav.familie.ef.sak.vedtak.VedtakService
 import no.nav.familie.kontrakter.felles.Behandlingstema
 import no.nav.familie.kontrakter.felles.Månedsperiode
 import no.nav.familie.kontrakter.felles.Tema
 import no.nav.familie.kontrakter.felles.ef.StønadType
 import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveRequest
+import no.nav.familie.kontrakter.felles.oppgave.IdentGruppe
 import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import no.nav.familie.leader.LeaderClient
 import org.slf4j.LoggerFactory
@@ -23,6 +25,7 @@ class SelvstendigService(
     val fagsakService: FagsakService,
     val behandlingService: BehandlingService,
     val vedtakService: VedtakService,
+    val sigrunService: SigrunService,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
     private val secureLogger = LoggerFactory.getLogger("secureLogger")
@@ -42,8 +45,20 @@ class SelvstendigService(
                 )
             val oppgaverForSelvstendige = oppgaveService.hentOppgaver(finnOppgaveRequest)
             secureLogger.info("Antall oppgaver for selvstendige med frist 15. desember ${oppgaverForSelvstendige.oppgaver.size}")
-            secureLogger.info("Oppgave for selvstendig: ${oppgaverForSelvstendige.oppgaver.first()}")
-            val personIdent = oppgaverForSelvstendige.oppgaver.mapNotNull { it.personident }.first()
+            secureLogger.info("Oppgave for selvstendig: ${oppgaverForSelvstendige.oppgaver.firstOrNull()}")
+            if (oppgaverForSelvstendige.oppgaver.isEmpty() ||
+                oppgaverForSelvstendige.oppgaver
+                    .first()
+                    .identer
+                    ?.any { it.gruppe == IdentGruppe.FOLKEREGISTERIDENT } == false
+            ) {
+                secureLogger.info("fant ingen oppgaver for selvstendige med frist 15. desember")
+            }
+            val førsteOppgave = oppgaverForSelvstendige.oppgaver.first()
+            val personIdent =
+                førsteOppgave.identer?.first { it.gruppe == IdentGruppe.FOLKEREGISTERIDENT }?.ident
+                    ?: førsteOppgave.personident ?: throw Exception("Fant ikke registrert ident på oppgave ${førsteOppgave.id}")
+
             secureLogger.info("Kontroller person med ident: $personIdent")
             val fagsaker = fagsakService.finnFagsaker(setOf(personIdent))
             val fagsakIds = fagsaker.filter { it.stønadstype == StønadType.OVERGANGSSTØNAD }.map { it.id }
@@ -55,6 +70,10 @@ class SelvstendigService(
             val antallMåneder = perioder?.count { it.periode.overlapper(Månedsperiode(YearMonth.of(YearMonth.now().year, 1), YearMonth.of(YearMonth.now().year, 12))) }
 
             secureLogger.info("Antall måneder $antallMåneder med vedtak i ${YearMonth.now().year} for person $personIdent")
+
+            val fagsakPersonId = fagsaker.filter { it.stønadstype == StønadType.OVERGANGSSTØNAD }.first().fagsakPersonId
+            val inntekt = sigrunService.hentInntektForAlleÅrMedInntekt(fagsakPersonId)
+            secureLogger.info("Inntekt for person $inntekt")
         }
         // har bruker hatt vedtak??
     }
