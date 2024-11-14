4dc595d76a9d4d1296f5556c226bacffad02005b
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-11-14 23:18:23 +0100

    Logg oppgave

--- src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigService.kt
@@ -11,6 +11,7 @@ import no.nav.familie.kontrakter.felles.Tema
 import no.nav.familie.kontrakter.felles.ef.StønadType
 import no.nav.familie.kontrakter.felles.oppgave.FinnOppgaveRequest
 import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
+import no.nav.familie.leader.LeaderClient
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDate
@@ -27,31 +28,34 @@ class SelvstendigService(
     private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
     fun hentOppgaver() {
-        val mappeIdForSelvstendigNæringsdrivende = oppgaveService.finnMapper(OppgaveUtil.ENHET_NR_NAY).single { it.navn == "61 Selvstendig næringsdrivende" }.id
-        secureLogger.info("MappeId for selvstendige: $mappeIdForSelvstendigNæringsdrivende")
-        logger.info("MappeId for selvstendige: $mappeIdForSelvstendigNæringsdrivende")
-        val finnOppgaveRequest =
-            FinnOppgaveRequest(
-                tema = Tema.ENF,
-                behandlingstema = Behandlingstema.Overgangsstønad,
-                oppgavetype = Oppgavetype.Fremlegg,
-                fristTomDato = LocalDate.of(YearMonth.now().year, 12, 15),
-                mappeId = mappeIdForSelvstendigNæringsdrivende.toLong(),
-            )
-        val oppgaverForSelvstendige = oppgaveService.hentOppgaver(finnOppgaveRequest)
-        secureLogger.info("Antall oppgaver for selvstendige med frist 15. desember ${oppgaverForSelvstendige.oppgaver.size}")
-        val personIdent = oppgaverForSelvstendige.oppgaver.mapNotNull { it.personident }.first()
-        secureLogger.info("Kontroller person med ident: $personIdent")
-        val fagsaker = fagsakService.finnFagsaker(setOf(personIdent))
-        val fagsakIds = fagsaker.filter { it.stønadstype == StønadType.OVERGANGSSTØNAD }.map { it.id }
-        val behandlinger = behandlingService.hentBehandlinger(fagsakIds.toSet())
+        if (LeaderClient.isLeader() == true) {
+            val mappeIdForSelvstendigNæringsdrivende = oppgaveService.finnMapper(OppgaveUtil.ENHET_NR_NAY).single { it.navn == "61 Selvstendig næringsdrivende" }.id
+            secureLogger.info("MappeId for selvstendige: $mappeIdForSelvstendigNæringsdrivende")
+            logger.info("MappeId for selvstendige: $mappeIdForSelvstendigNæringsdrivende")
+            val finnOppgaveRequest =
+                FinnOppgaveRequest(
+                    tema = Tema.ENF,
+                    behandlingstema = Behandlingstema.Overgangsstønad,
+                    oppgavetype = Oppgavetype.Fremlegg,
+                    fristTomDato = LocalDate.of(YearMonth.now().year, 12, 15),
+                    mappeId = mappeIdForSelvstendigNæringsdrivende.toLong(),
+                )
+            val oppgaverForSelvstendige = oppgaveService.hentOppgaver(finnOppgaveRequest)
+            secureLogger.info("Antall oppgaver for selvstendige med frist 15. desember ${oppgaverForSelvstendige.oppgaver.size}")
+            secureLogger.info("Oppgave for selvstendig: ${oppgaverForSelvstendige.oppgaver.first()}")
+            val personIdent = oppgaverForSelvstendige.oppgaver.mapNotNull { it.personident }.first()
+            secureLogger.info("Kontroller person med ident: $personIdent")
+            val fagsaker = fagsakService.finnFagsaker(setOf(personIdent))
+            val fagsakIds = fagsaker.filter { it.stønadstype == StønadType.OVERGANGSSTØNAD }.map { it.id }
+            val behandlinger = behandlingService.hentBehandlinger(fagsakIds.toSet())
 
-        val vedtak = vedtakService.hentVedtak(behandlinger.first().id)
+            val vedtak = vedtakService.hentVedtak(behandlinger.first().id)
 
-        val perioder = vedtak.perioder?.perioder?.filter { !it.periodeType.midlertidigOpphørEllerSanksjon() }
-        val antallMåneder = perioder?.count { it.periode.overlapper(Månedsperiode(YearMonth.of(YearMonth.now().year, 1), YearMonth.of(YearMonth.now().year, 12))) }
+            val perioder = vedtak.perioder?.perioder?.filter { !it.periodeType.midlertidigOpphørEllerSanksjon() }
+            val antallMåneder = perioder?.count { it.periode.overlapper(Månedsperiode(YearMonth.of(YearMonth.now().year, 1), YearMonth.of(YearMonth.now().year, 12))) }
 
-        secureLogger.info("Antall måneder $antallMåneder med vedtak i ${YearMonth.now().year} for person $personIdent")
+            secureLogger.info("Antall måneder $antallMåneder med vedtak i ${YearMonth.now().year} for person $personIdent")
+        }
         // har bruker hatt vedtak??
     }
 }
