9a36aa4e645032d0beb15c27bba82e274d81ac80
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-11-20 13:42:42 +0100

    Linterendringer

--- src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigScheduler.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigScheduler.kt
@@ -3,18 +3,15 @@ package no.nav.familie.ef.sak.selvstendig
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Profile
-import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Profile("!integrasjonstest")
 @Component
 class SelvstendigScheduler(
     val næringsinntektKontrollService: NæringsinntektKontrollService,
-): ApplicationListener<ApplicationReadyEvent> {
-
+) : ApplicationListener<ApplicationReadyEvent> {
     // @Scheduled(cron = "0 0 0 * * ?") - blir en scheduler
     override fun onApplicationEvent(event: ApplicationReadyEvent) {
-        println("TEST TEST TEST")
         næringsinntektKontrollService.sjekkNæringsinntektMotForventetInntekt()
     }
 }
