2044ef8ddddb0f306d66768951b6714c04b477a7
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-11-20 14:46:45 +0100

    Linterendringer

--- src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigScheduler.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigScheduler.kt
@@ -1,6 +1,5 @@
 package no.nav.familie.ef.sak.selvstendig
 
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -9,8 +8,7 @@ import org.springframework.stereotype.Service
 @Service
 class SelvstendigScheduler(
     val næringsinntektKontrollService: NæringsinntektKontrollService,
-){
-
+) {
     @Scheduled(initialDelay = 60 * 1000L, fixedDelay = 365 * 24 * 60 * 60 * 1000L) // Kjører ved oppstart av app
     fun sjekkNæringsinntekt() {
         næringsinntektKontrollService.sjekkNæringsinntektMotForventetInntekt()
