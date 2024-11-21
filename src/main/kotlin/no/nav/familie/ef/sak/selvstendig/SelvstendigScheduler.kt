86a54d13d99e5fee7742dca5c365ef2dd047fccc
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2024-11-21 11:05:36 +0100

    Inntektskontroll selvstendige (#2719)
    Inntektskontroll av selvstendige med næringsinntekt over 50 000

---
+++ src/main/kotlin/no/nav/familie/ef/sak/selvstendig/SelvstendigScheduler.kt
@@ -0,0 +1,16 @@
+package no.nav.familie.ef.sak.selvstendig
+
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Profile("!integrasjonstest")
+@Service
+class SelvstendigScheduler(
+    val næringsinntektKontrollService: NæringsinntektKontrollService,
+) {
+    @Scheduled(initialDelay = 60 * 1000L, fixedDelay = 365 * 24 * 60 * 60 * 1000L) // Kjører ved oppstart av app
+    fun sjekkNæringsinntekt() {
+        næringsinntektKontrollService.sjekkNæringsinntektMotForventetInntekt()
+    }
+}
