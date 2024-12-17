b8a58f27b7ffa85adfdcdfc116e735111bc3c8d6
throndi <56258085+throndi@users.noreply.github.com>
2024-12-17 14:32:45 +0100

    Skal sende med kontantstøtte perioder til frontend

--- src/main/kotlin/no/nav/familie/ef/sak/kontantstøtte/KontantstøtteService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/kontantstøtte/KontantstøtteService.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ef.sak.kontantstøtte
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.identer
 import org.springframework.stereotype.Service
+import java.time.YearMonth
 
 @Service
 class KontantstøtteService(
@@ -18,8 +19,15 @@ class KontantstøtteService(
 fun HentUtbetalingsinfoKontantstøtte.tilDto(): HentUtbetalingsinfoKontantstøtteDto =
     HentUtbetalingsinfoKontantstøtteDto(
         this.ksSakPerioder.isNotEmpty() || this.infotrygdPerioder.isNotEmpty(),
+        this.ksSakPerioder.map { KsakPeriodeDto(it.fomMåned, it.tomMåned) },
     )
+
+data class KsakPeriodeDto(
+    val fomMåned: YearMonth,
+    val tomMåned: YearMonth? = null,
+)
 
 data class HentUtbetalingsinfoKontantstøtteDto(
     val finnesUtbetaling: Boolean,
+    val perioder: List<KsakPeriodeDto>,
 )
