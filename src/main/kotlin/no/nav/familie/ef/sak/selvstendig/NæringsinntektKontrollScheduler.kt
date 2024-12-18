0c5d8566b96a794627f6c8f1840dfbbcabf2f6dc
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-12-18 15:36:04 +0100

    Legger til featuretoggle slik at inntektskontroll kan kjøres i dev

---
+++ src/main/kotlin/no/nav/familie/ef/sak/selvstendig/NæringsinntektKontrollScheduler.kt
@@ -0,0 +1,21 @@
+package no.nav.familie.ef.sak.selvstendig
+
+import no.nav.familie.ef.sak.infrastruktur.featuretoggle.FeatureToggleService
+import no.nav.familie.ef.sak.infrastruktur.featuretoggle.Toggle
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Profile("!integrasjonstest")
+@Service
+class NæringsinntektKontrollScheduler(
+    val næringsinntektKontrollService: NæringsinntektKontrollService,
+    val featureToggleService: FeatureToggleService,
+) {
+    @Scheduled(initialDelay = 60 * 1000L, fixedDelay = 365 * 24 * 60 * 60 * 1000L) // Kjører ved oppstart av app
+    fun sjekkNæringsinntekt() {
+        if (featureToggleService.isEnabled(Toggle.KONTROLLER_NÆRINGSINNTEKT)) {
+            næringsinntektKontrollService.kontrollerInntektForSelvstendigNæringsdrivende()
+        }
+    }
+}
