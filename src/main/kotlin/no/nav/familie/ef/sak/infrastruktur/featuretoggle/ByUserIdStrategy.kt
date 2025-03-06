001d2252c33ceb83374d50e4c061b46b70b02cc7
ma10s <mattis.janitz@nav.no>
2025-03-06 13:54:29 +0100

    Strategy fra unleash krever UnleashContext.

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/ByUserIdStrategy.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/ByUserIdStrategy.kt
@@ -1,12 +1,24 @@
 package no.nav.familie.ef.sak.infrastruktur.featuretoggle
 
+import io.getunleash.UnleashContext
 import io.getunleash.strategy.Strategy
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
+import org.slf4j.LoggerFactory
 
 class ByUserIdStrategy : Strategy {
     override fun getName(): String = "userWithId"
 
-    override fun isEnabled(map: MutableMap<String, String>): Boolean {
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
+    override fun isEnabled(
+        map: MutableMap<String, String>,
+        unleashContext: UnleashContext,
+    ) : Boolean {
+        secureLogger.warn("ByUserIdStrategy called with unused context: $unleashContext")
+        return isEnabled(map)
+    }
+
+    fun isEnabled(map: MutableMap<String, String>): Boolean {
         if (SikkerhetContext.erSystembruker()) {
             return false
         }
