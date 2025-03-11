ea8980cf2d56699af006695dc90a93f1cbe19b06
krikof-nav <kristian.kofoed@nav.no>
2025-03-11 13:33:36 +0100

    Forsøk på å oppdatere unleash med unleash context.

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/ByEnvironmentStrategy.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/ByEnvironmentStrategy.kt
@@ -4,19 +4,17 @@ import io.getunleash.UnleashContext
 import io.getunleash.strategy.Strategy
 
 class ByEnvironmentStrategy : Strategy {
-    companion object {
-        private const val MILJØ_KEY = "miljø"
-    }
-
     override fun getName(): String = "byEnvironment"
 
-    override fun isEnabled(map: Map<String, String>): Boolean = isEnabled(map, UnleashContext.builder().build())
-
     override fun isEnabled(
-        map: Map<String, String>,
+        map: MutableMap<String, String>,
         unleashContext: UnleashContext,
     ): Boolean =
         unleashContext.environment
             .map { env -> map[MILJØ_KEY]?.split(',')?.contains(env) ?: false }
             .orElse(false)
+
+    companion object {
+        private const val MILJØ_KEY = "miljø"
+    }
 }
--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/ByUserIdStrategy.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/ByUserIdStrategy.kt
@@ -1,12 +1,16 @@
 package no.nav.familie.ef.sak.infrastruktur.featuretoggle
 
+import io.getunleash.UnleashContext
 import io.getunleash.strategy.Strategy
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
 
 class ByUserIdStrategy : Strategy {
     override fun getName(): String = "userWithId"
 
-    override fun isEnabled(map: MutableMap<String, String>): Boolean {
+    override fun isEnabled(
+        map: MutableMap<String, String>,
+        unleashContext: UnleashContext,
+    ): Boolean {
         if (SikkerhetContext.erSystembruker()) {
             return false
         }
