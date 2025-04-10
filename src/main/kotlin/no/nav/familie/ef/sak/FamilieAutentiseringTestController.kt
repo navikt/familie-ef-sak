241e60695fdc7c1ed9c1fd78f0d0048b44a5e28d
krikof-nav <kristian.kofoed@nav.no>
2025-04-10 12:27:30 +0200

    Endrer test controller for autentisering til å motta gitt rolle, samt returnere ok om ting er ok.

--- src/main/kotlin/no/nav/familie/ef/sak/FamilieAutentiseringTestController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/FamilieAutentiseringTestController.kt
@@ -1,27 +1,28 @@
 package no.nav.familie.ef.sak
 
+import no.nav.familie.ef.sak.behandlingsflyt.steg.BehandlerRolle
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/autentisering/")
+@RequestMapping("/api/autentisering")
 @ProtectedWithClaims(issuer = "azuread")
 class FamilieAutentiseringTestController(
     private val tilgangService: TilgangService,
 ) {
     @PostMapping
-    @RequestMapping("test-token-validering)")
+    @RequestMapping("test-token-validering")
     @ProtectedWithClaims(issuer = "azuread")
-    fun testTokenValidering() {
-        val harTilgang = tilgangService.validerHarForvalterrolle()
-        logger.info("Har tilgang: $harTilgang")
-    }
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(this::class.java)
+    @Profile("!prod")
+    fun testRolleValidering(
+        @RequestBody behandlerRolle: BehandlerRolle,
+    ): String {
+        tilgangService.validerTilgangTilRolle(behandlerRolle)
+        return "ok"
     }
 }
