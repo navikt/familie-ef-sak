bbb595308c95bcd16a5a153894b65c05376a2eef
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-04-28 15:45:33 +0200

    Skal kun sende med listen over automatiske brev

--- src/main/kotlin/no/nav/familie/ef/sak/oppfølgingsoppgave/OppfølgingsoppgaveController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/oppfølgingsoppgave/OppfølgingsoppgaveController.kt
@@ -1,7 +1,6 @@
 package no.nav.familie.ef.sak.oppfølgingsoppgave
 
 import no.nav.familie.ef.sak.behandling.oppgaveforopprettelse.OppgaverForOpprettelseDto
-import no.nav.familie.ef.sak.oppfølgingsoppgave.automatiskBrev.AutomatiskBrevDto
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,7 +13,7 @@ data class OppfølgingsoppgaveDto(
     val behandlingid: UUID,
     val oppgaverForOpprettelse: OppgaverForOpprettelseDto?,
     val oppgaveIderForFerdigstilling: List<Long>?,
-    val automatiskBrev: AutomatiskBrevDto?,
+    val automatiskBrev: List<String>,
 )
 
 @RestController
@@ -37,7 +36,7 @@ class OppfølgingsoppgaveController(
                 behandlingid = behandlingid,
                 oppgaverForOpprettelse = oppgaverForOpprettelse,
                 oppgaveIderForFerdigstilling = oppgaverForFerdigstilling.oppgaveIder,
-                automatiskBrev = automatiskBrev,
+                automatiskBrev = automatiskBrev.brevSomSkalSendes,
             ),
         )
     }
