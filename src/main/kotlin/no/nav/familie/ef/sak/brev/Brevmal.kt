b3905b601764cf4617db0d9dbe31d24a1f5d8e11
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-05-08 13:35:51 +0200

    Fjerner ubrukt kode.

--- src/main/kotlin/no/nav/familie/ef/sak/brev/Brevmal.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/brev/Brevmal.kt
@@ -5,24 +5,4 @@ enum class Brevmal(
     val tittel: String,
 ) {
     VARSEL_OM_AKTIVITETSPLIKT("varselAktivitetsplikt", "Varsel om aktivitetsplikt"),
-    UKJENT("", ""),
 }
-
-// enum class Brevmal {
-//    VARSEL_OM_AKTIVITETSPLIKT,
-//    UKJENT,
-// }
-//
-// fun Brevmal.tilApinavn(): String {
-//    return when (this) {
-//        Brevmal.VARSEL_OM_AKTIVITETSPLIKT -> "varselAktivitetsplikt"
-//        Brevmal.UKJENT -> ""
-//    }
-// }
-//
-// fun Brevmal.tilTittel(): String {
-//    return when (this) {
-//        Brevmal.VARSEL_OM_AKTIVITETSPLIKT -> "Varsel om aktivitetsplikt"
-//        Brevmal.UKJENT -> ""
-//    }
-// }
