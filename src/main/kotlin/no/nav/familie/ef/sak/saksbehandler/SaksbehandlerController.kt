55a3d236af11d5152c5df4cb7b787df361a210e0
krikof-nav <kristian.kofoed@nav.no>
2025-05-20 11:31:51 +0200

    Endrer fra til GetMapping til PostMapping i hentSaksbehandlerInformasjon.

--- src/main/kotlin/no/nav/familie/ef/sak/saksbehandler/SaksbehandlerController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/saksbehandler/SaksbehandlerController.kt
@@ -5,7 +5,7 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,9 +18,9 @@ class SaksbehandlerController(
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    @GetMapping("/behandler-informasjon")
+    @PostMapping("/behandler-informasjon")
     fun hentSaksbehandlerInformasjon(
-        @RequestBody requestBody: HentSaksbehandlerInformasjonRequestBody
+        @RequestBody requestBody: HentSaksbehandlerRequest
     ): Ressurs<Saksbehandler> {
         logger.info("Henter saksbehandler informasjon for: $requestBody.navIdent")
         val saksbehandler = oppgaveClient.hentSaksbehandlerInfo(requestBody.navIdent)
@@ -28,6 +28,6 @@ class SaksbehandlerController(
     }
 }
 
-data class HentSaksbehandlerInformasjonRequestBody(
+data class HentSaksbehandlerRequest(
     val navIdent: String,
 )
