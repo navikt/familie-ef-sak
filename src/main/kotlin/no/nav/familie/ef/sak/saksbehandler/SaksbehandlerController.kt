bcdd5a1b1b6e198f62b74df060d31293f30db571
krikof-nav <kristian.kofoed@nav.no>
2025-05-20 12:45:46 +0200

    Endrer hentSaksbehandlerInformasjon metode til å være en GetMapping og tar utgangspunkt i token satt i header.

--- src/main/kotlin/no/nav/familie/ef/sak/saksbehandler/SaksbehandlerController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/saksbehandler/SaksbehandlerController.kt
@@ -1,12 +1,12 @@
 package no.nav.familie.ef.sak.saksbehandler
 
+import no.nav.familie.ef.sak.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.familie.ef.sak.oppgave.OppgaveClient
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.saksbehandler.Saksbehandler
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,16 +18,12 @@ class SaksbehandlerController(
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    @PostMapping("/behandler-informasjon")
-    fun hentSaksbehandlerInformasjon(
-        @RequestBody requestBody: HentSaksbehandlerRequest
-    ): Ressurs<Saksbehandler> {
-        logger.info("Henter saksbehandler informasjon for: $requestBody.navIdent")
-        val saksbehandler = oppgaveClient.hentSaksbehandlerInfo(requestBody.navIdent)
+    @GetMapping("/behandler-informasjon")
+    fun hentSaksbehandlerInformasjon(): Ressurs<Saksbehandler> {
+        val navIdent = SikkerhetContext.hentSaksbehandler()
+        logger.info("Henter saksbehandler informasjon for: $navIdent")
+
+        val saksbehandler = oppgaveClient.hentSaksbehandlerInfo(navIdent)
         return Ressurs.success(saksbehandler)
     }
 }
-
-data class HentSaksbehandlerRequest(
-    val navIdent: String,
-)
