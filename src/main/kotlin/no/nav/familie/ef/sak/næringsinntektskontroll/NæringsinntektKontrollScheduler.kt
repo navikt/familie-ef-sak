4f2cf6abeee12512822764d129fc9701d5af7c06
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2025-06-10 10:26:41 +0200

    Fikser feil ved lokal kjøring. Skrur av næringsinntekt-kontrollen, da det krever mye mocking for at kjøringen skal gi mening.

--- src/main/kotlin/no/nav/familie/ef/sak/næringsinntektskontroll/NæringsinntektKontrollScheduler.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/næringsinntektskontroll/NæringsinntektKontrollScheduler.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
-@Profile("!integrasjonstest")
+@Profile("!integrasjonstest && !local")
 @Service
 class NæringsinntektKontrollScheduler(
     val næringsinntektKontrollService: NæringsinntektKontrollService,
