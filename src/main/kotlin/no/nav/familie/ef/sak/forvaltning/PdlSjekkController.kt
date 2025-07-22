975269adf6e052e2a118ab1c68269cf11a20faba
ma10s <mattis.janitz@nav.no>
2025-07-22 13:26:02 +0200

    Forvaltningsendepunkt - behov for informasjon om data (annen forelder)med NPID da disse kan gi feil ved journalføring (opprettelse av grunnlagsdata)

--- src/main/kotlin/no/nav/familie/ef/sak/forvaltning/PdlSjekkController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/forvaltning/PdlSjekkController.kt
@@ -3,7 +3,13 @@ package no.nav.familie.ef.sak.forvaltning
 import no.nav.familie.ef.sak.fagsak.FagsakService
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonopplysningerService
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.Familierelasjonsrolle
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.PdlAnnenForelder
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.PdlPersonForelderBarn
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.PdlSøker
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.identer
+import no.nav.familie.kontrakter.felles.personopplysning.ADRESSEBESKYTTELSEGRADERING
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
@@ -19,6 +25,7 @@ class PdlSjekkController(
     private val personService: PersonService,
     private val fagsakService: FagsakService,
     private val tilgangService: TilgangService,
+    private val personopplysningerService: PersonopplysningerService,
 ) {
     private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
@@ -43,4 +50,52 @@ class PdlSjekkController(
         secureLogger.info("$count personer funnet")
         return count
     }
+
+    @PostMapping("/annenforelder")
+    fun sjekkAnnenForelder(
+        @RequestBody ident: String,
+    ): Int {
+        tilgangService.validerHarForvalterrolle()
+        val søker = personService.hentSøker(ident)
+        val strengesteAdressebeskyttelseForPersonMedRelasjoner = personopplysningerService.hentStrengesteAdressebeskyttelseForPersonMedRelasjoner(ident)
+
+        if (strengesteAdressebeskyttelseForPersonMedRelasjoner != ADRESSEBESKYTTELSEGRADERING.UGRADERT) {
+            return 0
+        }
+
+        val barn = hentPdlBarn(søker)
+        val andreForeldre =
+            hentPdlBarneForeldre(
+                barn = barn,
+                personIdent = ident,
+            )
+
+        secureLogger.info("Søker har ${barn.size} barn og ${andreForeldre.size} andre foreldre")
+
+        andreForeldre.forEach { (id, forelder) ->
+            secureLogger.info("Annen forelder: $id - ${forelder.navn} gleldende? ")
+            forelder.folkeregisteridentifikator.forEach { pid ->
+                secureLogger.info("Annen forelder-ident status: ${pid.status}, historisk = ${pid.metadata.historisk}")
+            }
+        }
+
+        return andreForeldre.size
+    }
+
+    private fun hentPdlBarneForeldre(
+        barn: Map<String, PdlPersonForelderBarn>,
+        personIdent: String,
+    ): Map<String, PdlAnnenForelder> =
+        barn
+            .flatMap { it.value.forelderBarnRelasjon }
+            .filter { it.relatertPersonsIdent != personIdent && it.relatertPersonsRolle != Familierelasjonsrolle.BARN }
+            .mapNotNull { it.relatertPersonsIdent }
+            .distinct()
+            .let { personService.hentAndreForeldre(it) }
+
+    private fun hentPdlBarn(pdlSøker: PdlSøker): Map<String, PdlPersonForelderBarn> =
+        pdlSøker.forelderBarnRelasjon
+            .filter { it.relatertPersonsRolle == Familierelasjonsrolle.BARN }
+            .mapNotNull { it.relatertPersonsIdent }
+            .let { personService.hentPersonForelderBarnRelasjon(it) }
 }
