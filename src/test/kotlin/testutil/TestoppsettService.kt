32f540636846afc72988dcdd07668c9bb3beca79
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2025-07-23 10:34:43 +0200

    Dersom noe er revurdert manuelt siste 3 måneder skal det ikke med i inntektskontrollen, både for opprettelse av oppgaver ved inntektsendring og automatisk revurdering. Denne endringen filtrerer ut behandlinger som har blitt revurdert rett før g-omregning, altså at vedtakstidspunkt er mindre enn 3 mnd på behandling før g-omregning.

--- src/test/kotlin/testutil/TestoppsettService.kt
+++ src/test/kotlin/testutil/TestoppsettService.kt
@@ -1,5 +1,9 @@
 package no.nav.familie.ef.sak.testutil
 
+import no.nav.familie.ef.sak.behandling.BehandlingRepository
+import no.nav.familie.ef.sak.behandling.domain.Behandling
+import no.nav.familie.ef.sak.behandling.domain.BehandlingResultat.INNVILGET
+import no.nav.familie.ef.sak.behandling.domain.BehandlingStatus.FERDIGSTILT
 import no.nav.familie.ef.sak.fagsak.FagsakPersonRepository
 import no.nav.familie.ef.sak.fagsak.FagsakRepository
 import no.nav.familie.ef.sak.fagsak.domain.Fagsak
@@ -7,15 +11,20 @@ import no.nav.familie.ef.sak.fagsak.domain.FagsakDomain
 import no.nav.familie.ef.sak.fagsak.domain.FagsakPerson
 import no.nav.familie.ef.sak.fagsak.domain.PersonIdent
 import no.nav.familie.ef.sak.fagsak.domain.tilFagsakMedPerson
+import no.nav.familie.ef.sak.repository.behandling
+import no.nav.familie.ef.sak.repository.fagsak
+import no.nav.familie.ef.sak.repository.fagsakPerson
 import org.springframework.context.annotation.Profile
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Profile("integrasjonstest || local")
 @Service
 class TestoppsettService(
     private val fagsakPersonRepository: FagsakPersonRepository,
     private val fagsakRepository: FagsakRepository,
+    private val behandlingRepository: BehandlingRepository,
 ) {
     fun opprettPerson(ident: String) = fagsakPersonRepository.insert(FagsakPerson(identer = setOf(PersonIdent(ident))))
 
@@ -34,6 +43,22 @@ class TestoppsettService(
                     sporbar = fagsak.sporbar,
                 ),
             ).tilFagsakMedPerson(person.identer)
+    }
+
+    fun lagreFagsakOgBehandlingForPersonIdent(
+        personIdent: String,
+        vedtakstidspunkt: LocalDateTime? = null,
+    ): Behandling {
+        val personMedGOmregningBehandling = fagsakPerson(identer = setOf(PersonIdent(personIdent)))
+        val fagsak = lagreFagsak(fagsak(person = personMedGOmregningBehandling))
+        val behandling =
+            behandling(
+                fagsak,
+                resultat = INNVILGET,
+                vedtakstidspunkt = vedtakstidspunkt,
+                status = FERDIGSTILT,
+            )
+        return behandlingRepository.insert(behandling)
     }
 
     private fun hentEllerOpprettPerson(fagsak: Fagsak): FagsakPerson =
