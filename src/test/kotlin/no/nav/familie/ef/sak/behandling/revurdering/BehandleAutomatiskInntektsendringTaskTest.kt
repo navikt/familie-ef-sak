6f87a0feb3c56d6c34a31115c665102e801ed687
gunnsteingarmo <141132903+gunnsteingarmo@users.noreply.github.com>
2025-08-01 12:26:29 +0200

    Fiks - sjekk om fagsak har behandlinger (#2965)
    * Sjekker om det finnes noen behandlinger på fagsak
    
    * Oppdatert forventet svar på tester.

--- src/test/kotlin/no/nav/familie/ef/sak/behandling/revurdering/BehandleAutomatiskInntektsendringTaskTest.kt
+++ src/test/kotlin/no/nav/familie/ef/sak/behandling/revurdering/BehandleAutomatiskInntektsendringTaskTest.kt
@@ -323,27 +323,27 @@ class BehandleAutomatiskInntektsendringTaskTest : OppslagSpringRunnerTest() {
 
     val forventetInntektsbegrunnelse =
         """
-        Periode som er kontrollert: ${YearMonth.now().minusMonths(6).tilNorskFormat()} til ${YearMonth.now().minusMonths(1).tilNorskFormat()}.
+        Periode som er kontrollert: februar 2025 til juli 2025.
         
-        Forventet årsinntekt i mars 2025: 144 000 kroner.
+        Forventet årsinntekt i april 2025: 144 000 kroner.
         - 10 % opp: 13 200 kroner per måned.
         - 10 % ned: 10 800 kroner per måned.
         
-        Inntekten i mars 2025 er 16 000 kroner. Inntekten har økt minst 10 prosent denne måneden og alle månedene etter dette. Stønaden beregnes på nytt fra måneden etter 10 prosent økning.
+        Inntekten i april 2025 er 16 000 kroner. Inntekten har økt minst 10 prosent denne måneden og alle månedene etter dette. Stønaden beregnes på nytt fra måneden etter 10 prosent økning.
         
-        Har lagt til grunn faktisk inntekt bakover i tid. Fra og med juli 2025 er stønaden beregnet ut ifra gjennomsnittlig inntekt for april, mai og juni.
+        Har lagt til grunn faktisk inntekt bakover i tid. Fra og med august 2025 er stønaden beregnet ut ifra gjennomsnittlig inntekt for mai, juni og juli.
         
         A-inntekt er lagret.
         """.trimIndent()
 
     val forventetInntektsbegrunnelseForrigeVedtak0ForventetInntekt =
         """
-        Forventet årsinntekt i desember 2024: 0 kroner.
+        Forventet årsinntekt i januar 2025: 0 kroner.
             - Månedsinntekt 1/2 G: 5 423 kroner
         
         Mottar uredusert stønad.
         
-        Inntekten i desember 2024 er 12 000 kroner. Bruker har inntekt over 1/2 G denne måneden og alle månedene etter dette.
+        Inntekten i januar 2025 er 12 000 kroner. Bruker har inntekt over 1/2 G denne måneden og alle månedene etter dette.
         Stønaden beregnes på nytt fra måneden etter inntekten oversteg 1/2 G.
         """.trimIndent()
 }
