40b024a94ebbdf15416cd55fd076580649ae37ac
ma10s <mattis.janitz@nav.no>
2025-08-12 10:26:52 +0200

    Proxy for enkelt kunne kjøre forvaltningsoppgaver i iverksett.

---
+++ src/main/kotlin/no/nav/familie/ef/sak/forvaltning/IverksettProxyTaskForvaltningController.kt
@@ -0,0 +1,41 @@
+package no.nav.familie.ef.sak.forvaltning
+
+import io.swagger.v3.oas.annotations.Operation
+import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
+import no.nav.familie.http.client.AbstractRestClient
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestOperations
+import java.net.URI
+
+@RestController
+@RequestMapping("/api/forvaltning/iverksett/")
+@ProtectedWithClaims(issuer = "azuread")
+class IverksettProxyTaskForvaltningController(
+    private val tilgangService: TilgangService,
+    @Value("\${FAMILIE_EF_IVERKSETT_URL}")
+    private val familieEfIverksettUri: String,
+    @Qualifier("azure")
+    private val restOperations: RestOperations,
+) : AbstractRestClient(restOperations, "familie.ef.iverksett.forvaltning") {
+    @PostMapping("taskid/{taskId}")
+    @Operation(
+        description =
+            "Kopierer task i iverksett med id angitt taskId. Tasken i iverksett må ha status MANUELL_OPPFØLGING.",
+        summary =
+            "NB! Dette gjelder task i Iverksett, ikke i ef-sak. " +
+                "Brukes for å opprette en task-kopi uten plukket-antall",
+    )
+    fun restartIverksettTask(
+        @PathVariable taskId: Long,
+    ) {
+        tilgangService.validerHarForvalterrolle()
+        val url = URI.create("$familieEfIverksettUri/api/forvaltning/task/restart/$taskId")
+        postForEntity<Any>(url, "")
+    }
+}
