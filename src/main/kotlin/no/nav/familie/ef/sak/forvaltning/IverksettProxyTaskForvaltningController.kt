19b96f242bf6094ce95b6200e0a49de63d09f72f
ma10s <mattis.janitz@nav.no>
2025-08-13 10:07:33 +0200

    Vil hente Ressurs

--- src/main/kotlin/no/nav/familie/ef/sak/forvaltning/IverksettProxyTaskForvaltningController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/forvaltning/IverksettProxyTaskForvaltningController.kt
@@ -3,9 +3,11 @@ package no.nav.familie.ef.sak.forvaltning
 import io.swagger.v3.oas.annotations.Operation
 import no.nav.familie.ef.sak.infrastruktur.sikkerhet.TilgangService
 import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -36,6 +38,13 @@ class IverksettProxyTaskForvaltningController(
     ) : String {
         tilgangService.validerHarForvalterrolle()
         val url = URI.create("$familieEfIverksettUri/api/forvaltning/task/restart/$taskId")
-        return postForEntity<String>(url, "")
+        return postForEntity<Ressurs<String>>(uri = url, payload = "", httpHeaders = textHeaders()).data!!
+    }
+
+    private fun textHeaders(): HttpHeaders {
+        val httpHeaders = HttpHeaders()
+        httpHeaders.add("Content-Type", "text/plain;charset=UTF-8")
+        httpHeaders.acceptCharset = listOf(Charsets.UTF_8)
+        return httpHeaders
     }
 }
