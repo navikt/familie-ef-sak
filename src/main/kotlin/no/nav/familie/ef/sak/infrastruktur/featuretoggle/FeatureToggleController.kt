ea6ed452da383a4605d80e96633b61e31e4d29ec
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-08-28 09:48:27 +0200

    Retter skrivefeil

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleController.kt
@@ -27,7 +27,7 @@ class FeatureToggleController(
             Toggle.VIS_AUTOMATISK_INNTEKTSENDRING,
             Toggle.BEHANDLE_AUTOMATISK_INNTEKTSENDRING,
             Toggle.OPPDATER_BEHANDLINGSTATUS,
-            Toggle.FRONTEND_VIS_BEREGNINGSSKEJMA,
+            Toggle.FRONTEND_VIS_BEREGNINGSSKJEMA,
         )
 
     @GetMapping
