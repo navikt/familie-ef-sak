78313af91d6c217aae95d8d6a77158f197bcb111
endrehm <endre.heien.mo@nav.no>
2025-09-09 13:29:07 +0200

    Lagt til nytt endepunkt for forvaltning og egen task for forvaltning

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/revurdering/AutomatiskRevurderingController.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/revurdering/AutomatiskRevurderingController.kt
@@ -6,20 +6,27 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.YearMonth
 
 @RestController
 @RequestMapping(path = ["/api/automatisk-revurdering"])
 @ProtectedWithClaims(issuer = "azuread")
 class AutomatiskRevurderingController(
-    private val automatiskRevurderingService: AutomatiskRevurderingService,
-    private val revurderingService: RevurderingService,
     private val taskService: TaskService,
 ) {
     @PostMapping
     fun forsøkAutomatiskRevurdering(
         @RequestBody personIdenter: List<String>,
     ) {
-        val task = OpprettAutomatiskRevurderingFraForvaltningTask.opprettTask(personIdenter)
+        val task = OpprettAutomatiskRevurderingTask.opprettTask(PayloadOpprettAutomatiskRevurderingTask(personIdenter, YearMonth.now()))
+        taskService.save(task)
+    }
+
+    @PostMapping("/forvaltning")
+    fun forsøkAutomatiskRevurderingForvaltning(
+        @RequestBody personIdenter: List<String>,
+    ) {
+        val task = OpprettAutomatiskRevurderingFraForvaltningTask.opprettTask(PayloadOpprettAutomatiskRevurderingFraForvaltningTask(personIdenter, YearMonth.now()))
         taskService.save(task)
     }
 }
