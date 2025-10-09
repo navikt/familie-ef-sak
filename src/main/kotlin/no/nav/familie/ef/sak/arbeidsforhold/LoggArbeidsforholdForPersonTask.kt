8636c3242fa8e388224c813549fde87c08b3bdad
endrehm <endre.heien.mo@nav.no>
2025-10-09 13:22:47 +0200

    Fjernet ubrukt funksjon for å hente arbeidsforhold i service og kaller direkte på klient i task

--- src/main/kotlin/no/nav/familie/ef/sak/arbeidsforhold/LoggArbeidsforholdForPersonTask.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/arbeidsforhold/LoggArbeidsforholdForPersonTask.kt
@@ -1,9 +1,7 @@
 package no.nav.familie.ef.sak.arbeidsforhold
 
-import no.nav.familie.ef.sak.arbeidsforhold.ekstern.ArbeidsforholdService
-import no.nav.familie.ef.sak.fagsak.FagsakService
+import no.nav.familie.ef.sak.arbeidsforhold.ekstern.ArbeidsforholdClient
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.secureLogger
-import no.nav.familie.kontrakter.felles.ef.StønadType
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
@@ -18,18 +16,14 @@ import java.util.Properties
     beskrivelse = "Logger arbeidsforhold for gitt person",
 )
 class LoggArbeidsforholdForPersonTask(
-    private val arbeidsforholdService: ArbeidsforholdService,
-    private val fagsakService: FagsakService,
+    private val arbeidsforholdClient: ArbeidsforholdClient,
 ) : AsyncTaskStep {
     override fun doTask(task: Task) {
         val personIdent = task.payload
-        val fagsak = fagsakService.finnFagsak(setOf(personIdent), StønadType.OVERGANGSSTØNAD)
         secureLogger.info("Logg arbeidsforhold for person $personIdent")
-        if (fagsak != null) {
-            val arbeidsforhold = arbeidsforholdService.hentArbeidsforhold(fagsak.id)
-            arbeidsforhold.forEach { arbeidsforhold ->
-                secureLogger.info("Arbeidsforhold or person $personIdent: $arbeidsforhold")
-            }
+        val arbeidsforhold = arbeidsforholdClient.hentArbeidsforhold(personIdent)
+        arbeidsforhold.forEach { arbeidsforhold ->
+            secureLogger.info("Arbeidsforhold or person $personIdent: $arbeidsforhold")
         }
     }
 
