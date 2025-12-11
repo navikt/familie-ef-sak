07b3e4a517d005a9796288f70992c7a04f56875b
Mattis Janitz <mattis.janitz@nav.no>
2025-12-11 07:55:47 +0100

    Vil legge inn kjøredatoer for konsistensavstemming for 2026. (#3091)

---
+++ src/main/resources/db/migration/V158__konsistensavstemming_tidsplan_2026.sql
@@ -0,0 +1,26 @@
+-- INSERT INTO konsistensavstemming_jobb (triggerdato)
+-- VALUES (TO_DATE('25.11.2024', 'DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('07.01.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('03.02.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('31.03.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('27.04.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('02.06.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('03.06.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('30.06.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('03.08.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('01.09.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('01.10.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('03.11.2026','DD.MM.YYYY'));
+INSERT INTO konsistensavstemming_jobb (triggerdato)
+VALUES (TO_DATE('24.11.2026','DD.MM.YYYY'));
