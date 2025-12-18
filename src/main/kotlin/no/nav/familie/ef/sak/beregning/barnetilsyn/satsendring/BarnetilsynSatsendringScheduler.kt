7f48b2b33a0131e01cca556c1bf122f13596bae5
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2025-12-18 13:28:53 +0100

    Aktiver scheduler som finner fagsaker som skal satsendres. Det forventes 8 stk (#3096)

--- src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringScheduler.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/beregning/barnetilsyn/satsendring/BarnetilsynSatsendringScheduler.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.sak.beregning.barnetilsyn.satsendring
 
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Profile("!integrasjonstest")
@@ -11,7 +12,7 @@ class BarnetilsynSatsendringScheduler(
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    // @Scheduled(initialDelay = 60 * 1000L, fixedDelay = 365 * 24 * 60 * 60 * 1000L) // Kjører ved oppstart av app
+    @Scheduled(initialDelay = 60 * 1000L, fixedDelay = 365 * 24 * 60 * 60 * 1000L) // Kjører ved oppstart av app
     fun opprettTask() {
         logger.info("Starter scheduler for satsendring av barnetilsyn")
         barnetilsynSatsendringService.opprettTask()
