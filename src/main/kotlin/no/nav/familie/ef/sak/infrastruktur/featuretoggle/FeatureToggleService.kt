51f677c4f19a90c5c0a46ae73bfb5a6f874ab98a
olekvernberg <ole.christian.kvernberg@nav.no>
2026-01-05 13:50:02 +0100

    Fjerner all kode relatert til næringsinntektkontroll Fjernet featuretoggle

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/featuretoggle/FeatureToggleService.kt
@@ -14,7 +14,6 @@ enum class Toggle(
     val beskrivelse: String? = null,
 ) {
     // Release
-    KONTROLLER_NÆRINGSINNTEKT("familie.ef.sak.kontroller-naeringsinntekt"),
     FRONTEND_KOPIER_KNAPP_ERROR_ALERT("familie.ef.sak.frontend-alert-error-med-copy-button", "Release"),
     FRONTEND_VIS_BEREGNINGSSKJEMA("familie.ef.sak.frontend-vis-beregningsskjema", "Release"),
     VIS_ANDRE_YTELSER("familie.ef.sak.vis-andre-ytelser", "Release"),
