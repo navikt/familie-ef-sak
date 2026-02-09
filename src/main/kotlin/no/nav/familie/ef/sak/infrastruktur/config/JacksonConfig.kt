04d0a2c1124ac7351ed3d8bb577d7d584a34b7ab
olekvernberg <ole.christian.kvernberg@nav.no>
2026-02-09 15:06:16 +0100

    Fix swagger

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/config/JacksonConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/config/JacksonConfig.kt
@@ -2,16 +2,21 @@ package no.nav.familie.ef.sak.infrastruktur.config
 
 import no.nav.familie.ef.sak.infrastruktur.config.ObjectMapperProvider.objectMapper
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.ByteArrayHttpMessageConverter
+import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class JacksonConfig : WebMvcConfigurer {
-    override fun extendMessageConverters(
-        converters: MutableList<org.springframework.http.converter.HttpMessageConverter<*>>,
-    ) {
-        val jackson2Converter = MappingJackson2HttpMessageConverter(objectMapper)
+    override fun extendMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
         converters.removeIf { it is MappingJackson2HttpMessageConverter }
-        converters.add(0, jackson2Converter)
+
+        // Sørg for at ByteArrayHttpMessageConverter er først (kreves for springdoc)
+        converters.removeIf { it is ByteArrayHttpMessageConverter }
+        converters.add(0, ByteArrayHttpMessageConverter())
+
+        // Legg til custom Jackson-converter etter ByteArrayHttpMessageConverter
+        converters.add(1, MappingJackson2HttpMessageConverter(objectMapper))
     }
 }
