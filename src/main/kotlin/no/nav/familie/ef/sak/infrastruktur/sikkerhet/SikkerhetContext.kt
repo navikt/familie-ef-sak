f56aed470b0f954a3f493b0b26deff4766a79754
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-03-10 16:11:17 +0100

    Forøsker å hente token fra header.

--- src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/sikkerhet/SikkerhetContext.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/infrastruktur/sikkerhet/SikkerhetContext.kt
@@ -1,18 +1,24 @@
 package no.nav.familie.ef.sak.infrastruktur.sikkerhet
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.familie.ef.sak.behandlingsflyt.steg.BehandlerRolle
 import no.nav.familie.ef.sak.infrastruktur.config.RolleConfig
 import no.nav.familie.sikkerhet.EksternBrukerUtils
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.slf4j.LoggerFactory
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 
 object SikkerhetContext {
     private const val SYSTEM_NAVN = "System"
     const val SYSTEM_FORKORTELSE = "VL"
+    private const val NAV_GROUPS_HEADER = "Nav-Groups"
 
     val NAVIDENT_REGEX = """^[a-zA-Z]\d{6}$""".toRegex()
 
     private val secureLogger = LoggerFactory.getLogger("secureLogger")
+    private val objectMapper = jacksonObjectMapper()
 
     fun erMaskinTilMaskinToken(): Boolean {
         val claims = SpringTokenValidationContextHolder().getTokenValidationContext().getClaims("azuread")
@@ -70,8 +76,13 @@ object SikkerhetContext {
                 onFailure = { if (strict) error("Finner ikke navn på innlogget bruker") else SYSTEM_NAVN },
             )
 
-    fun hentGrupperFraToken(): Set<String> =
-        Result
+    fun hentGrupperFraToken(): Set<String> {
+        val grupperFraHeader = hentGrupperFraHeader()
+        if (grupperFraHeader.isNotEmpty()) {
+            return grupperFraHeader
+        }
+
+        return Result
             .runCatching { SpringTokenValidationContextHolder().getTokenValidationContext() }
             .fold(
                 onSuccess = {
@@ -81,6 +92,19 @@ object SikkerhetContext {
                 },
                 onFailure = { emptySet() },
             )
+    }
+
+    private fun hentGrupperFraHeader(): Set<String> =
+        Result
+            .runCatching {
+                val requestAttributes = RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes
+                val header = requestAttributes?.request?.getHeader(NAV_GROUPS_HEADER)
+                if (header != null) {
+                    objectMapper.readValue<List<String>>(header).toSet()
+                } else {
+                    emptySet()
+                }
+            }.getOrElse { emptySet() }
 
     fun harTilgangTilGittRolle(
         rolleConfig: RolleConfig,
