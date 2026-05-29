f357b639db6217a3c2d7c92bf3f4604cd33e13dd
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-05-29 12:57:13 +0200

    Ny extension funksjon for å mappe om TidligereVedtaksperioderDto. Hvis historiskPensjon er null skal VET_IKKE returneres.

--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/soknad/EksternSøknadService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/soknad/EksternSøknadService.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.sak.ekstern.soknad
 
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.TidligereVedtaksperioderService
+import no.nav.familie.ef.sak.vilkår.dto.TidligereVedtaksperioderDto
 import no.nav.familie.ef.sak.vilkår.dto.tilDto
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -18,13 +19,20 @@ class EksternSøknadService(
             val folkeregisteridentifikatorer = personService.hentSøker(personIdent).folkeregisteridentifikator
             val tidligereVedtaksperioder = tidligereVedtaksperioderService.hentTidligereVedtaksperioder(folkeregisteridentifikatorer)
 
-            if (tidligereVedtaksperioder.tilDto().harTidligereVedtaksperioder()) {
-                TidligereVedtakStatus.JA
-            } else {
-                TidligereVedtakStatus.NEI
-            }
+            tidligereVedtaksperioder.tilDto().tilTidligereVedtakStatus()
         } catch (e: Exception) {
             logger.warn("Feil ved sjekk av tidligere innvilget vedtak", e)
             TidligereVedtakStatus.VET_IKKE
         }
+}
+
+private fun TidligereVedtaksperioderDto.tilTidligereVedtakStatus(): TidligereVedtakStatus {
+    val harFraInfotrygd = infotrygd?.harTidligereInnvilgetVedtak() ?: false
+    val harFraSak = sak?.harTidligereInnvilgetVedtak() ?: false
+
+    if (harFraInfotrygd || harFraSak) return TidligereVedtakStatus.JA
+    if (historiskPensjon == null) return TidligereVedtakStatus.VET_IKKE
+    if (historiskPensjon) return TidligereVedtakStatus.JA
+
+    return TidligereVedtakStatus.NEI
 }
