41c55543aadbd8bf0f9448cc2255b7ef8b0d8ba0
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-05-29 12:14:47 +0200

    Skal også sjekke historikk i PE PP - historisk pensjon for å kunne utlede om en person har tidligere vedtak som enslig forsørger.

--- src/main/kotlin/no/nav/familie/ef/sak/ekstern/soknad/EksternSøknadService.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/ekstern/soknad/EksternSøknadService.kt
@@ -1,38 +1,24 @@
 package no.nav.familie.ef.sak.ekstern.soknad
 
-import no.nav.familie.ef.sak.fagsak.FagsakService
-import no.nav.familie.ef.sak.infotrygd.InfotrygdService
 import no.nav.familie.ef.sak.opplysninger.personopplysninger.PersonService
-import no.nav.familie.ef.sak.opplysninger.personopplysninger.pdl.identer
-import no.nav.familie.ef.sak.tilkjentytelse.TilkjentYtelseRepository
-import no.nav.familie.kontrakter.felles.ef.StønadType
+import no.nav.familie.ef.sak.opplysninger.personopplysninger.TidligereVedtaksperioderService
+import no.nav.familie.ef.sak.vilkår.dto.tilDto
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class EksternSøknadService(
-    private val fagsakService: FagsakService,
-    private val tilkjentYtelseRepository: TilkjentYtelseRepository,
     private val personService: PersonService,
-    private val infotrygdService: InfotrygdService,
+    private val tidligereVedtaksperioderService: TidligereVedtaksperioderService,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     fun harTidligereInnvilgetVedtak(personIdent: String): TidligereVedtakStatus =
         try {
-            val personIdenter = personService.hentPersonIdenter(personIdent).identer()
+            val folkeregisteridentifikatorer = personService.hentSøker(personIdent).folkeregisteridentifikator
+            val tidligereVedtaksperioder = tidligereVedtaksperioderService.hentTidligereVedtaksperioder(folkeregisteridentifikatorer)
 
-            val harInnvilgetIEfSak =
-                StønadType.values().any { stønadstype ->
-                    fagsakService
-                        .finnFagsak(personIdenter, stønadstype)
-                        ?.let { tilkjentYtelseRepository.finnAlleIverksatteForFagsak(it.id) }
-                        ?.any { it.andelerTilkjentYtelse.isNotEmpty() } == true
-                }
-
-            val harInnvilgetIInfotrygd = infotrygdService.eksisterer(personIdent)
-
-            if (harInnvilgetIEfSak || harInnvilgetIInfotrygd) {
+            if (tidligereVedtaksperioder.tilDto().harTidligereVedtaksperioder()) {
                 TidligereVedtakStatus.JA
             } else {
                 TidligereVedtakStatus.NEI
