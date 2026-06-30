f272583a223c9ec5df02cf3a9fb6f702d667abcf
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-06-30 10:11:51 +0200

    Validerer at saksbehandler eier behandling og den er under behandling for å kunne gjøre endringer til begrunnelse.

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/Regelendring2026Service.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/Regelendring2026Service.kt
@@ -1,12 +1,16 @@
 package no.nav.familie.ef.sak.behandling
 
 import no.nav.familie.ef.sak.behandling.domain.Regelendring2026
+import no.nav.familie.ef.sak.infrastruktur.exception.brukerfeilHvis
+import no.nav.familie.ef.sak.oppgave.TilordnetRessursService
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class Regelendring2026Service(
     private val regelendring2026Repository: Regelendring2026Repository,
+    private val behandlingService: BehandlingService,
+    private val tilordnetRessursService: TilordnetRessursService,
 ) {
     fun hent(behandlingId: UUID): Regelendring2026? = regelendring2026Repository.findByBehandlingId(behandlingId)
 
@@ -14,6 +18,14 @@ class Regelendring2026Service(
         behandlingId: UUID,
         begrunnelse: String,
     ) {
+        val saksbehandling = behandlingService.hentSaksbehandling(behandlingId)
+        brukerfeilHvis(saksbehandling.status.behandlingErLåstForVidereRedigering()) {
+            "Behandling er låst for videre redigering"
+        }
+        brukerfeilHvis(!tilordnetRessursService.tilordnetRessursErInnloggetSaksbehandler(behandlingId)) {
+            "Behandlingen eies av en annen saksbehandler"
+        }
+
         val eksisterende = hent(behandlingId)
         if (eksisterende != null) {
             regelendring2026Repository.update(
