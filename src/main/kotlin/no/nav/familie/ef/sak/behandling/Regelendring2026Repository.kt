22fa3676cac28154761a5f05a92be0959004377a
olekvernberg <ole.christian.kvernberg@nav.no>
2026-07-15 12:44:41 +0200

    Upsert begrunnelse i regelendring2026 for å unngå errors med insert av duplikate primary keys (hvis flere podder prøver å inserte samtidig)

--- src/main/kotlin/no/nav/familie/ef/sak/behandling/Regelendring2026Repository.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/behandling/Regelendring2026Repository.kt
@@ -3,6 +3,8 @@ package no.nav.familie.ef.sak.behandling
 import no.nav.familie.ef.sak.behandling.domain.Regelendring2026
 import no.nav.familie.ef.sak.repository.InsertUpdateRepository
 import no.nav.familie.ef.sak.repository.RepositoryInterface
+import org.springframework.data.jdbc.repository.query.Modifying
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
@@ -11,4 +13,18 @@ interface Regelendring2026Repository :
     RepositoryInterface<Regelendring2026, UUID>,
     InsertUpdateRepository<Regelendring2026> {
     fun findByBehandlingId(behandlingId: UUID): Regelendring2026?
+
+    @Modifying
+    @Query(
+        """
+        INSERT INTO regelendring_2026 (behandling_id, begrunnelse)
+        VALUES (:behandlingId, :begrunnelse)
+        ON CONFLICT (behandling_id) DO UPDATE SET
+            begrunnelse = excluded.begrunnelse
+        """,
+    )
+    fun upsert(
+        behandlingId: UUID,
+        begrunnelse: String,
+    )
 }
